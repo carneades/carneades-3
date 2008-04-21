7f0997dee45af3fe8dbd6338e0e0b5c4490fa92c
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2008-04-21 13:30:10 +0000

    repaired directory structure

---
+++ lib/carneades/argument-diagram.scm
@@ -0,0 +1,112 @@
+(module argument-diagram mzscheme
+  (require "statement.scm")
+  (require "argument.scm")
+  (require "stream.scm")
+  (require (prefix unify: "unify.scm"))
+  (require (prefix config: "config.scm"))
+  (require (lib "string.ss" "srfi" "13"))
+  (require (prefix stream: "stream.scm"))
+  (require (lib "file.ss"))
+  (require (lib "process.ss"))
+  
+  (provide diagram view diagram* view*)
+  
+  ; get-id: hash-table datum -> symbol
+  ; get the symbol used to identify some datum, generating
+  ; one if the datum does not yet have an identifier.
+  ; The identifers are suitable for naming nodes and arrows 
+  ; in the DOT language, for use with GraphViz
+  (define (get-id tbl d)
+    (hash-table-get tbl d 
+                    (lambda () (let ((v (gensym "g")))(hash-table-put! tbl d v) v))))
+  
+  ; diagram*: argument-graph context substitutions
+  ;           (-> statement string) output-port -> void
+  (define (diagram* ag context subs statement->string port)
+    (let ((tbl (make-hash-table 'equal)))
+      (display "digraph g {\n    rankdir = \"RL\";\n" port)
+      (print-statements ag context subs tbl (statements ag) statement->string  port)
+      (print-arguments ag context subs tbl (list-arguments ag) port)
+      (display "}\n" port)))
+  
+  
+  ; print-statements: argument-graph context substitutions 
+  ;              hash-table (list-of statement) (-> statement string) output-port -> void
+  (define (print-statements ag c subs tbl statements statement->string port)
+    (define (print-statement n)
+      (let ((id (get-id tbl n)))
+        (fprintf port "    ~a [shape=box, label=~v, style=~v];\n"
+                 id
+                 (statement->string (subs n))
+                 (if (or (and (acceptable? ag c n)
+                              (not (rejected? c n)))
+                         (accepted? c n))
+                     "filled" 
+                     ""))))
+    (for-each print-statement statements))
+  
+  ; print-arguments: argument-graph context substitutions
+  ;                  hash-table (list-of argument) output-port -> void
+  (define (print-arguments ag c subs tbl args port)
+    (define (print-argument arg)
+      (fprintf port "    ~a [shape=ellipse, label=~v, style=~v];\n"
+               (get-id tbl (argument-id arg))
+               (if (and (argument-scheme arg)
+                        (< 0 (string-length (argument-scheme arg))))
+                   (argument-scheme arg)
+                   (symbol->string (argument-id arg)))
+               (if (all-premises-hold? ag c arg) "filled" ""))
+      (fprintf port "    ~a -> ~a~a;\n" 
+               (get-id tbl (argument-id arg))
+               (get-id tbl (argument-conclusion arg))
+               (if (eq? (argument-direction arg) 'con) " [arrowhead=\"onormal\"]" ""))
+      (for-each (lambda (p) 
+                  (fprintf port "    ~a -> ~a [arrowhead=\"~a~a\"];\n" 
+                           (get-id tbl (premise-atom p))
+                           (get-id tbl (argument-id arg))
+                           (cond ((assumption? p) "dot")
+                                 ((exception? p) "odot") 
+                                 (else "none"))
+                           (if (negative-premise? p) "tee" "")))
+                (argument-premises arg))) 
+    (for-each print-argument args))
+  
+  ; view*: argument-graph context substitutions
+  ;        (-> statement string) -> void
+  ; Provides a convenient way to display an argument graph. 
+  ; Based on code contributed by András Förhécz <fandrew@mit.bme.hu>.
+  ; To do: find a way to put the viewer process in the background, but still have the 
+  ; temporary files deleted after use.
+  
+  (define (view* ag c subs statement->string)
+    (let* ((suffix config:preferred-graphic-format)
+           (format (if (equal? config:preferred-graphic-format "ps") 
+                       "ps2" 
+                       config:preferred-graphic-format))
+           (tmp-dot (make-temporary-file "mztmp~a.dot")))    
+      (call-with-output-file tmp-dot
+        (lambda (port) (diagram* ag c subs statement->string port))
+        'truncate)
+      (if (equal? format "dot")
+          (begin (system (string-append config:viewer " " (path->string tmp-dot)))
+                 (delete-file tmp-dot))
+          (let ((tmp-output (make-temporary-file (string-append "mztmp~a." suffix))))
+            (system (string-append config:dot 
+                                   (string-append " -T" format " ")
+                                   (path->string tmp-dot) 
+                                   " -o " 
+                                   (path->string tmp-output)))
+            (system (string-append config:viewer " " (path->string tmp-output)))
+            (delete-file tmp-dot)
+            (delete-file tmp-output)))))
+  
+  
+  ; diagram: argument-graph context -> void
+  (define (diagram ag c)
+    (diagram* ag c unify:identity (lambda (s) (format "~a" s)) (current-output-port)))
+  
+  ; view: argument-graph context -> void
+  (define (view ag c)
+    (view* ag c unify:identity (lambda (s) (format "~a" s))))
+  
+  ) ; end module argument-map
---
+++ lib/carneades/unify.scm
@@ -0,0 +1,142 @@
+(module unify mzscheme
+    
+  (provide variable? identity unify* unify genvar rename-variables ground?)
+  
+  ; Based on the implementation of the unification algorithm from
+  ; the book "The Scheme Programming Language", by Kent Dybvig.
+  
+  ; type term = literal | variable | list
+  ; type literal = symbol | number | string | boolean
+
+  ; variable? : object -> boolean
+  ; A logic variable is represented as a symbol prefixed with a 
+  ; question mark, as in SWRL. For example: '?x
+  (define variable? 
+    (lambda (u)
+      (and (symbol? u)
+           (let ((s (symbol->string u)))
+             (and (> (string-length s) 0)
+                  (eq? (string-ref s 0) #\?))))))
+  
+  ; literal? : object -> boolean
+  ; Warning: variables are also literals. Be sure to check whether
+  ; an object is a variable before checking whether it is a literal,
+  ; in contexts where the two need to be distinguished.
+  (define literal?
+    (lambda (u)
+      (or (symbol? u)
+          (number? u)
+          (string? u)
+          (boolean? u))))
+  
+    
+  ; occurs? returns true if and only if u occurs in v
+  (define occurs?
+    (lambda (u v)
+      (and (pair? v)
+           (let f ((l (cdr v)))
+             (and (pair? l)
+                  (or (eq? u (car l))
+                      (occurs? u (car l))
+                      (f (cdr l)))))))) 
+
+  ;; substitution constructors: identity and sigma
+  
+  ; identity substition
+  (define identity (lambda (x) x))
+  
+  ; sigma returns a new substitution procedure extending s by
+  ; the substitution of u with v
+  (define sigma
+    (lambda (u v s)
+      (lambda (x)
+        (let f ((x (s x)))
+          (cond ((variable? x) (if (eq? x u) v x))
+                ((pair? x) (cons (car x) (map f (cdr x))))
+                (else x))))))
+    
+
+  ; try-subst tries to substitute u for v but may require a
+  ; full unification if (s u) is not a variable, and it may
+  ; fail if it sees that u occurs in v.
+  (define try-subst
+    (lambda (u v s ks kf occurs-check)
+      (let ((u (s u)))
+        (if (not (variable? u))
+            (unify* u v s ks kf occurs-check)
+            (let ((v (s v)))
+              (cond
+                ((eq? u v) (ks s))
+                ((and occurs-check (occurs? u v)) (kf "cycle"))
+                (else (ks (sigma u v s))))))))) 
+
+  ; unify* attempts to unify u and v with a continuation-passing
+  ; style that returns a substitution to the success argument
+  ; ks or an error message to the failure argument kf.  The
+  ; substitution itself is represented by a procedure from
+  ; variables to terms. The occurs-check flag determines whether
+  ; the occurs check is performed.
+  (define unify*
+    (lambda (u v s ks kf occurs-check)
+      (cond
+        ((variable? u) (try-subst u v s ks kf occurs-check))
+        ((variable? v) (try-subst v u s ks kf occurs-check))
+        ((and (literal? u) (literal? v))
+         (if  (equal? u v) (ks s) (kf "clash")))
+        ((and (pair? u) 
+              (pair? v) 
+              (eq? (car u) (car v))
+              (= (length u) (length v)))
+         (let f ((u (cdr u)) (v (cdr v)) (s s))
+           (if (null? u)
+               (ks s)
+               (unify* (car u)
+                       (car v)
+                       s
+                       (lambda (s) (f (cdr u) (cdr v) s))
+                       kf
+                       occurs-check))))
+        (else (kf "clash"))))) 
+
+  ; unify: term term -> substitution | #f
+  ; unify is a simplified interface to unify*, where the initial
+  ; substitution is the identity procedure, the initial success
+  ; continuation returns the unified term, the initial failure
+  ; continuation returns #f and the occurs-check is not performed.
+  (define unify
+    (lambda (u v)
+      (unify* u
+              v
+              identity
+              (lambda (s) s)
+              (lambda (msg) #f)
+              #f)))
+  
+  ; genvar: -> variable
+  ; generate a fresh, unique variable
+  (define (genvar) (gensym "?"))
+
+  ; rename: hash-table term -> term
+  ; Systematically rename the variables in term, keeping track of the 
+  ; replacements in the hash table. Warning: this can have side-effects. 
+  ; The hash-table can be modified.
+  (define (rename-variables tbl trm)
+    (cond ((variable? trm) 
+           (hash-table-get tbl trm 
+                           (lambda () 
+                             (let ((v (genvar)))
+                               (hash-table-put! tbl trm v) 
+                               v))))
+          ((pair? trm)
+           (cons (rename-variables tbl (car trm)) (rename-variables tbl (cdr trm)))) 
+          (else trm)))
+
+  ; ground: term -> boolean
+  (define (ground? trm)
+    (cond ((variable? trm) #f)
+          ((literal? trm) #t)
+          ((pair? trm) (and (ground? (car trm))
+                            (ground? (cdr trm))))
+          (else #t)))
+                                     
+) ; end of unify module
