bceb7e69b870211609a9f4b63ca50092db43cc30
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2008-06-26 15:47:57 +0000

    Removed the xml directory, in order to replace it with a better version.

--- src/carneades/lib/xml/ssax/compile.scm
+++
@@ -1,8 +0,0 @@
-
-(define files
-  (list "char-encoding.scm" "input-parse.scm"
-        "ssax-code.scm" "util.scm"
-        "common.scm" "look-for-str.scm"
-        "ssax-parser.scm"))
-
-(for-each compile-file files)
