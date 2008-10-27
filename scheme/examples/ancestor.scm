de2586682b104c212230a0fa631c49c88a0db104
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2008-10-27 16:21:23 +0000

    Modified the ancestor example to use assumptions in the the context to model the facts, instead of bodiless rules.

--- scheme/examples/ancestor.scm
+++ scheme/examples/ancestor.scm
@@ -3,23 +3,12 @@
 (import (rnrs) 
         (carneades rule)
         (carneades shell)
+        (carneades argument)
+        (carneades argument-builtins)
         (carneades lib srfi lightweight-testing))
 
 (define rb1 
   (rulebase
-   
-   (rule* facts 
-          (parent Caroline Tom) 
-          (parent Caroline Ines)
-          (parent Dustin Tom)
-          (parent Dustin Ines)
-          (parent Tom Gloria)
-          (parent Ines Hildegard)
-          (Male Tom)
-          (Parent Tom)
-          (Female Ines)
-          (Parent Ines)
-          )
    
    (rule* r1 
           (if (parent ?x ?y)
@@ -27,7 +16,7 @@
    
    (rule* r2 
           (if (and (ancestor ?x ?z) 
-                   (ancestor ?z ?y))
+                   (parent ?z ?y))
               (ancestor ?x ?y)))
    
    (rule* r3
@@ -36,9 +25,8 @@
               (Father ?x)))
    (rule* r4
           (if (Father ?x)
-              (and
-               (Male ?x)
-               (Parent ?x))))
+              (and (Male ?x)
+                   (Parent ?x))))
    
    (rule* r5
           (if (and (Female ?x)
@@ -47,17 +35,31 @@
    
    (rule* r6
           (if (Mother ?x)
-              (and (Female)
-                   (Parent))))
+              (and (Female ?x)
+                   (Parent ?x))))
    
    
    )) ; rulebase
 
+(define assumptions
+  '((parent Caroline Tom) 
+    (parent Caroline Ines)
+    (parent Dustin Tom)
+    (parent Dustin Ines)
+    (parent Tom Gloria)
+    (parent Ines Hildegard)
+    (Male Tom)
+    (Parent Tom)
+    (Female Ines)
+    (Parent Ines)
+    ))
 
 ; engine integer integer  -> statement -> (stream-of argument-state)
 (define (engine max-nodes max-turns)
-  (make-engine max-nodes max-turns 
-               (list (generate-arguments-from-rules rb1 '()))))
+  (make-engine* max-nodes max-turns 
+               (accept default-context assumptions)
+               (list (generate-arguments-from-rules rb1 '())
+                     builtins)))
 
 (define e1 (engine 50 1))
 
@@ -68,8 +70,8 @@
 (check (all-in? '(ancestor Caroline Tom) e1) => #t)
 (check (no-argument-found? '(parent Hildegard Tom) e1) => #t)
 (check (all-in? '(ancestor Caroline Gloria) e1) => #t)
-(check (all-in? '(applies ?r (parent ?x ?y)) e1) => #t)
-(check (all-in? '(applies facts (parent Caroline Tom)) e1) => #t)
+(check (all-in? '(applies ?r (ancestor ?x ?y)) e1) => #t)
+(check (all-in? '(applies r1 (ancestor Caroline Tom)) e1) => #t)
 (check (all-in? '(Father Tom) e1) => #t)
 (check (all-in? '(Mother Ines) e1) => #t)
 
