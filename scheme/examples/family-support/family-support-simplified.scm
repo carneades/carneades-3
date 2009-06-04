d1c286676af7fc87af1d14a341b0af209d376a2e
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2009-06-04 12:33:40 +0000

    - Added a simplified version of the family support example, without casebased reasoning and testimonial evidence, for use in a course.

---
+++ scheme/examples/family-support/family-support-simplified.scm
@@ -0,0 +1,41 @@
+#!r6rs
+
+(import (rnrs)
+        (carneades lkif2)
+        (carneades argument)
+        (carneades argument-builtins)
+        (carneades dlp) ; Description Logic Programming subset of Description Logic
+        (carneades rule)
+        (carneades shell))
+
+(ontology family-relations
+          
+          (define-primitive-role family:ancestor family:relative)
+          (define-primitive-role family:parent family:ancestor)
+          (define-primitive-role family:ancestor (inverse family:descendent))
+          (define-primitive-role family:mother family:parent)
+          (define-primitive-role family:father family:parent)
+          (define-primitive-role family:sibling family:relative)
+          (define-primitive-role family:brother family:sibling)
+          (define-primitive-role family:sister family:sibling))
+
+(define family-data (lkif-import "family-support.xml"))
+
+(define family-support (lkif-data-rulebase family-data))
+
+; type critical-question = excluded | priority | valid
+
+(define argument-generators
+  (list builtins
+        (generate-arguments-from-ontology family-relations '())
+        (generate-arguments-from-rules family-support '(excluded))))
+
+
+; 1. Find the best arguments about whether Tom is obligated to support Gloria, without considering the
+; issue of undue hardship
+
+(define e1 (make-engine* 50 2 empty-argument-graph argument-generators))
+
+(show1 '(family:obligatedToSupport Max ?y) e1)
+
+; end of file
