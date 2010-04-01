4a738d6d7fbb96d2eee63a17569d027fa0dbab23
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-04-01 10:01:51 +0000

    Support for leiningen

---
+++ clojure/Carneades/project.clj
@@ -0,0 +1,10 @@
+(defproject carneades "0.0.1-SNAPSHOT"
+  :description "Carneades is an argument mapping application, with a graphical 
+user interface, and a software library for building applications supporting 
+various argumentation tasks."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [org.clojars.pallix/jgraphx "1.3.1.5"]])
+
+;; jgraphx will be downloaded from clojars.org
+
