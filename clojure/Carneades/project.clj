9ce81d88323aed5a7945bd664d8301fcdec0e1fe
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-04-29 08:51:16 +0000

    use the thread classloader to read the property file to solve the classloader problem with lein swank

--- clojure/Carneades/project.clj
+++ clojure/Carneades/project.clj
@@ -1,10 +1,12 @@
+
 (defproject carneades "0.0.1-SNAPSHOT"
   :description "Carneades is an argument mapping application, with a graphical 
 user interface, and a software library for building applications supporting 
 various argumentation tasks."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojars.pallix/jgraphx "1.3.1.5"]])
+                 [org.clojars.pallix/jgraphx "1.3.1.5"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
 
 ;; jgraphx will be downloaded from clojars.org
 
