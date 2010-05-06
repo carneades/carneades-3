699592e41cb90a5bbd6f161b7a4fb11884217688
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-05-06 11:55:24 +0000

    missing LGPL headers

--- clojure/Carneades/src/carneades/engine/argument_search.clj
+++ clojure/Carneades/src/carneades/engine/argument_search.clj
@@ -1,3 +1,18 @@
+;;; Carneades Argumentation Library and Tools.
+;;; Copyright (C) 2010 Thomas F. Gordon, Fraunhofer FOKUS, Berlin
+;;; 
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License version 3 (LGPL-3)
+;;; as published by the Free Software Foundation.
+;;; 
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for details.
+;;; 
+;;; You should have received a copy of the GNU Lesser General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns carneades.engine.argument-search
   (:use clojure.contrib.def
         carneades.engine.utils
