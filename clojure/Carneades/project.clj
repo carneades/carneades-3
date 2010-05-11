0301454920cd9cfb1cfa709de95d0cab4f09ae48
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-05-11 10:15:51 +0000

    updated clj-sandbox version

--- clojure/Carneades/project.clj
+++ clojure/Carneades/project.clj
@@ -6,6 +6,7 @@ various argumentation tasks."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [org.clojars.pallix/jgraphx "1.4.0.1"]
-                 [clj-sandbox "0.3.4"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+                 [clj-sandbox "0.3.5"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+                     [lein-run "1.0.0-SNAPSHOT"]])
 
