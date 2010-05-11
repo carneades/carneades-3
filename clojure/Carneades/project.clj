f49966130ccd30380e845df3b248558247fe5b89
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-05-11 10:15:55 +0000

    update to jgraphx-1.4.0.2 (fix the export problem with dashed lines)

--- clojure/Carneades/project.clj
+++ clojure/Carneades/project.clj
@@ -5,7 +5,7 @@ user interface, and a software library for building applications supporting
 various argumentation tasks."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojars.pallix/jgraphx "1.4.0.1"]
+                 [org.clojars.pallix/jgraphx "1.4.0.2"]
                  [clj-sandbox "0.3.5"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]
                      [lein-run "1.0.0-SNAPSHOT"]])
