3273f6e59e123d7dfff2c52d36261cf8f9302dd4
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2010-05-20 16:49:12 +0000

    Modified to KnowledgeTools example to illustrate the argument-from-argument module.

--- examples/KnowledgeTools/kt-ex1.scm
+++ examples/KnowledgeTools/kt-ex1.scm
@@ -9,7 +9,6 @@
         (carneades stream)
         (carneades argument-search)
         (prefix (carneades argument) argument:)
-        (prefix (carneades evidence) e:)
         (carneades argument-diagram)
         (carneades argument-from-arguments)
         (carneades lib srfi lightweight-testing))
@@ -19,38 +18,13 @@
 (define lkif-data (lkif:lkif-import "kt-ex1.xml"))
  
 (define ag1 (car (lkif:lkif-data-argument-graphs lkif-data)))
-
-(define witness (e:make-witness "Gerd"))
-
-(define form1 
-  (e:make-form 
-   'form1
-   ; questions
-   (list (e:make-question 'k473 'boolean 'one "Max 10% of nominal value?"))
-   ; help text, in SXML format
-   null))
-
-(define testimony (e:make-testimony witness (list form1)))
+(define ag2 (argument:accept argument:empty-argument-graph (list "max. 10% of nominal value")))
 
 ; engine integer integer argument-graph -> statement -> (stream-of argument-state)
-(define (engine max-nodes max-turns ag)
-  (make-engine* max-nodes max-turns ag
-                (list  (e:generate-arguments-from-testimony testimony) ; ask the user first
-                       (generate-arguments-from-argument-graph ag1))))
+(define e1 
+  (make-engine* 20 1 ag2
+                (list (generate-arguments-from-argument-graph ag1))))
 
-
-(define c1 (argument:accept argument:empty-argument-graph (list 'k473)))
-(define e1 (engine 20 1 c1))
-
-;(check (succeed? 'k473 e1) => #t)  ; max 10% of nominal value   
-;(check (succeed? 'k472 e1) => #t)  ; cash payment
-;(check (succeed? 'k470 e1) => #t)  ; counter-performance
-;(check-report)
-
-
-; Answers to provide to questions asked:
-; 1. Max 10% of nominal value?
-; Answer: (all #t)
-
-(ask1 'k470 e1) 
-; (show1 'k470 e1) 
+; (view ag1)
+; (ask1 "cash payment" e1) 
+(show1 "cash payment" e1) 
