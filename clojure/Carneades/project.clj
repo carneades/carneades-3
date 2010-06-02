c07697961d14db4ffd08cd2ab6adbb3702b6340d
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-06-02 08:57:21 +0000

    owlapi dependency in project.clj

--- clojure/Carneades/project.clj
+++ clojure/Carneades/project.clj
@@ -6,6 +6,7 @@ various argumentation tasks."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [org.clojars.pallix/jgraphx "1.4.0.2"]
+                 [org.clojars.pallix/owlapi "3.0.0"]
                  [clj-sandbox "0.3.7"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]
                      [lein-run "1.0.0-SNAPSHOT"]])
