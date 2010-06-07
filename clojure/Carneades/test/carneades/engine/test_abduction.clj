a27d657ba5bf486ae0b9ebc13ee1fc0397724ffd
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-06-07 12:31:16 +0000

    fix for test_abduction.clj

--- clojure/Carneades/test/carneades/engine/test_abduction.clj
+++ clojure/Carneades/test/carneades/engine/test_abduction.clj
@@ -34,8 +34,7 @@
                               (pm u)
                               (ex v)))
         a3 (argument :a3 false 0.6 :con r [(pm t)] nil)
-        a5 (make-arg :a5 (con r
-                              (pm w)))
+        a5 (argument :a5 false 0.4 :con r [(pm w)] nil)
         a1 (argument :a1 false 0.6 :con p [(pm r)] nil)
         a2 (argument :a2 false 0.4 :pro p [(pm s) (pm q)] nil)
         ag (assoc-standard (assert-arguments
@@ -49,7 +48,11 @@
               (statement-complement v)
               (statement-complement t)
               w s}
-        in-label-p (statement-in-label ag asm p)]
+        in-label-p (statement-in-label ag asm p)
+        in-label-not-p
+        (set (statement-in-label ag asm (statement-complement p)))
+        out-label-p (statement-out-label ag asm p)
+        out-label-not-p (statement-out-label ag asm (statement-complement p))]
     ;; (dotrace [combine-conjunction-of-dnf
     ;;           statement-in-label
     ;;           ba-in-label]
@@ -59,6 +62,18 @@
                  (= (second in-label-p) (list q)))
             (and (= (first in-label-p) (list q))
                  (= (second in-label-p) (list p)))))
+
+    (is (= (count in-label-not-p)) 3)
+    (is (contains? in-label-not-p (list (statement-complement p))))
+    (is (contains? in-label-not-p (list r)))
+    (is (contains? in-label-not-p (list u)))
+
+    (is (= (count out-label-p) 1))
+    (is (= (first out-label-p) (list true)))
+
+    (is (= (count out-label-not-p) 1))
+    (is (= (first out-label-not-p) (list true)))
+
     ;; (let [x (statement-in-label ag asm p)]
     ;;   (printf "=============\n")
     ;;   (pprint x)
