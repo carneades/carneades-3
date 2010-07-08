05318b2c0d5aa1f6702c72b8efdf44cec4d0f41e
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-07-08 14:54:54 +0000

    CarneadesEditorUIComponents project

---
+++ clojure/CarneadesEditorUIComponents/src/main/java/carneades/editor/uicomponents/App.java
@@ -0,0 +1,13 @@
+package carneades.editor.uicomponents;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
