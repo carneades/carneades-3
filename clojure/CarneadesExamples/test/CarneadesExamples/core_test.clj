2bc665a1c9c72cd599011ccd9c15bb61429a510c
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-07-08 17:04:30 +0000

    moved examples in the CarneadesExamples project, and added CarneadesMapComponent project

---
+++ clojure/CarneadesExamples/test/CarneadesExamples/core_test.clj
@@ -0,0 +1,6 @@
+(ns CarneadesExamples.core-test
+  (:use [CarneadesExamples.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
