5afa8f0d817bd00147a5eb5082c710f6e40fb16b
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-07-12 09:55:42 +0000

    use of protocols for the MVC pattern of the Editor

--- clojure/CarneadesEditor/src/carneades/editor/core.clj
+++ clojure/CarneadesEditor/src/carneades/editor/core.clj
@@ -1,11 +1,13 @@
 (ns carneades.editor.core
   (:use carneades.editor.view.editorapplication
         carneades.editor.controller.listeners-register)
+  (:import carneades.editor.view.editorapplication.SwingView)
   (:gen-class))
 
 (defn -main [& args]
   (prn "Starting the Carneades Editor...")
-  (register-listeners)
+  (let [view (SwingView.)]
+    (register-listeners view))
   (launch-application args))
 
 ;; printf broken in the JAR?
