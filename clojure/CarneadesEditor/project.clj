7fe7f7ba038a0c4ec0e686565af439ceb3270419
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2010-07-15 10:01:06 +0000

    Updated the project.clj files to use the latest beta of Clojure 1.2.

--- clojure/CarneadesEditor/project.clj
+++ clojure/CarneadesEditor/project.clj
@@ -1,7 +1,7 @@
 (defproject CarneadesEditor "1.0.0-SNAPSHOT"
   :description "Carneades is an argument mapping application, with a graphical user interface, and a software library for building applications supporting various argumentation tasks. This is the graphical editor."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
                  [carneades-engine "1.0.0-SNAPSHOT"]
                  [carneades-editor-uicomponents "1.0.0-SNAPSHOT"]
                  [carneades-mapcomponent "1.0.0-SNAPSHOT"]]
