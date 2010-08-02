f8d60b9b575acae014cf0d21c07bca36e679199e
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-08-02 16:08:46 +0000

    missing swing listener file

---
+++ clojure/CarneadesEditor/src/carneades/editor/controller/swing_listeners.clj
@@ -0,0 +1,97 @@
+(ns carneades.editor.controller.swing-listeners
+  (:use clojure.contrib.def
+        clojure.contrib.swing-utils
+        carneades.editor.view.editorapplication
+        carneades.editor.view.tree
+        carneades.editor.controller.listeners
+        carneades.editor.utils.swing)
+  (:import (carneades.editor.uicomponents EditorApplicationView)
+           (carneades.editor.view.tree GraphInfo LkifFileInfo)))
+
+(defvar- *viewinstance* (EditorApplicationView/instance))
+
+(defvar- *openFileMenuItem* (.openFileMenuItem *viewinstance*))
+(defvar- *closeTabMenuItem* (.closeTabMenuItem *viewinstance*))
+(defvar- *closeFileMenuItem* (.closeFileMenuItem *viewinstance*))
+(defvar- *exportFileMenuItem* (.exportFileMenuItem *viewinstance*))
+(defvar- *printPreviewFileMenuItem* (.printPreviewFileMenuItem *viewinstance*))
+(defvar- *aboutHelpMenuItem* (.aboutHelpMenuItem *viewinstance*))
+
+(defvar- *closeLkifFileMenuItem* (.closeLkifFileMenuItem *viewinstance*))
+(defvar- *exportLkifFileMenuItem* (.exportLkifFileMenuItem *viewinstance*))
+
+(defvar- *openGraphMenuItem* (.openGraphMenuItem *viewinstance*))
+(defvar- *closeGraphMenuItem* (.closeGraphMenuItem *viewinstance*))
+(defvar- *exportGraphMenuItem* (.exportGraphMenuItem *viewinstance*))
+
+(defvar- *openFileButton* (.openFileButton *viewinstance*))
+
+(defn- mouse-click-in-tree-listener [event view]
+  (let [clickcount (.getClickCount event)]
+    (when-let [node (.getLastSelectedPathComponent *lkifsTree*)]
+      (when-let [info (.getUserObject node)]
+        (prn "info")
+        (prn info)
+        (case clickcount
+            1 (condp instance? info 
+                GraphInfo (on-select-graphid view (:path (:lkifinfo info))
+                                             (:id info))
+                LkifFileInfo (on-select-lkif-file view (:path info))
+                nil)
+            2 (condp instance? info
+                GraphInfo (on-edit-graphid view (:path (:lkifinfo info))
+                                           (:id info))
+                nil)
+            nil)))))
+
+(defn- close-file-listener [event view]
+  (prn "close file listener")
+  (when-let [node (.getLastSelectedPathComponent *lkifsTree*)]
+    (when-let [info (.getUserObject node)]
+      (condp instance? info
+        LkifFileInfo (on-close-file view (:path info))
+        GraphInfo (on-close-file view (:path (:lkifinfo info)))
+        nil))))
+
+(defn- close-listener [event view]
+  (let [[path id] (current-graph view)]
+    (on-close-graph view path id)))
+
+(defn- open-graph-listener [event view]
+  (prn "open-graph-listener")
+  (when-let [node (.getLastSelectedPathComponent *lkifsTree*)]
+    (when-let [info (.getUserObject node)]
+      (condp instance? info
+        GraphInfo (on-open-graph view (:path (:lkifinfo info)) (:id info))
+        nil))))
+
+(defn- close-graph-listener [event view]
+  (when-let [node (.getLastSelectedPathComponent *lkifsTree*)]
+    (when-let [info (.getUserObject node)]
+      (condp instance? info
+        GraphInfo (on-close-graph view (:path (:lkifinfo info)) (:id info))
+        nil))))
+
+(defn- export-file-listener [event view]
+  (when-let [node (.getLastSelectedPathComponent *lkifsTree*)]
+    (when-let [info (.getUserObject node)]
+      (condp instance? info
+        GraphInfo (if-let [[path id] (current-graph view)]
+                    (on-export-graph view path id)
+                    (on-export-graph view (:path (:lkifinfo info)) (:id info)))
+        LkifFileInfo (if-let [[path id] (current-graph view)]
+                       (on-export-graph view path id)
+                       (on-export-file view (:path info)))
+        nil))))
+
+(defn- export-element-listener [event view]
+  (when-let [node (.getLastSelectedPathComponent *lkifsTree*)]
+    (when-let [info (.getUserObject node)]
+      (condp instance? info
+        GraphInfo (on-export-graph view (:path (:lkifinfo info)) (:id info))
+        LkifFileInfo (on-export-file view (:path info))
+        nil))))
+
+(defn- printpreview-listener [event view]
+  (if-let [[path id] (current-graph view)]
+    (on-printpreview-graph view path id)))
