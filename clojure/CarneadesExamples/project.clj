fafad5db9b9b2b4c6a00676618cb1ecde6a29c05
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-08-02 09:08:05 +0000

    updated clojure version to 1.2.0-RC1

--- clojure/CarneadesExamples/project.clj
+++ clojure/CarneadesExamples/project.clj
@@ -2,8 +2,8 @@
   :description "Carneades is an argument mapping application, with a graphical 
 user interface, and a software library for building applications supporting 
 various argumentation tasks. This are the examples"
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]
                  [carneades-engine "1.0.0-SNAPSHOT"]
                  [carneades-mapcomponent "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
--- clojure/CarneadesMapComponent/project.clj
+++ clojure/CarneadesMapComponent/project.clj
@@ -2,8 +2,8 @@
   :description "Carneades is an argument mapping application, with a graphical 
 user interface, and a software library for building applications supporting 
 various argumentation tasks. This is the Swing Argument Map Component."
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]
                  [org.clojars.pallix/jgraphx "1.4.0.8"]
                  [carneades-engine "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.0"]])
