7242d55e048a9bd077a554a5a927315793c95815
pallix <pallix@66655247-6d46-0410-a939-8778526650e4>
2010-08-04 15:53:50 +0000

    dashed, dotted and crossed styles for exception, assumption and negated premises

--- clojure/CarneadesMapComponent/project.clj
+++ clojure/CarneadesMapComponent/project.clj
@@ -6,4 +6,5 @@ various argumentation tasks. This is the Swing Argument Map Component."
                  [org.clojure/clojure-contrib "1.2.0-RC1"]
                  [org.clojars.pallix/jgraphx "1.4.0.8"]
                  [carneades-engine "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+  :dev-dependencies [[swank-clojure "1.2.0"]]
+  :aot [carneades.mapcomponent.negconnectorshape])
