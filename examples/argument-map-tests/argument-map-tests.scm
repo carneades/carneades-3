2023a58c94fe4e9e505f20f382b095fc5a469143
tfgordon <tfgordon@66655247-6d46-0410-a939-8778526650e4>
2010-08-05 10:13:15 +0000

    Added an example for testing the visualization of argument graphs, called "argument-graph-tests".

---
+++ examples/argument-map-tests/argument-map-tests.scm
@@ -0,0 +1,45 @@
+#!r6rs
+
+; Constructs an argument graph with all kinds of statement nodes, premises and arguments
+; and exports the graph to LKIF. For testing different visualization and mapping
+; methods.
+
+(import (rnrs)
+        (carneades argument)
+        (carneades rule)
+        (carneades lkif2)
+        (carneades argument-diagram))
+
+
+(define-argument a1
+  (pro "P"
+       (pr "Q")
+       (am "R")
+       (ex "S")))
+
+(define-argument a2
+  (con "P"
+       (pr '(not "T"))
+       (am '(not "U"))
+       (ex '(not "V"))))
+
+(define-argument a3 
+  (pro "R" 
+       (pr "W")))
+
+(define-argument a4 
+  (con "V"
+      (pr "X")))
+
+(define ag1 (make-argument-graph 'ag1 "argument graph test" "P"))
+(set! ag1 (assert-arguments ag1 (list a1 a2 a3 a4)))
+(set! ag1 (accept ag1 (list "Q" '(not "S") "W" "X")))
+(set! ag1 (reject ag1 (list "T"))) ; to check that reject is same as accepting the complement
+(set! ag1 (question ag1 (list "R")))
+(set! ag1 (assign-standard ag1 'se (list "P")))
+
+; (define ld (make-lkif-data (lkif-data-sources kb1) (lkif-data-rulebase kb1) (list ag1)))
+(define ld (make-lkif-data '() (rulebase) (list ag1)))
+(lkif-export '() ld "argument-map-tests.xml")
+
+(view ag1)
