18c102c350da6dcb67f41333f37beff42404c835
Stefan Ballnat <stefan.ballnat@fokus.fraunhofer.de>
2010-08-24 18:10:35 +0200

    - exists example added - npa licensing added
    Signed-off-by: Stefan Ballnat <stefan.ballnat@fokus.fraunhofer.de>

---
+++ src/CarneadesExamples/src/carneades/examples/exists.clj
@@ -0,0 +1,70 @@
+
+(ns carneades.examples.exists
+  ;(:require )
+  (:use
+    carneades.engine.argument
+    carneades.engine.rule
+    carneades.engine.shell
+    carneades.engine.argument-builtins
+    carneades.ui.diagram.viewer
+    )
+  ;(:import )
+  )
+
+(declare type-gens)
+
+(def rb
+  (rulebase
+
+   (rule r1 (if (or (= ?x 3)
+                    (= ?x 4))
+                (q ?x)))
+
+;    (assertions facts
+;         (u 2 5)
+;         (u 3 4)
+;         (u 4 3)
+;         (u 5 2))
+    ))
+
+(def type-rb
+  (rulebase
+
+    (rule t1 (if (exists x (t x) (u x ?y))
+               (s ?y)))
+
+   (assertions type-facts
+         (t 2)
+         (t 3)
+         (t 4)
+         (t 5)
+         (t 6)
+         (u 2 5)
+         (u 3 4)
+         (u 4 3)
+         (u 5 2))))
+
+(def type-gens (list (generate-arguments-from-rules type-rb '())))
+
+(def goal '(exists x (s x) (q x)))
+;(def goal '(exists x (t x) (u x ?y)))
+;(def goal '(s ?y))
+
+(def sols (construct-arguments goal
+                                  200
+                                  1
+                                  *empty-argument-graph*
+                                  (list (generate-arguments-from-rules rb '())
+                                        (generate-arguments-from-rules type-rb '())
+                                        (builtins type-gens))))
+
+(def ag1 (unite-solutions sols))
+(def ag2 (unite-solutions-with-candidates sols))
+
+(def engine (make-engine* 200
+                                 0
+                                 *empty-argument-graph*
+                                 (list (generate-arguments-from-rules rb '())
+                                       (builtins type-gens))))
+
+;(def sol2 (engine goal))
