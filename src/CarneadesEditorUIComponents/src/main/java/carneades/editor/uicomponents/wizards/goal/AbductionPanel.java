5560a0a98ddbc2325592d30aaf8172d34bbd6932
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2010-10-25 13:58:26 +0200

    goal wizard (beta)

--- src/CarneadesEditorUIComponents/src/main/java/carneades/editor/uicomponents/wizards/goal/AbductionPanel.java
+++ src/CarneadesEditorUIComponents/src/main/java/carneades/editor/uicomponents/wizards/goal/AbductionPanel.java
@@ -11,6 +11,8 @@
 
 package carneades.editor.uicomponents.wizards.goal;
 
+import javax.swing.JEditorPane;
+
 /**
  *
  * @author pal
@@ -31,72 +33,91 @@ public class AbductionPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        jPanel1 = new javax.swing.JPanel();
+        buttonsPanel = new javax.swing.JPanel();
 
-        jLabel1.setText("Positions:");
+        positionLabel.setText("Position:");
 
-        positionsList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        positionsList.setName("positions"); // NOI18N
-        jScrollPane1.setViewportView(positionsList);
+        statementsList.setName("statements"); // NOI18N
+        jScrollPane1.setViewportView(statementsList);
 
-        jLabel2.setText("Sort by:");
+        firstPositionButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/go-first.png"))); // NOI18N
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane2.setViewportView(jTextArea1);
+        previousPositionButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/go-previous.png"))); // NOI18N
+
+        nextPositionButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/go-next.png"))); // NOI18N
+
+        lastPositionButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/go-last.png"))); // NOI18N
+
+        javax.swing.GroupLayout buttonsPanelLayout = new javax.swing.GroupLayout(buttonsPanel);
+        buttonsPanel.setLayout(buttonsPanelLayout);
+        buttonsPanelLayout.setHorizontalGroup(
+            buttonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buttonsPanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(firstPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(previousPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(nextPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lastPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        buttonsPanelLayout.setVerticalGroup(
+            buttonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buttonsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(buttonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(firstPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, Short.MAX_VALUE)
+                    .addComponent(lastPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, Short.MAX_VALUE)
+                    .addComponent(previousPositionButton, 0, 0, Short.MAX_VALUE)
+                    .addComponent(nextPositionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+
+        jPanel1.add(buttonsPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 421, Short.MAX_VALUE)
-                        .addContainerGap())
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(positionLabel, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 321, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addContainerGap())
-                            .addComponent(jComboBox1, 0, 100, Short.MAX_VALUE)))))
+                        .addGap(14, 14, 14)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 401, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
+                .addComponent(positionLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel buttonsPanel;
+    public final javax.swing.JButton firstPositionButton = new javax.swing.JButton();
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    public final javax.swing.JList positionsList = new javax.swing.JList();
+    public final javax.swing.JButton lastPositionButton = new javax.swing.JButton();
+    public final javax.swing.JButton nextPositionButton = new javax.swing.JButton();
+    public final javax.swing.JLabel positionLabel = new javax.swing.JLabel();
+    public final javax.swing.JButton previousPositionButton = new javax.swing.JButton();
+    public final javax.swing.JList statementsList = new javax.swing.JList();
     // End of variables declaration//GEN-END:variables
 
      // our modifications:
--- src/CarneadesEditorUIComponents/src/main/java/carneades/editor/uicomponents/wizards/goal/ProponentPanel.java
+++ src/CarneadesEditorUIComponents/src/main/java/carneades/editor/uicomponents/wizards/goal/ProponentPanel.java
@@ -31,11 +31,15 @@ public class ProponentPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        pointOfView = new javax.swing.ButtonGroup();
+        positiveNegativeGroup = new javax.swing.ButtonGroup();
+        inOutGroup = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        inRadioButton = new javax.swing.JRadioButton();
+        outRadioButton = new javax.swing.JRadioButton();
+        positiveRadioButton = new javax.swing.JRadioButton();
+        negativeRadioButton = new javax.swing.JRadioButton();
+        jLabel3 = new javax.swing.JLabel();
 
         jLabel1.setText("The main issue is:");
 
@@ -48,15 +52,27 @@ public class ProponentPanel extends javax.swing.JPanel {
         mainIssueTextArea.setMinimumSize(new java.awt.Dimension(0, 50));
         mainIssueTextArea.setPreferredSize(new java.awt.Dimension(260, 100));
 
-        jLabel2.setText("What is your point of view? I am a...");
+        jLabel2.setText("I want the...");
 
-        pointOfView.add(jRadioButton1);
-        jRadioButton1.setText("Proponent");
-        jRadioButton1.setName("proponent"); // NOI18N
+        inOutGroup.add(inRadioButton);
+        inRadioButton.setSelected(true);
+        inRadioButton.setText("in");
+        inRadioButton.setName("in"); // NOI18N
 
-        pointOfView.add(jRadioButton2);
-        jRadioButton2.setText("Opponent");
-        jRadioButton2.setName("opponent"); // NOI18N
+        inOutGroup.add(outRadioButton);
+        outRadioButton.setText("out");
+        outRadioButton.setName("out"); // NOI18N
+
+        positiveNegativeGroup.add(positiveRadioButton);
+        positiveRadioButton.setSelected(true);
+        positiveRadioButton.setText("positive");
+        positiveRadioButton.setName("positive"); // NOI18N
+
+        positiveNegativeGroup.add(negativeRadioButton);
+        negativeRadioButton.setText("negative");
+        negativeRadioButton.setName("negative"); // NOI18N
+
+        jLabel3.setText("to be...");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -66,20 +82,27 @@ public class ProponentPanel extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(mainIssueTextArea, javax.swing.GroupLayout.DEFAULT_SIZE, 381, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
-                        .addContainerGap(274, Short.MAX_VALUE))
+                        .addContainerGap(279, Short.MAX_VALUE))
+                    .addComponent(jLabel2)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(mainIssueTextArea, javax.swing.GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
-                        .addGap(152, 152, 152))
+                        .addComponent(positiveRadioButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(negativeRadioButton)
+                        .addContainerGap(235, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addContainerGap(164, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jRadioButton1)
-                        .addContainerGap(298, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jRadioButton2)
-                        .addContainerGap(301, Short.MAX_VALUE))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(inRadioButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(outRadioButton))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(307, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -90,22 +113,32 @@ public class ProponentPanel extends javax.swing.JPanel {
                 .addComponent(mainIssueTextArea, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jRadioButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jRadioButton2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(11, 11, 11)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(positiveRadioButton)
+                    .addComponent(negativeRadioButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel3)
+                .addGap(4, 4, 4)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(outRadioButton)
+                    .addComponent(inRadioButton))
+                .addContainerGap(64, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup inOutGroup;
+    private javax.swing.JRadioButton inRadioButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JLabel jLabel3;
     public final javax.swing.JTextArea mainIssueTextArea = new javax.swing.JTextArea();
-    private javax.swing.ButtonGroup pointOfView;
+    private javax.swing.JRadioButton negativeRadioButton;
+    private javax.swing.JRadioButton outRadioButton;
+    private javax.swing.ButtonGroup positiveNegativeGroup;
+    private javax.swing.JRadioButton positiveRadioButton;
     // End of variables declaration//GEN-END:variables
 
     // our modifications:
