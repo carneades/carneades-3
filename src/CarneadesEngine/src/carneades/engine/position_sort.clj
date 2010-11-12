ced37144bd497e5612be4471636ca6137f82b7d1
Stefan Ballnat <stefan.ballnat@fokus.fraunhofer.de>
2010-11-12 14:12:00 +0100

    - added position sorting library - added minimality function to abduction
    Signed-off-by: Stefan Ballnat <stefan.ballnat@fokus.fraunhofer.de>

--- src/CarneadesEngine/src/carneades/engine/position_sort.clj
+++ src/CarneadesEngine/src/carneades/engine/position_sort.clj
@@ -14,15 +14,27 @@
     (let [sum (apply + (map 
                          (fn [s]
                            (let [n (get-node ag s)]
-                             ;(println "depth of " s (depth-in ag n))
                              (depth-in ag n)))
                          p))]
-      ;(println "getting position depth" p sum)
-      sum)
-    ))
+      sum)))
+
+(defn- position-height
+  [ag]
+  (fn [p]
+    (let [sum (apply + (map
+                         (fn [s]
+                           (let [n (get-node ag s)]
+                             (height-in ag n)))
+                         p))]
+      sum)))
 
 (defn sort-by-depth
   [ag]
   (fn [label]
     (sort-by (position-depth ag) label)))
+
+(defn sort-by-height
+  [ag]
+  (fn [label]
+    (sort-by (position-height ag) label)))
 
