af25d21849a4438c7e8e66c3fe4b2593d96d8a1b
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2010-11-29 11:24:57 +0100

    logging of handlers operations to a file

--- src/CarneadesEditor/src/carneades/editor/core.clj
+++ src/CarneadesEditor/src/carneades/editor/core.clj
@@ -7,15 +7,23 @@
         carneades.editor.controller.listeners.register)
   (:gen-class))
 
+(defn- log [logging]
+  (when logging
+    (prn "LOGGING activated")
+    (let [date (.format (java.text.SimpleDateFormat. "yyyyMMdd-HHmmss")
+                        (java.util.Date.))
+          stream (java.io.FileWriter. (str "carneades-log-" date ".txt"))]
+      (alter-var-root #'*out* (fn [_] stream))
+      (alter-var-root #'*err* (fn [_] stream)))
+    ))
+
 (defn start []
   (prn "Starting the Carneades Editor...")
+  (log true)
   (let [view (create-swingview)]
     (init view)
     (register-listeners view)
     (show view)))
-
-;; (alter-var-root #'*out* (fn [_] *out*))
-;; (alter-var-root #'*err* (fn [_] *err*))
 
 (defn -main [& args]
   (start))
