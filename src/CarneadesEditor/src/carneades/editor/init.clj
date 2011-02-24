262b9a574804ec2730a2a6da41f5f49a034667db
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-02-24 15:58:57 +0100

    third attempt for Mac OS X

--- src/CarneadesEditor/src/carneades/editor/init.clj
+++ src/CarneadesEditor/src/carneades/editor/init.clj
@@ -1,20 +1,19 @@
 ;;; Copyright © 2010 Fraunhofer Gesellschaft 
 ;;; Licensed under the EUPL V.1.1
 
-(ns carneades.editor.init
-  (:use clojure.contrib.def)
-  ;; use seems to cause a side-effect? making
-  ;; the setted property ineffective, so we use require:
-  (:require [carneades.editor.utils.swing :as swing]))
-
 ;;; import this file first to ensure this code is executed
 ;;; before any other
 
+;; this needs to be runned before the :use of swing
+(System/setProperty "com.apple.mrj.application.apple.menu.about.name"
+                    "Carneades Editor")
+
+(ns carneades.editor.init
+  (:use clojure.contrib.def
+        carneades.editor.utils.swing))
+
 (defvar- *runonce*
   (do
-    (System/setProperty "com.apple.mrj.application.apple.menu.about.name"
-                        "Carneades Editor")
-    (swing/set-look-and-feel "Nimbus")
     ;; works only with native Mac L&F:
-    ;; (System/setProperty "apple.laf.useScreenMenuBar" "true")
-    ))
+    ;; (System/setProperty "apple.laf.useScreenMenuBar" "true")    
+    (set-look-and-feel "Nimbus")))
