09c0637416b16d23450e045a3681704d1b23484a
Stefan Ballnat <stefan.ballnat@fokus.fraunhofer.de>
2011-02-25 11:05:28 +0100

    namespace class added
    Signed-off-by: Stefan Ballnat <stefan.ballnat@fokus.fraunhofer.de>

---
+++ src/CarneadesWS/CarneadesWS-ejb/src/main/java/org/fokus/carneades/NS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.fokus.carneades;
+
+/**
+ *
+ * @author stb
+ */
+public class NS {
+    
+    public static final String CORE = "clojure.core";
+    public static final String MONADS = "clojure.contrib.monads";
+    public static final String STACKTRACE = "clojure.stacktrace";
+    
+    public static final String LKIF = "carneades.engine.lkif";
+    public static final String SHELL = "carneades.engine.shell";
+    public static final String ARGUMENT = "carneades.engine.argument";
+    public static final String ASK = "carneades.engine.ask";    
+    public static final String UNIFY = "carneades.engine.unify";
+
+}
