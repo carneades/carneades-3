5b828d3a77f1db0cf0b5a6d7d7cd95c1f1600763
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-03-31 10:42:21 +0200

    upgrade version to 1.0.0

--- src/CarneadesEditor/project.clj
+++ src/CarneadesEditor/project.clj
@@ -1,7 +1,7 @@
 ;;; Copyright © 2010 Fraunhofer Gesellschaft 
 ;;; Licensed under the EUPL V.1.1
 
-(defproject carneades-editor "0.9.6-SNAPSHOT"
+(defproject carneades-editor "1.0.0-SNAPSHOT"
   :description "Carneades is an argument mapping application, with a graphical user interface, and a software library for building applications supporting various argumentation tasks. This is the graphical editor."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -15,5 +15,5 @@
   ;; to create a self-executing JAR comment {:skip-aot true}
   ;; then invoke "cake uberjar" and pray.
   ;; "lein uberjar" does not work because of a bug.
-  :main ^{:skip-aot true}
+  :main ;; ^{:skip-aot true}
   carneades.editor.core)
