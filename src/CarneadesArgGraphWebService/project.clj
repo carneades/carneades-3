bc7404d93b8da21b6b361e64bf0d6aac438fb28d
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2011-09-26 17:07:37 +0200

    Created a CarneadesArgGraphWebService project and began work.

---
+++ src/CarneadesArgGraphWebService/project.clj
@@ -0,0 +1,12 @@
+;;; Copyright (C) 2011 Fraunhofer Gesellschaft 
+;;; Licensed under the EUPL V.1.1
+
+(defproject CarneadesArgGraphWebService "0.1.0"
+  :description "A RESTful Web service for Carneades argument graphs"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.h2database/h2 "1.3.160"]
+                 [org.clojure/java.jdbc "0.0.6"]])
+
+
+            
---
+++ src/CarneadesArgGraphWebService/src/carneades/agws/core.clj
@@ -0,0 +1,1 @@
+(ns carneades.agws.core)
