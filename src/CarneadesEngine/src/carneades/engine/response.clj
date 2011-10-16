6b8d3d1d14055feea00195f144af35307cc8f99f
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2011-10-16 12:31:48 +0200

    Bug fixes in the new argument construction module.

--- src/CarneadesEngine/src/carneades/engine/response.clj
+++ src/CarneadesEngine/src/carneades/engine/response.clj
@@ -1,4 +1,17 @@
-(ns carneades.engine.response)
+(ns 
+  ^{:doc "Defines the structure to be returned by argument generators.
+          An argument generator is a function of the type:
+          
+          statement substitutions -> (seq-of response)
+          
+          The arguments returned by a generator need not be fully
+          instantiated by the substitutions. The argument construction
+          module will create goals for the premises of the argument
+          which are not ground."}
+  
+  carneades.engine.response)
+
+; type generator: statement substitutions -> (seq-of response)
 
 (defrecord response
   [substitutions   ; (term -> term) map
