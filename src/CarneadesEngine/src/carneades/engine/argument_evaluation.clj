7251aa2167ff03444cfd180265721cc48ab69e84
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2011-10-26 19:44:31 +0200

    Changed statement labels to integers in the range of 0.0 to 1.0

--- src/CarneadesEngine/src/carneades/engine/argument_evaluation.clj
+++ src/CarneadesEngine/src/carneades/engine/argument_evaluation.clj
@@ -3,10 +3,9 @@
   (:use carneades.engine.statement
         carneades.engine.argument-graph))
 
-(defn label?
-  "The set of argument and statement labels is {:in, :out, :undecided}."
-  [x]
-  (contains? #{:in, :out, :undecided} x))
+; type label = integer in the range of 0.0 ... 1.0
+; where 1.0 means "in", 0.0 means "out" and anything in between 
+; means "undecided".  (Thanks to Trevor for this idea.)
 
 (defrecord Evaluation 
   [statement-labels   ; (symbol -> label) map, where the keys are statement node ids
