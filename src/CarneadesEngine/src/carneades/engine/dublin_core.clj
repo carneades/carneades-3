3e3230a8c07ce1816b032829c9446b37c57acd19
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2011-11-17 10:18:35 +0100

    Continue work on the database; Simplified the Dublin core metadata attributes to be strings, rather than sequences of strings.
    Dublin Core requires each attribute to have multiple values.  The
    values are now separated by semi-colons in the string.  I expect this
    will be more practical and easier in typical use cases.

--- src/CarneadesEngine/src/carneades/engine/dublin_core.clj
+++ src/CarneadesEngine/src/carneades/engine/dublin_core.clj
@@ -3,41 +3,50 @@
 ; type language = :en | :de | :nl | :fr ...
 
 ; A Source record describes a source using the Dublin Core Metadata Element Set.
-; As each Dublin Core element may have multiple values, they are represented as vectors
+; As each Dublin Core element may have multiple values.  In our
+; encoding, all values of a property are in single string, separated by
+; semicolons. The single exception is the description property, which
+; is represented as a map from a keyword for some language (:en, :de, etc.) to 
+; a description in this language.
 
 (defrecord Metadata
-  [contributor      ; vector of strings 
-   coverage         ; vector of strings
-   creator          ; vector of strings
-   date             ; vector of strings
-   description      ; vector of (language -> string) maps
-   format           ; vector of strings
-   identifier       ; vector of strings
-   publisher        ; vector of strings
-   relation         ; vector of strings
-   rights           ; vector of strings
-   source           ; vector of strings (ids of sources from which this source is derived)
-   subject          ; vector of strings
-   title            ; vector of strings
-   type])           ; vector of strings
+  [contributor      ; string 
+   coverage         ; string 
+   creator          ; string 
+   date             ; string 
+   description      ; (language -> string) map 
+   format           ; string 
+   identifier       ; string 
+   language         ; string 
+   publisher        ; string 
+   relation         ; string 
+   rights           ; string 
+   source           ; string 
+   subject          ; string 
+   title            ; string 
+   type])           ; string 
  
 (defn make-metadata
   [& values]
   (merge 
     (Metadata. 
-      []   ; contributor
-      []   ; coverage
-      []   ; creator 
-      []   ; date
-      []   ; description 
-      []   ; format
-      []   ; identifier
-      []   ; publisher 
-      []   ; relation
-      []   ; rights
-      []   ; source 
-      []   ; subject
-      []   ; title 
-      [])  ; type
+      ""   ; contributor
+      ""   ; coverage
+      ""   ; creator 
+      ""   ; date
+      {}   ; description 
+      ""   ; format
+      ""   ; identifier
+      ""   ; language
+      ""   ; publisher 
+      ""   ; relation
+      ""   ; rights
+      ""   ; source 
+      ""   ; subject
+      ""   ; title 
+      "")  ; type
     (apply hash-map values)))
+
+(defn metadata? [x] (instance? Metadata x))
+
 
