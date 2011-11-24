48fe0a57d90e9f2fcdf34647504df342e018979f
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2011-11-24 15:25:56 +0100

    Continued work on the web service.
    A first complete implementation of the web service is just about
    finished, but still needs to be thoroughly tested.  The main open task
    is to handle user authentication and also public, read-only access
    without authentication.

--- src/CarneadesWebService/src/carneades/web/service.clj
+++ src/CarneadesWebService/src/carneades/web/service.clj
@@ -6,10 +6,22 @@
          clojure.pprint
          compojure.core
          carneades.engine.statement
+         carneades.engine.argument
+         carneades.engine.dublin-core
          carneades.database.db)
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
             [clojure.java.jdbc :as jdbc]))
+
+(defmacro with-db [db & body]   
+  ; To Do: Exception Handling
+  `(try (jdbc/with-connection 
+           ~db
+           (jdbc/transaction ~@body))
+        (catch Exception e 
+               {:status 500
+                 :body "The server encountered an unexpected condition 
+                        which prevented it from fulfilling the request."})))
 
 (defn json-response [data & [status]]
   (if (nil? data)
@@ -25,10 +37,11 @@
         (statement? stmt) (merge stmt {:atom (str (:atom stmt))}),
         :else nil))
 
-(defn- unpack-statement
-  [m]
-  (assoc (map->statement m) 
-         :atom (read-string (:atom m))))
+(defn- unpack-statement [s]
+   (cond (string? s) (read-string s),
+         (map? s) (assoc (map->statement s) 
+                    :atom (read-string (:atom s))),
+        :else nil))
 
 (defn- pack-argument
   [arg]
@@ -36,65 +49,200 @@
     nil
     (merge arg
            {:conclusion (pack-statement (:conclusion arg)),
-            :premises (map (fn [p] (assoc p :statement  (pack-statement (:statement p))))
+            :premises (map (fn [p] (assoc p :statement 
+                                          (pack-statement (:statement p))))
                            (:premises arg))})))
 
+(defn- unpack-argument [arg]
+  (assoc arg
+         :conclusion (unpack-statement (:conclusion arg))
+         :premises (map (fn [p] (assoc p :statement (unpack-statement (:statement p))))
+                        (:premises arg))))
+
 ;; To Do (CRITICAL): handle database exceptions to avoid printing of database password
-;; To Do: wrap the delete and other destructive operations in transactions.
 ;; To Do: commands for logging into and creating databases
 ;; To Do: make all read operations public, without login, at least optionally
-;; To Do: retrieving passwords from cookies
+;; To Do: retrieving passwords 
 ;; To Do: include the ids of undercutters and rebuttals in argument records
-;; To Do: search operations
+;; To Do: search operations, including full text search
 ;; To Do: LKIF import/export
 ;; To Do: OPML export
-;; To Do: review security issues and validate input
+;; To Do: review security issues 
+;; To Do: validate input
   
 (defroutes handlers
+
   ;; Metadata         
+
   (GET "/metadata/:db" [db] 
-       (json-response (list-metadata (make-db db "pw1"))))
-  (GET "/metadata/:db/:id" [db id] 
-       (json-response (read-metadata (make-db db "pw1") (read-string id))))
-  (POST "/metadata" {params :params}  (json-response "Not Yet Implemented")) 
-  (PUT "/metadata" {params :params}  (json-response "Not Yet Implemented"))  
+    (let [db2 (make-db db "pw1")]
+      (with-db db2 (json-response (list-metadata db2)))))
+           
+  (GET "/metadata/:db/:id" [db id]
+    (let [db2 (make-db db "pw1")]
+      (with-db db2 (json-response (read-metadata db2 (read-string id))))))
+           
+  (POST "/metadata" request
+    (let [m (read-json (slurp (:body request)))
+          db (make-db (:db (:params request)) "pw1")]
+      (with-db db (json-response (create-metadata db (map->metadata db m)))))) 
+            
+  (PUT "/metadata" request   
+    (let [m (read-json (slurp (:body request)))
+          db (make-db (:db (:params request)) "pw1")
+          id (read-string (:id (:params request)))]
+      (with-db db (json-response (update-metadata db id m))))) 
+           
   (DELETE "/metadata/:db/:id" [db id] 
-          (json-response (delete-metadata (make-db db "pw1") (read-string id))))
+    (let [db2 (make-db db "pw1")]
+      (with-db db2 (json-response (delete-metadata db2 (read-string id))))))
                  
   ;; Statements
+           
   (GET "/statement/:db" [db] 
-       (json-response (map pack-statement (list-statements (make-db db "pw1")))))
+    (let [db2 (make-db db "pw1")]
+      (with-db db2 (json-response (map pack-statement (list-statements db2))))))           
+  
   (GET "/statement/:db/:id" [db id] 
-       (json-response (pack-statement (read-statement (make-db db "pw1") 
-                                                      (read-string id)))))
+    (let [db2 (make-db db "pw1")]
+       (with-db db2 
+         (json-response (pack-statement (read-statement db2 (read-string id)))))))
            
   (POST "/statement/:db" request  
-        (let [m (read-json (slurp (:body request)))
-              s (unpack-statement m)]
-          (let [db (make-db (:db (:params request)) "pw1")]
-            (jdbc/with-connection 
-              db
-              (jdbc/transaction
-                (json-response (create-statement db s)))))))
-
-  (PUT "/statement" {params :params}  (json-response "Not Yet Implemented"))  
-  (DELETE "/statement/:db/:id" [db id] 
-          (json-response (delete-statement (make-db db "pw1") (read-string id))))
-                    
-  ;; Arguments     
-  (GET "/argument/:db" [db] 
-       (json-response (map pack-argument (list-arguments (make-db db "pw1")))))
-  (GET "/argument/:db/:id" [db id] 
-       (json-response (pack-argument (read-argument (make-db db "pw1") (read-string id)))))
-  (POST "/argument" {params :params}  (json-response "Not Yet Implemented"))
-  (PUT "/argument" {params :params}  (json-response "Not Yet Implemented"))  
-  (DELETE "/argument/:db/:id" [db id] 
-          (json-response (delete-argument (make-db db "pw1") (read-string id))))
+    (let [m (read-json (slurp (:body request)))
+          s (unpack-statement m)
+          db (make-db (:db (:params request)) "pw1")]
+       (prn request)
+       (with-db db (json-response (create-statement db (map->statement s))))))
            
-  ;; Other         
-  (GET "/" [] "<h1>Carneades Web Service</h1>")
+  (PUT "/statement" request  
+    (let [m (read-json (slurp (:body request)))
+          s (unpack-statement m)
+          db (make-db (:db (:params request)) "pw1")
+          id (read-string (:id (:params request)))]
+       (with-db db (json-response (update-statement db id s)))))        
+            
+  (DELETE "/statement/:db/:id" [db id] 
+    (let [db2 (make-db db "pw1")]
+      (with-db db2 (json-response (delete-statement db2 (read-string id))))))
+                    
+  ;; Arguments  
+              
+  (GET "/argument/:db" [db]
+    (let [db2 (make-db db "pw1")] 
+      (with-db db2 (json-response (map pack-argument (list-arguments db2))))))
+       
+  (GET "/argument/:db/:id" [db id]
+    (let [db2 (make-db db "pw1")]  
+      (with-db db2 (json-response (pack-argument (read-argument db2 (read-string id)))))))
+           
+  (POST "/argument" request  
+    (let [m (read-json (slurp (:body request)))
+          arg (unpack-argument m)
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (create-argument db (map->argument arg))))))
+           
+  (PUT "/argument" request  
+    (let [m (read-json (slurp (:body request)))
+          arg (unpack-argument m)
+          db (make-db (:db (:params request)) "pw1")
+          id (read-string (:id (:params request)))]
+       (with-db db (json-response (update-argument db id arg)))))   
+           
+  (DELETE "/argument/:db/:id" [db id] 
+    (let [db2 (make-db db "pw1")]
+      (with-db db2
+        (json-response (delete-argument db2 (read-string id))))))
+           
+  ;; Namespaces
+           
+  (GET "/namespace/:db" [db]
+    (let [db2 (make-db db "pw1")] 
+      (with-db db2 (json-response (list-namespaces db2)))))
+  
+  (GET "/namespace/:db/:prefix" [db prefix]
+    (let [db2 (make-db db "pw1")]  
+      (with-db db2 (json-response (read-namespace db2 prefix)))))
+
+  (POST "/namespace/" request  
+    (let [prefix (:prefix (:params request)),
+          uri (read-json (slurp (:body request))),
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (create-namespace db prefix uri)))))
+           
+  (PUT "/namespace/" request  
+    (let [prefix (:prefix (:params request)),
+          uri (read-json (slurp (:body request))),
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (update-namespace db prefix uri)))))
+           
+  (DELETE "/namespace/:db/:prefix" [db prefix] 
+    (let [db2 (make-db db "pw1")]
+      (with-db db2
+        (json-response (delete-namespace db2 prefix)))))
+           
+  ;; Statement Polls
+           
+  (GET "/statement-poll/:db" [db]
+    (let [db2 (make-db db "pw1")] 
+      (with-db db2 (json-response (list-statement-poll db2)))))
+  
+  (GET "/statement-poll/:db/:id" [db id]
+    (let [db2 (make-db db "pw1")]  
+      (with-db db2 (json-response (read-statement-poll db2 (read-string id))))))
+
+  (POST "/statement-poll" request  
+    (let [userid (:id (:params request)),
+          votes (read-json (slurp (:body request))),
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (create-statement-poll db userid votes)))))
+           
+  (PUT "/statement-poll" request  
+    (let [userid (:id (:params request)),
+          votes (read-json (slurp (:body request)))
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (update-statement-poll db userid votes)))))
+           
+  (DELETE "/statement-poll/:db/:id" [db id] 
+    (let [db2 (make-db db "pw1")]
+      (with-db db2
+        (json-response (delete-statement-poll db2 (read-string id)))))) 
+                     
+  ;; Argument Polls
+           
+  (GET "/argument-poll/:db" [db]
+    (let [db2 (make-db db "pw1")] 
+      (with-db db2 (json-response (list-argument-poll db2)))))
+  
+  (GET "/argument-poll/:db/:id" [db id]
+    (let [db2 (make-db db "pw1")]  
+      (with-db db2 (json-response (read-argument-poll db2 (read-string id))))))
+
+  (POST "/argument-poll" request  
+    (let [userid (:id (:params request)),
+          votes (read-json (slurp (:body request))),
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (create-argument-poll db userid votes)))))
+           
+  (PUT "/argument-poll" request  
+    (let [userid (:id (:params request)),
+          votes (read-json (slurp (:body request)))
+          db (make-db (:db (:params request)) "pw1")]
+       (with-db db (json-response (update-argument-poll db userid votes)))))
+           
+  (DELETE "/argument-poll/:db/:id" [db id] 
+    (let [db2 (make-db db "pw1")]
+      (with-db db2
+        (json-response (delete-argument-poll db2 (read-string id)))))) 
+           
+  ;; Other 
+                   
+  (GET "/" [] 
+    "<h1>Carneades Web Service</h1>
+     <p>This web service is part of the <a href=\"http://carneades.github.com\">
+     Carneades Argumentation System</a></p>")                                                                      
   (route/resources "/")
-  (route/not-found "Page not found"))
+  (route/not-found "Page not found."))
 
 (def carneades-web-service
   (handler/site handlers))
