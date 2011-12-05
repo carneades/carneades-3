8a058a9471969db2b9094b8d6af9086e3f15f6c3
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-05 16:00:56 +0100

    more templating for the webapp

--- src/CarneadesWebApplication/resources/public/js/app/login.js
+++ src/CarneadesWebApplication/resources/public/js/app/login.js
@@ -15,7 +15,43 @@ function display_argument(argid)
     ajax_get('/argument/' + CARNEADES.database + '/' + argid,
             function(argument_data) {
                 argument_data.direction = argument_data.pro ? "pro" : "con";
-                $('body').replaceWith(ich.argument(argument_data));
+                var argument_html = ich.argument(argument_data);
+                $('body').html(argument_html.filter('#argument'));
+
+                add_statements_handlers(get_statements(argument_data));
             });
 }
 
+
+// Returns the statements of the premises and of the conclusion
+function get_statements(argument_data)
+{
+    var statements = [];
+    $.each(argument_data.premises, 
+           function(index, premise) {
+               statements.push(premise.statement);
+           });
+    statements.push(argument_data.conclusion);
+    return statements;
+}
+
+
+function add_statements_handlers(statements)
+{
+    $.each(statements, function(index, statement) {
+               $('#statement' + statement.id).click(
+                   function(obj) {
+                       display_statement(statement.id);
+                   });
+           }
+          );
+}
+
+function display_statement(id)
+{
+    ajax_get('/statement/' + CARNEADES.database + '/' + id,
+            function(statement_data) {
+                var statement_html = ich.statement(statement_data);
+                $('body').html(statement_html.filter('#statement'));
+            });
+}
