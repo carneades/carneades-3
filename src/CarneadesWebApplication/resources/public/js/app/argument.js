eba8cdb835b3f1de40e12ba4bbb2178b0f76916e
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-05 18:52:48 +0100

    use of links in the argument page

--- src/CarneadesWebApplication/resources/public/js/app/argument.js
+++ src/CarneadesWebApplication/resources/public/js/app/argument.js
@@ -14,34 +14,8 @@ function display_argument(db, argid)
     ajax_get(argument_url(db, argid),
             function(argument_data) {
                 argument_data.direction = argument_data.pro ? "pro" : "con";
+                argument_data.db = db;
                 var argument_html = ich.argument(argument_data);
                 $('body').html(argument_html.filter('#argument'));
-
-                add_statements_handlers(db, get_statements(argument_data));
             });
-}
-
-
-// Returns the statements of the premises and of the conclusion
-function get_statements(argument_data)
-{
-    var statements = [];
-    $.each(argument_data.premises, 
-           function(index, premise) {
-               statements.push(premise.statement);
-           });
-    statements.push(argument_data.conclusion);
-    return statements;
-}
-
-
-function add_statements_handlers(db, statements)
-{
-    $.each(statements, function(index, statement) {
-               $('#statement' + statement.id).click(
-                   function(obj) {
-                       set_statement_url(db, statement.id);
-                   });
-           }
-          );
 }
