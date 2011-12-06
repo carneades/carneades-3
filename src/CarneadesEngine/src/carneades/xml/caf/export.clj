914e63d5fd97b35f1225507f9a2b47ea332d9d8f
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2011-12-06 14:56:17 +0100

    In the CAF export module, we now check for "blank" strings
    instead of just empty ones.

--- src/CarneadesEngine/src/carneades/xml/caf/export.clj
+++ src/CarneadesEngine/src/carneades/xml/caf/export.clj
@@ -3,7 +3,8 @@
 
 (ns ^{:doc "Functions for exporting argument graphs to XML using the Carneades Argument Format (CAF)."}
     carneades.xml.caf.export
-  (:use carneades.engine.statement
+  (:use clojure.string
+        carneades.engine.statement
         carneades.engine.argument-graph
         clojure.contrib.prxml))
 
@@ -12,44 +13,27 @@
 ;   CAF could be extended with elements for the namespaces.  Perhaps another name should be used, to 
 ;   avoid confusion with XML namespaces, which are at another level.
 
-(defrecord Metadata
-  [contributor      ; string or nil 
-   coverage         ; string or nil 
-   creator          ; string or nil 
-   date             ; string or nil 
-   description      ; (language -> string) map or nil
-   format           ; string or nil 
-   identifier       ; string or nil 
-   language         ; string or nil 
-   publisher        ; string or nil 
-   relation         ; string or nil 
-   rights           ; string or nil 
-   source           ; string or nil 
-   subject          ; string or nil 
-   title            ; string or nil 
-   type])           ; string or nil 
-
 (defn- metadata->xml
   [md]
   [:metadata 
    (dissoc md 
            :description
-           (if (empty? (:contributor md)) :contributor)
-           (if (empty? (:coverage md)) :coverage)
-           (if (empty? (:creator md)) :creator)
-           (if (empty? (:date md)) :date)
-           (if (empty? (:format md)) :format)
-           (if (empty? (:identifier md)) :identifier)
-           (if (empty? (:language md)) :language)
-           (if (empty? (:publisher md)) :publisher)
-           (if (empty? (:relation md)) :relation)
-           (if (empty? (:rights md)) :rights)
-           (if (empty? (:source md)) :source)
-           (if (empty? (:subject md)) :subject)
-           (if (empty? (:title md)) :title)
-           (if (empty? (:type md)) :type))
+           (when (blank? (:contributor md)) :contributor)
+           (when (blank? (:coverage md)) :coverage)
+           (when (blank? (:creator md)) :creator)
+           (when (blank? (:date md)) :date)
+           (when (blank? (:format md)) :format)
+           (when (blank? (:identifier md)) :identifier)
+           (when (blank? (:language md)) :language)
+           (when (blank? (:publisher md)) :publisher)
+           (when (blank? (:relation md)) :relation)
+           (when (blank? (:rights md)) :rights)
+           (when (blank? (:source md)) :source)
+           (when (blank? (:subject md)) :subject)
+           (when (blank? (:title md)) :title)
+           (when (blank? (:type md)) :type))
    (reduce (fn [v description]
-             (if (empty? (second description)) 
+             (if (blank? (second description)) 
                v
                (conj v [:description 
                         {:lang (name (first description))} 
@@ -77,13 +61,13 @@
   (reduce (fn [v stmt] 
             (conj v [:statement (-> stmt 
                                     (dissoc :text :header :premise-of :pro :con
-                                            (if (empty? (:value stmt)) :value)
-                                            (if (empty? (:weight stmt)) :weight))
+                                            (when (nil? (:value stmt)) :value)
+                                            (when (nil? (:weight stmt)) :weight))
                                     (assoc :standard (standard->string (:standard stmt)))
                                     (assoc :atom (pack-atom (:atom stmt))))
-                     (metadata->xml (:header stmt))
+                     (if (nil? (:header stmt)) "" (metadata->xml (:header stmt)))
                      (reduce (fn [v description]
-                               (if (empty? (second description)) 
+                               (if (blank? (second description)) 
                                  v
                                  (conj v [:description 
                                           {:lang (name (first description))} 
@@ -98,9 +82,9 @@
   (reduce (fn [v arg]
             (conj v [:argument 
                      (dissoc arg :header :conclusion :premises 
-                             (if (empty? (:weight arg)) :weight)
-                             (if (empty? (:value arg)) :value))
-                     (metadata->xml (:header arg))
+                             (when (nil? (:weight arg)) :weight)
+                             (when (nil? (:value arg)) :value))
+                     (if (nil? (:header arg)) "" (metadata->xml (:header arg)))
                      [:conclusion {:statement (literal-atom (:conclusion arg))}]
                      (reduce (fn [v p] (conj v [:premise (dissoc p :id :argument)]))
                              [:premises]
@@ -121,7 +105,7 @@
      (metadata->xml (:header ag))
      (statement-nodes->xml (vals (:statement-nodes ag)))
      (argument-nodes->xml (vals (:argument-nodes ag)))
-     (references->xml (vals (:references ag)))]))
+     (references->xml (:references ag))]))
 
 
   
