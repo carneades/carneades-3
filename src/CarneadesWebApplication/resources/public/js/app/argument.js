fe71d1da24f7b39f4cf10fca1e6d1b67bc78f488
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-06 14:53:33 +0100

    metadata partial template for webapp

--- src/CarneadesWebApplication/resources/public/js/app/argument.js
+++ src/CarneadesWebApplication/resources/public/js/app/argument.js
@@ -15,7 +15,18 @@ function display_argument(db, argid)
             function(argument_data) {
                 argument_data.direction = argument_data.pro ? "pro" : "con";
                 argument_data.db = db;
+                argument_data.header = {title : "One Title"};
+                argument_data.conclusion.statement_text = statement_text(argument_data.conclusion);
+                set_premises_text(argument_data);
                 var argument_html = ich.argument(argument_data);
                 $('body').html(argument_html.filter('#argument'));
             });
+}
+
+function set_premises_text(argument_data)
+{
+    $.each(argument_data.premises, 
+           function(index, premise) {
+               premise.statement.statement_text = statement_text(premise.statement);
+           });
 }
--- src/CarneadesWebApplication/resources/public/js/app/statement.js
+++ src/CarneadesWebApplication/resources/public/js/app/statement.js
@@ -13,7 +13,17 @@ function display_statement(db, stmtid)
 {
     ajax_get(statement_url(db, stmtid),
             function(statement_data) {
+                statement_data.db = db;
+                statement_data.statement_text = statement_text(statement_data);
                 var statement_html = ich.statement(statement_data);
                 $('body').html(statement_html.filter('#statement'));
             });
+}
+
+function statement_text(statement)
+{
+    if(statement.text) {
+        return statement.text[CARNEADES.lang] || statement.atom;
+    }
+    return statement.atom;
 }
