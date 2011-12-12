022f97be55693b9dd12cddc445069b1bdafa56ef
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-12 14:55:44 +0100

    improved webapp

--- src/CarneadesWebApplication/resources/public/js/app/login.js
+++ src/CarneadesWebApplication/resources/public/js/app/login.js
@@ -1,7 +1,7 @@
 
 function on_connect()
 {
-    CARNEADES.database = "aston2"; // $('input[name=database]').val();
+    CARNEADES.database = "aston4"; // $('input[name=database]').val();
     // CARNEADES.username = $('input[name=username]').val();
     // CARNEADES.password = $('input[name=password]').val();
 
