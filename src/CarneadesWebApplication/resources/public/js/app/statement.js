968ba2fb70ff1c1f7689088ff3cb3fd5ea914fcb
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-12 12:41:13 +0100

    improved the web app

--- src/CarneadesWebApplication/resources/public/js/app/statement.js
+++ src/CarneadesWebApplication/resources/public/js/app/statement.js
@@ -18,20 +18,51 @@ function fill_statement_template(statement_data)
 
 function display_statement(db, stmtid)
 {
-    ajax_get(statement_url(db, stmtid),
-            function(statement_data) {
-                statement_data.db = db;
-                
-                if(statement_data.header) {
-                    ajax_get('/metadata/' + db + '/' + statement_data.header,
-                             function(metadata) {    
-                                 statement_data.metadata_text = format_metadata(metadata);
-                                 fill_statement_template(statement_data);
-                             });
-                } else {
-                    fill_statement_template(statement_data);                    
-                }
-            });
+    ajax_get('/statement-info/' + db + '/' + stmtid,
+             function(info) {
+                 info.db = db;
+                 set_statement_title_text(info);
+                 set_description_text(info);
+                 set_procon_texts(info);                            
+                 fill_statement_template(info);
+             });
+}
+
+function set_description_text(info)
+{
+    if(info.header) {
+        info.description_text = info.header.description ? info.header.description['en'] : "";        
+    } else {
+        info.description_text = "";
+    }
+}
+
+function set_statement_title_text(info)
+{
+    var default_text = "Statement";
+    if(info.header) {
+        info.statement_title_text = info.header.title ? info.header.title['en'] : default_text;
+    } else {
+        info.statement_title_text = default_text;
+    }
+}
+
+function set_arg_texts(info, direction)
+{
+    $.each(info[direction], 
+           function(index, metadata) {
+               var text = format_metadata(metadata);
+               text = text.length == 0 ? "Argument" : text;
+               info[direction][index].argument_text = text;
+               info[direction][index].id = info.pro[index]; // used by the template to create the ahref
+           });
+}
+
+function set_procon_texts(info)
+{
+    set_arg_texts(info, 'pro_metadata');
+    set_arg_texts(info, 'con_metadata');
+
 }
 
 function statement_text(statement)
