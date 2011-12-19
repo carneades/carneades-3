a8c50591baad1c042c9cb629ea3e2db53bb91c72
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-19 17:06:04 +0100

    port of the svg map export to the new engine (evaluation not working)

--- src/CarneadesExamples/project.clj
+++ src/CarneadesExamples/project.clj
@@ -5,8 +5,6 @@
   :description "Carneades is an argument mapping application, with a graphical 
 user interface, and a software library for building applications supporting 
 various argumentation tasks. These are some examples."
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [carneades-engine "1.0.0-SNAPSHOT"]
-                 [carneades-mapcomponent "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+                 [carneades-engine "1.0.0-SNAPSHOT"]])
