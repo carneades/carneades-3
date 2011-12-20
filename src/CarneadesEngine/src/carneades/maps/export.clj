9036f647395fb7d50c9c46f46c329b532b449e71
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-12-20 11:59:06 +0100

    added missing svg export files

---
+++ src/CarneadesEngine/src/carneades/maps/export.clj
@@ -0,0 +1,193 @@
+;;; Copyright © 2010-2011 Fraunhofer Gesellschaft
+;;; Licensed under the EUPL V.1.1
+
+(ns carneades.maps.export
+  (:use clojure.pprint
+        carneades.engine.argument-graph
+        carneades.engine.argument-evaluation
+        carneades.engine.caes
+        lacij.graph.svg.graph
+        lacij.graph.core
+        lacij.layouts.layout
+        lacij.view.core
+        lacij.opt.annealing
+        carneades.engine.statement
+        [carneades.maps export-params format-statement subset-ag])
+  (:require [analemma.xml :as xml]
+            [analemma.svg :as svg]
+            [clojure.string :as s]
+            [tikkba.utils.dom :as dom])
+  (:import (java.io InputStreamReader ByteArrayInputStream
+                    ByteArrayOutputStream)))
+
+(defn gen-arg-id
+  [arg]
+  (keyword (str "a" (str (Math/abs (hash arg))))))
+
+(defn gen-stmt-id
+  ;; Please think twice before making changes to this function!
+  ;; some ids with special characters could not be accepted by batik
+  ;; the generation of the layout would still works but with
+  ;; a strange output
+  [stmt]
+  ;; {:pre [(do (printf "%s -> " stmt) true)]
+  ;;  :post [(do (printf "%s\n" %) true)]}
+  (let [stmtstr (statement-formatted (map->statement stmt))
+        size 50
+        s (if (> (count stmtstr) size)
+            (subs stmtstr 0 size)
+            stmtstr)
+        s (s/replace s #"\W" "_")]
+    (keyword (str "s_" s "_" (str (Math/abs (hash stmt)))))))
+
+(defn pick-stmt-params
+  [ag stmt params]
+  ;; TODO get the complement and its evaluation
+  (let [comp stmt;; (literal-complement stmt)
+        ]
+    (cond (and (in-node? stmt) (out-node? comp))
+                (:stmt-inout-params params)
+          
+                (and (in-node? stmt) (in-node? comp))
+                (:stmt-inin-params params)
+          
+                (and (out-node? stmt) (in-node? comp))
+                (:stmt-outin-params params)
+          
+                :else
+                (:stmt-outout-params params))))
+
+(defn pick-arg-params
+  [ag arg params]
+  (cond (and (in-node? arg) (= (:direction arg) :pro))
+        (:arg-pro-applicable-params params)
+        
+        (and (in-node? arg) (= (:direction arg) :con))
+        (:arg-con-applicable-params params)
+        
+        (= (:direction arg) :pro)
+        (:arg-pro-notapplicable-params params)
+        
+        :else
+        (:arg-con-notapplicable-params params)))
+
+(defn add-statement
+  [svgmap stmt ag stmt-str params]
+  ;; TODO take the lang as a parameter
+  (let [stmtstr (stmt-to-str ag stmt stmt-str)
+        id (gen-stmt-id stmt)
+        stmt-params (merge (pick-stmt-params ag stmt params)
+                           {:label "" :x 0 :y 0 :shape :rect})
+        svgmap (add-node-kv svgmap id stmt-params)
+        svgmap (add-label-kv svgmap id
+                          (trunk-line stmtstr)
+                          (:stmtlabel-params params))]
+    svgmap))
+
+(defn add-arg-decorator
+  [svgmap arg argid]
+  {:pre [(argument-node? arg)]}
+  (if (= (:pro arg))
+    (add-decorator svgmap argid (make-plusdecorator))
+    (add-decorator svgmap argid (make-minusdecorator))))
+
+(defn add-argument-node
+  [svgmap arg ag params]
+  (let [arg-params (pick-arg-params ag arg params)
+        argid (gen-arg-id arg)
+        svgmap (add-node-kv svgmap argid arg-params)
+        svgmap (add-arg-decorator svgmap arg argid)]
+    svgmap))
+
+(defn add-conclusion-edge
+  [svgmap conclusion arg]
+  (let [id (geneid)
+        conclusionid (gen-stmt-id conclusion)
+        argid (gen-arg-id arg)]
+    (if (:pro arg)
+      (apply add-edge svgmap id argid conclusionid pro-argument-params)
+      (apply add-edge svgmap id argid conclusionid con-argument-params))))
+
+(defn add-premise-edge
+  [svgmap ag premise arg]
+  (let [edgeid (geneid)
+        argid (gen-arg-id arg)
+        stmtid (gen-stmt-id (get-statement-node ag (:atom premise)))]
+    (apply add-edge svgmap edgeid stmtid argid premise-params)
+    ;; (if (arg/premise-neg? premise)
+    ;;  (apply add-edge svgmap edgeid stmtid argid neg-premise-params)
+    ;;  (apply add-edge svgmap edgeid stmtid argid premise-params))
+    ))
+
+(defn add-premises-edges
+  [svgmap ag premises arg]
+  (reduce (fn [svgmap premise]
+            (add-premise-edge svgmap ag premise arg))
+          svgmap premises))
+
+(defn add-argument
+  [svgmap arg ag params]
+  (let [conclusion (get-statement-node ag (:conclusion arg))
+        premises (map #(get-statement-node ag (:statement %)) (:premises arg))
+        svgmap (add-argument-node svgmap arg ag params)
+        svgmap (add-conclusion-edge svgmap conclusion arg)
+        svgmap (add-premises-edges svgmap ag premises arg)]
+    svgmap))
+
+(defn add-entities
+  [svgmap ag stmt-str params]
+  (let [statements (vals (:statement-nodes ag))
+        arguments (vals (:argument-nodes ag))]
+    (let [svgmap (reduce (fn [svgmap stmt]
+                           (add-statement svgmap stmt ag stmt-str params))
+                         svgmap statements)
+          svgmap (reduce (fn [svgmap arg]
+                           (add-argument svgmap arg ag params))
+                         svgmap arguments)]
+      svgmap)))
+
+(defn add-markers
+  [map]
+  (reduce (fn [map marker]
+            (add-def map marker))
+          map
+          default-markers))
+
+(defn export-ag-helper
+  [ag stmt-str & options]
+  (let [options-kv (apply hash-map options)
+        width (get options-kv :width 1280)
+        height (get options-kv :height 1024)
+        layouttype (get options-kv :layout :hierarchical)
+        svgmap (create-graph :width width :height height)
+        svgmap (add-markers svgmap)
+        params (merge default-params options-kv)
+        ag (apply subset-ag ag options)
+        svgmap (add-entities svgmap ag stmt-str params)
+        svgmap (time (apply layout svgmap layouttype options))
+        svgmap (build svgmap)]
+    svgmap))
+
+(defn export-ag
+  "Exports an argument graph to a SVG file.
+
+   Options are :treeify, :full-duplication, :depth,
+   :layout and all options supported by the layout"
+  [ag stmt-str filename & options]
+  (let [ag (evaluate carneades-evaluator ag)
+        map (apply export-ag-helper ag stmt-str options)]
+    (export map filename :indent "yes")))
+
+(defn export-ag-os
+  "Exports an argument graph to an InputStream.
+
+   Options are :treeify, :full-duplication, :depth,
+   :layout and all options supported by the layout"
+  [ag stmt-str & options]
+  (let [map (apply export-ag-helper ag stmt-str options)
+        os (ByteArrayOutputStream.)
+        v (view map)]
+    (dom/spit-xml os v :indent "yes")
+    (InputStreamReader.
+     (ByteArrayInputStream. (.toByteArray os))
+     "UTF8")))
---
+++ src/CarneadesEngine/src/carneades/maps/subset_ag.clj
@@ -0,0 +1,62 @@
+(ns carneades.maps.subset-ag
+  (:use clojure.pprint
+        carneades.engine.statement
+        carneades.engine.argument
+        carneades.engine.argument-graph
+        carneades.engine.caes
+        carneades.engine.argument))
+
+(defn is-root?
+  [ag stmt]
+  (empty? (:premise-of (get-statement-node ag stmt))))
+
+(defn has-root?
+  [ag arg]
+  (is-root? ag (:conclusion arg)))
+
+(defn add-arg
+  [subset arg]
+  (enter-argument subset (map->argument arg)))
+
+(defn expand-border
+  [ag border]
+  (set (mapcat (fn [arg]
+                 (mapcat (fn [premise]
+                           (map (fn [argid] (get-argument-node ag argid))
+                                (concat (:pro (get-statement-node ag (:atom premise)))
+                                        (:con (get-statement-node ag (:atom premise))))))
+                         (:premises arg)))
+               border)))
+
+(defn walk-ag
+  [subset ag border visited depth treeify]
+  (if (or (zero? depth) (empty? border))
+    subset
+    (let [subset (reduce add-arg subset border)
+          newborder (expand-border ag border)]
+      (recur subset ag newborder (apply conj visited border) (dec depth) treeify))))
+
+(defn update-statuses
+  [subset ag]
+  ;; TODO
+  subset
+  ;; (let [statements (vals (:statement-nodes subset))]
+  ;;   (reduce (fn [subset stmt]
+  ;;             (cond (accepted? ag stmt) (accept subset [stmt])
+  ;;                   (rejected? ag stmt) (reject subset [stmt])
+  ;;                   (questioned? ag stmt) (question subset [stmt])
+  ;;                   :else subset))
+  ;;           subset
+  ;;           statements))
+  )
+
+(defn subset-ag
+  [ag & options]
+  ag
+  ;; (let [options (apply hash-map options)
+  ;;       {:keys [depth treeify] :or {treeify true depth Integer/MAX_VALUE}} options
+  ;;       argroots (filter (fn [arg] (has-root? ag arg)) (arguments ag))
+  ;;       subset (walk-ag (make-argument-graph) ag argroots #{} depth treeify)
+  ;;       subset (update-statuses subset ag)]
+  ;;   subset)
+  )
