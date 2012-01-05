03308b3d74a1d75a1795a3f2f8cc5c8556c3485a
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2012-01-05 15:05:43 +0100

    fixed a few minor bugs and unicode issues

--- src/CarneadesEngine/src/carneades/maps/lacij_export.clj
+++ src/CarneadesEngine/src/carneades/maps/lacij_export.clj
@@ -1,4 +1,4 @@
-;;; Copyright ? 2010-2011 Fraunhofer Gesellschaft
+;;; Copyright (c) 2010-2011 Fraunhofer Gesellschaft
 ;;; Licensed under the EUPL V.1.1
 
 (ns carneades.maps.lacij-export
@@ -100,7 +100,6 @@
   [svgmap ag premise arg]
   (let [edgeid (geneid)
         argid (gen-arg-id arg)
-        ; stmtid (gen-stmt-id (get-statement-node ag (literal-atom premise)))
         stmtid (gen-stmt-id (get (:statement-nodes ag) (:statement premise)))]
     (apply add-edge svgmap edgeid stmtid argid premise-params)
     ;; (if (arg/premise-neg? premise)
@@ -145,7 +144,7 @@
 
 (defn export-ag-helper
   [ag stmt-str options]
-  (let [; ag (evaluate carneades-evaluator ag)
+  (let [ag (evaluate carneades-evaluator ag)
         width (get options :width 1280)
         height (get options :height 1024)
         layouttype (get options :layout :hierarchical)
