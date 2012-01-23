cdff29e863a7abfed130b4e5d08f16c3e0f3e037
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-01-23 15:52:50 +0100

    upgrade compojure (fixes bug with Tomcat on Windows)

--- src/PolicyModellingTool2/project.clj
+++ src/PolicyModellingTool2/project.clj
@@ -1,7 +1,7 @@
 (defproject policymodellingtool2 "0.1.0"
   :description "Policy Modelling Tool for the IMPACT Policy project"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [compojure "0.6.4"]
+                 [compojure "1.0.1"]
                  [hiccup "0.3.6"]
                  [enlive "1.0.0"]
                  [carneades-engine "1.0.0-SNAPSHOT"]
@@ -9,6 +9,6 @@
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [carneades-mapcomponent "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-ring "0.4.5"]
+  :dev-dependencies [[lein-ring "0.5.0"]
                      [swank-clojure "1.4.0-SNAPSHOT"]]
   :ring {:handler impact.web.routes/app})
