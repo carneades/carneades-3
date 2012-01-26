76808c23134a13f773f8587c8be2194c1c6b1157
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-01-26 14:20:35 +0100

    various routes profiles for building JAR, WAR or for development.

---
+++ src/CarneadesWebApplication/src/carneades/web/application/routes_selfexe.clj
@@ -0,0 +1,30 @@
+(ns carneades.web.application.routes-selfexe
+  (:use compojure.core
+        ring.adapter.jetty
+        ring.middleware.params
+        ring.middleware.session
+        carneades.web.application.views.pages
+        carneades.web.service)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response])
+  (:gen-class))
+
+(def carneades-application-routes
+     [(GET "/" [] (index-page))
+      (route/files "/" {:root (str (System/getProperty "user.dir") "/data/public")})
+      (route/resources "/")
+      ])
+
+(def allroutes
+     (concat carneades-application-routes carneades-web-service-routes))
+
+(def app (handler/site (apply routes allroutes)))
+
+(defn start-server
+  []
+  (run-jetty #'app {:join? false :port 8080}))
+
+(defn -main
+  [& args]
+  (start-server))
