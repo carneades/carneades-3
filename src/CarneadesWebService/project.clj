ccd110109524d97df2012e7bf594b71abd754a9f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-02-17 12:53:18 +0100

    many fixes to make the web projects WAR compatible

--- src/CarneadesWebService/project.clj
+++ src/CarneadesWebService/project.clj
@@ -5,6 +5,7 @@
                  [org.clojure/data.json "0.1.1"]
                  [carneades-engine "2.0.0-SNAPSHOT"]
                  [compojure "1.0.1" :exclusion [clojure]]
+                 [ring/ring-servlet "1.0.1"]
                  [org.clojure/java.jdbc "0.1.1"]]
   :dev-dependencies [[lein-ring "0.4.5"]] 
   :ring {:handler carneades.web.service/carneades-web-service})
