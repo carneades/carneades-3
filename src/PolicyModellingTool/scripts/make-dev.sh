3e93fdb8e71979914e4cf4cac1aa0d959246c00f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-06 11:22:10 +0100

    fix build script for the new resource hierarchy

--- src/PolicyModellingTool/scripts/make-dev.sh
+++ src/PolicyModellingTool/scripts/make-dev.sh
@@ -3,4 +3,4 @@
 ### Changes the files to reflect a development configuration
 
 sed -i 's/impact.web.routes-war/impact.web.routes-dev/' project.clj
-sed -i 's/debug: false/debug: true/' ./resources/public/js/app/config.js
+sed -i 's/debug: false/debug: true/' ./resources/policymodellingtool/public/js/app/config.js
