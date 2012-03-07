de3f144136fc9c8292951cc46ee1855440304a7e
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-07 17:22:04 +0100

    work on policies evaluation

--- src/PolicyModellingTool/src/impact/web/controllers/policy_evaluation.clj
+++ src/PolicyModellingTool/src/impact/web/controllers/policy_evaluation.clj
@@ -4,31 +4,37 @@
 
 (defmulti ajax-handler (fn [json _] (ffirst json)))
 
-(defmethod ajax-handler :policyrules
-  [json session]
-  {:body (json-str {:policyrules (map str (get-policies (:policyrules json)))})})
-
-(defmethod ajax-handler :showgraph
-  [json session]
-  {:body (json-str {:graphpath (show-graph-url (get json :showgraph))})})
-
 (defmethod ajax-handler :evaluate
   [json session]
-  (let [{:keys [argGraph accept reject]} (:evaluate json)]
-    {:body (json-str {:evaluated (evaluate-graph argGraph accept reject)})}))
-
-(defmethod ajax-handler :abduction
-  [json session]
-  (let [{:keys [argGraph acceptability]} (:abduction json)
-        policies (find-policies argGraph (keyword acceptability))]
-    (prn "position =")
-    (prn policies)
-    {:body (json-str {:position policies
-                      :stmts_ids (get-stmt-to-ids argGraph)})}))
+  (let [policy (-> json :evaluate :policy)
+        dbname (evaluate-policy (symbol policy) session)]
+    {:session (assoc session :db dbname)
+     :body (json-str {:db dbname})}))
 
 (defn process-ajax-request
   [session body params]
   (let [json (read-json (slurp body))]
-    (prn "json =")
-    (prn json)
     (ajax-handler json session)))
+
+;; OLD:
+;; (defmethod ajax-handler :policyrules
+;;   [json session]
+;;   {:body (json-str {:policyrules (map str (get-policies (:policyrules json)))})})
+
+;; (defmethod ajax-handler :showgraph
+;;   [json session]
+;;   {:body (json-str {:graphpath (show-graph-url (get json :showgraph))})})
+
+;; (defmethod ajax-handler :evaluate
+;;   [json session]
+;;   (let [{:keys [argGraph accept reject]} (:evaluate json)]
+;;     {:body (json-str {:evaluated (evaluate-graph argGraph accept reject)})}))
+
+;; (defmethod ajax-handler :abduction
+;;   [json session]
+;;   (let [{:keys [argGraph acceptability]} (:abduction json)
+;;         policies (find-policies argGraph (keyword acceptability))]
+;;     (prn "position =")
+;;     (prn policies)
+;;     {:body (json-str {:position policies
+;;                       :stmts_ids (get-stmt-to-ids argGraph)})}))
