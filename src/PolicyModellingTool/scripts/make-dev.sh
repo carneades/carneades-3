bd6ab53c0fdd926d2687e7ba23a3c32f4de8da6e
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-13 17:30:31 +0100

    replace sed call to perl call in build scripts

--- src/PolicyModellingTool/scripts/make-dev.sh
+++ src/PolicyModellingTool/scripts/make-dev.sh
@@ -2,5 +2,5 @@
 
 ### Changes the files to reflect a development configuration
 
-sed -i 's/impact.web.routes-war/impact.web.routes-dev/' project.clj
-sed -i 's/debug: false/debug: true/' ./resources/policymodellingtool/public/js/app/config.js
+perl -i -pe 's/impact.web.routes-war/impact.web.routes-dev/g' project.clj
+perl -i -pe 's/debug: false/debug: true/' ./resources/policymodellingtool/public/js/app/config.js
