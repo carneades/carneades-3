3f21abe8ee654da0f6cb8e818664485d96e1c90c
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-13 14:57:10 +0100

    some styling and bug fixes

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/utils.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/utils.js
@@ -1,5 +1,6 @@
 // adds a format method to all string
 // place holders are of the form {0}, {1} etc
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
@@ -10,7 +11,7 @@ String.prototype.format = function() {
   });
 };
 
-// replaces the object properties names with minus (-) to properties names with underscore
+// // replaces the object properties names with minus (-) to properties names with underscore
 Object.prototype.normalize = function() {
     for(name in this) {
         if(typeof this[name] !== 'function') {
