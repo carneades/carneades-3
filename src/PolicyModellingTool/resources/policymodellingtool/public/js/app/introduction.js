f2e0bd6906524d7f906b23cda2247af88dbe07be
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-14 15:14:22 +0100

    proper styling

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/introduction.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/introduction.js
@@ -2,6 +2,7 @@ PM.display_introduction = function() {
     var introduction_html = ich.introduction();
     $('#pm').html(introduction_html.filter("#introduction"));
     $('#start').click(PM.on_start_button);
+    PM.activate('#introduction-item');
 };
 
 PM.on_start_button = function() {
