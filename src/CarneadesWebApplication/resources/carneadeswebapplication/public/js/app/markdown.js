b2a6374804c42d1dd9c76b3cbd8253e23a14d380
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-16 11:50:06 +0100

    remove uncessary p tags in the MD to HTML generation

--- src/CarneadesWebApplication/resources/carneadeswebapplication/public/js/app/markdown.js
+++ src/CarneadesWebApplication/resources/carneadeswebapplication/public/js/app/markdown.js
@@ -4,5 +4,11 @@ function markdown_to_html(md_text)
         return "";
     }
     var converter = Markdown.getSanitizingConverter(); 
-    return converter.makeHtml(md_text);
+    var html = converter.makeHtml(md_text);
+    // as a temporary fix, we remove <p> tags causing problem
+    // with the CSS styles
+    if(html.slice(0, 3) == "<p>") {
+        html = html.slice(3, -4);
+    }
+    return html;
 }
