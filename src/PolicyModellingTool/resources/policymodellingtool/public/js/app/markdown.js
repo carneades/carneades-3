970868900fb6fa318a4fac81ae63a9e9b175d108
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-28 15:00:50 +0200

    policies template filled from JSON

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/markdown.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/markdown.js
@@ -6,8 +6,8 @@ PM.markdown_to_html = function(md_text) {
     var html = converter.makeHtml(md_text);
     // as a temporary fix, we remove <p> tags causing problem
     // with the CSS styles
-    if(html.slice(0, 3) == "<p>") {
-        html = html.slice(3, -4);
-    }
+    // if(html.slice(0, 3) == "<p>") {
+    //     html = html.slice(3, -4);
+    // }
     return html;  
 };
