cd7d6f3814baff5bf7939e239451a9167bacdd07
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-03-28 18:51:14 +0200

    reading and selection policies from json

--- src/PolicyModellingTool/src/impact/web/controllers/policy_evaluation.clj
+++ src/PolicyModellingTool/src/impact/web/controllers/policy_evaluation.clj
@@ -4,38 +4,7 @@
 
 (defmulti ajax-handler (fn [json _] (ffirst json)))
 
-(defmethod ajax-handler :evaluate
-  [json session]
-  (let [policy (-> json :evaluate :policy)
-        ;; TODO: get the question from the session
-        dbname (evaluate-policy 'Q12 (symbol policy) session)]
-    {:session (assoc session :db dbname)
-     :body (json-str {:db dbname})}))
-
 (defn process-ajax-request
   [session body params]
   (let [json (read-json (slurp body))]
     (ajax-handler json session)))
-
-;; OLD:
-;; (defmethod ajax-handler :policyrules
-;;   [json session]
-;;   {:body (json-str {:policyrules (map str (get-policies (:policyrules json)))})})
-
-;; (defmethod ajax-handler :showgraph
-;;   [json session]
-;;   {:body (json-str {:graphpath (show-graph-url (get json :showgraph))})})
-
-;; (defmethod ajax-handler :evaluate
-;;   [json session]
-;;   (let [{:keys [argGraph accept reject]} (:evaluate json)]
-;;     {:body (json-str {:evaluated (evaluate-graph argGraph accept reject)})}))
-
-;; (defmethod ajax-handler :abduction
-;;   [json session]
-;;   (let [{:keys [argGraph acceptability]} (:abduction json)
-;;         policies (find-policies argGraph (keyword acceptability))]
-;;     (prn "position =")
-;;     (prn policies)
-;;     {:body (json-str {:position policies
-;;                       :stmts_ids (get-stmt-to-ids argGraph)})}))
