4f6f92edf3a29fce110c8c7e267e1e6447fa2034
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-04-13 15:53:18 +0200

    CSS styles!

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/main.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/main.js
@@ -54,5 +54,39 @@ $(function() {
   });
 
 function initPM() {
-     PM.display_introduction();
+    PM.display_introduction();
+    if(PM_CONFIG.debug) {
+        PM.load_uid_styles();
+    }
 }
+
+PM.load_uid_styles = function() {
+    var files = ['<link type="text/css" href="toolbox/css/impact-ui/jquery-ui-1.8.11.custom.css" rel="stylesheet" />',
+                 '<link type="text/css" href="toolbox/css/impact-ui/impact-green.css" rel="stylesheet" />',
+                 '<link rel="stylesheet" type="text/css" media="all" href="toolbox/css/impact-ui/plugins/jquery-ui-timepicker.css" />',
+                 '<link href="toolbox/css/main.css" rel="stylesheet" type="text/css" />',
+                 '<link rel="stylesheet" href="toolbox/css/policymodelling/style.css" type="text/css" />'
+                ];
+    
+    var scripts = ["toolbox/js/impact-ui/jquery-ui-1.8.11.custom.min.js",
+                   "toolbox/js/impact-ui/impact-init.js",
+                   "toolbox/js/impact-ui/jquery.jscrollpane.min.js",
+                   "toolbox/js/impact-ui/jquery.mousewheel.js",
+                   "toolbox/js/impact-ui/jquery.mwheelIntent.js",
+                   "toolbox/js/impact-ui/ui.checkbox.js",
+                   "toolbox/js/impact-ui/jquery.selectbox-0.5.js",
+                   "toolbox/js/impact-ui/jquery.busy.min.js",
+                   "toolbox/js/impact-ui/jquery.ui.timepicker.js"
+                   // "toolbox/js/main.js"
+                  ];
+
+    _.each(files, function(file) {
+               $('head').append(file);
+           });
+    
+    // http://stackoverflow.com/questions/6502943/resetting-document-ready-getscript
+    _.each(scripts, function(script) {
+               $.getScript(script);
+           });
+    
+};
