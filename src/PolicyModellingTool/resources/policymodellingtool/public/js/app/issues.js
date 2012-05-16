3d7be2bf4ae7c8275ccf8bf0cc30bd5299c47a82
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-05-16 17:10:00 +0200

    basic validation of the user's input

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/issues.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/issues.js
@@ -11,13 +11,16 @@ PM.display_issues = function() {
                 function(policies) {
                     var issues_html = ich.issues(policies);
                     $('#pm').html(issues_html.filter("#issues"));
+                    $('input').first().attr('checked', true);
                     $('#submit').click(PM.on_submit_issues);
-                    PM.activate('#issues-item');                        
+                    PM.activate('#issues-item');
                 });
 };
 
 PM.on_submit_issues = function() {
-    IMPACT.question = $('input[name="issue"]:checked').val();
-    PM.set_facts_url();
-    return false;  
+    if($('#issuesform').valid()) {
+        IMPACT.question = $('input[name="issue"]:checked').val();
+        PM.set_facts_url();
+    }
+    return false;
 };
