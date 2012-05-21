41662c55b22b859175dee07440b78b84af5caf31
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-05-21 14:46:42 +0200

    use of a namespace for the argument browser

--- src/CarneadesWebApplication/resources/carneadeswebapplication/public/js/app/premise.js
+++ src/CarneadesWebApplication/resources/carneadeswebapplication/public/js/app/premise.js
@@ -1,8 +1,8 @@
-function format_premise(premise)
+AGB.format_premise = function(premise)
 {
-    var statement_text = statement_text(premise.statement);
+    var statement_text = AGB.statement_text(premise.statement);
     var role = premise.role;
     
     
     return;
-}
+};
