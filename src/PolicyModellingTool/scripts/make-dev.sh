103e8a56d71898bd179db67cc44f19528027abc7
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-05-22 12:28:30 +0200

    dynamically loading of JS scripts and templates iff the PMT is inside the toolbox

--- src/PolicyModellingTool/scripts/make-dev.sh
+++ src/PolicyModellingTool/scripts/make-dev.sh
@@ -4,3 +4,5 @@
 
 perl -i -pe 's/impact.web.routes-war/impact.web.routes-dev/g' project.clj
 perl -i -pe 's/debug: false/debug: true/' ./resources/policymodellingtool/public/js/app/config.js
+perl -i -pe 's/in_uid_toolbox: true/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js
+cp resources/policymodellingtool/public/index.html.local resources/policymodellingtool/public/index.html
