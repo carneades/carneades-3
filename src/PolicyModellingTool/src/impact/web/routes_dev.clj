9687d35d3a2dfa20596c3e5be6ee366ff6db7697
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-05-25 12:22:55 +0200

    update project.clj and routes_dev

--- src/PolicyModellingTool/src/impact/web/routes_dev.clj
+++ src/PolicyModellingTool/src/impact/web/routes_dev.clj
@@ -1,7 +1,6 @@
 (ns impact.web.routes-dev
   (:use impact.web.routes
         carneades.web.service
-        carneades.web.application.routes
         compojure.core
         impact.web.jetty
         ring.adapter.jetty
@@ -11,22 +10,10 @@
 
 (defroutes all-impact-pm-tool-routes
   (context "/impactws" [] carneades-web-service-routes)
-  (context "/argumentbrowser" [] carneades-application-routes)
   (context "/policymodellingtool" [] impact-pm-tool-routes))
 
 (def impact-app
   (-> (handler/site all-impact-pm-tool-routes)
       (wrap-base-url)))
 
-;; (defonce impact-server (run-jetty #'impact-app {:join? false :port 8080}))
-
-;; (.start impact-server)
-;; (.stop impact-server)
-;; to comment when building the JAR:
-;; (defonce impact-server (run-jetty-with-wars #'impact-app
-;;                          {:join? false :port 8080}
-;;                          [{:context-path "/argumentbrowser"
-;;                            :war-path "../CarneadesWebApplication/carneades-web-application-1.0.0-SNAPSHOT-standalone.war"}
-;;                           {:context-path "/impactws"
-;;                            :war-path "../CarneadesWebService/carneades-web-service-1.0.0-SNAPSHOT-standalone.war"}]))
-
+(defonce impact-server (run-jetty #'impact-app {:join? false :port 8080}))
