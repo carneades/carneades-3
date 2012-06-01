440114205da2f9cd00a2cd7051a5ded0ce3fa380
Tom Gordon <thomas.gordon@fokus.fraunhofer.de>
2012-06-01 15:53:53 +0200

    First version of a new example policy model, of Polish regulations for tour operators
    for the SPOCS project

--- src/CarneadesExamples/src/carneades/examples/policies.clj
+++ src/CarneadesExamples/src/carneades/examples/policies.clj
@@ -7,5 +7,8 @@
  {:filename spocs_policies.clj
   :namespace carneades.examples.spocs-policies
   :name spocs-policies}
+ {:filename polish_tour_operator_insurance.clj
+  :namespace carneades.examples.polish-tour-operator-insurance
+  :name tour-operator-insurance }
 ]
 
