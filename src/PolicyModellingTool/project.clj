7ae3e279fb9875c1d29ff7d34a227bcd4fa6a70e
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-11 12:09:39 +0200

    some attempt at building a self executable JAR

--- src/PolicyModellingTool/project.clj
+++ src/PolicyModellingTool/project.clj
@@ -8,7 +8,8 @@
                  [ring/ring-servlet "1.0.1"]
                  [carneades-engine "2.0.0-SNAPSHOT"]
                  [carneades-web-service "1.0.0-SNAPSHOT"]
-                 [org.clojars.pallix/mygengo "1.0.0"]]
+                 [org.clojars.pallix/mygengo "1.0.0"]
+                 [lein-ring "0.5.4"]]
   :dev-dependencies [[lein-ring "0.5.4"]]
-  :ring {:handler impact.web.routes-dev/impact-app}
+  :ring {:handler impact.web.routes-selfexe}
   :jvm-opts ["-Xdebug" "-Xrunjdwp:transport=dt_socket,address=jdbconn,server=y,suspend=n"]) 
