0e376b6d1c8ef6475b7ebf924ace2b4bf935439c
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-11 12:09:39 +0200

    some attempt at building a self executable JAR

--- src/PolicyModellingTool/scripts/make-release.sh
+++ src/PolicyModellingTool/scripts/make-release.sh
@@ -2,7 +2,7 @@
 
 ### Changes the files to reflect a release configuration
 
-perl -i -pe 's/impact.web.routes-dev/impact.web.routes-war/' project.clj
+perl -i -pe 's/impact.web.routes-[^}]+}/impact.web.routes-war}/' project.clj
 perl -i -pe 's/debug: true/debug: false/' ./resources/policymodellingtool/public/js/app/config.js
 perl -i -pe 's/in_uid_toolbox: false/in_uid_toolbox: true/' ./resources/policymodellingtool/public/js/app/config.js 
 cp resources/policymodellingtool/public/index.html.in_uid_toolbox resources/policymodellingtool/public/index.html
