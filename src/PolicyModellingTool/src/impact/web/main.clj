9b548711d49eb8991cbe85d60b9a4e43336e6486
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-12 14:13:08 +0200

    comment unused namespaces

--- src/PolicyModellingTool/src/impact/web/main.clj
+++ src/PolicyModellingTool/src/impact/web/main.clj
@@ -1,12 +1,12 @@
 (ns impact.web.main
   (:use ring.adapter.jetty
-        clojure.contrib.javadoc.browse)
-  (:require impact.web.routes))
+        clojure.java.browse)
+  (:require impact.web.routes-war))
 
 (defn -main [& args]
   (let [port 8080
         url (str "http://localhost:" port)]
-    (run-jetty #'impact.web.routes/app {:join? false :port 8080})
-    (open-url-in-browser url)))
+    (run-jetty #'impact.web.routes-war/impact-app {:join? false :port 8080})
+    (browse-url url)))
 
 
