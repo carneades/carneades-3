b04e79054bf74eb21658707a02e6fa925f83502a
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-14 16:07:06 +0200

    added missing release script

---
+++ src/PolicyModellingTool/scripts/make-release-without-toolbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### Changes the files to reflect a release configuration which includes the toolbox css
+
+perl -i -pe 's/impact.web.routes-[a-z]+/impact.web.routes-war/' project.clj
+perl -i -pe 's/debug: [a-z]+/debug: false/' ./resources/policymodellingtool/public/js/app/config.js
+perl -i -pe 's/in_uid_toolbox: [a-z]+/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js 
+cp resources/policymodellingtool/public/index.html.local resources/policymodellingtool/public/index.html
