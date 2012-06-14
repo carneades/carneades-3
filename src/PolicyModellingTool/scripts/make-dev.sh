bcc572b3a9928ba3629af244d19f25fa6800001f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-14 16:00:22 +0200

    fix build.sh to build a WAR without the toolbox

--- src/PolicyModellingTool/scripts/make-dev.sh
+++ src/PolicyModellingTool/scripts/make-dev.sh
@@ -3,6 +3,6 @@
 ### Changes the files to reflect a development configuration
 
 perl -i -pe 's/impact.web.routes-[a-z]+/impact.web.routes-dev/' project.clj
-perl -i -pe 's/debug: false/debug: true/' ./resources/policymodellingtool/public/js/app/config.js
-perl -i -pe 's/in_uid_toolbox: true/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js
+perl -i -pe 's/debug: [a-z]+/debug: true/' ./resources/policymodellingtool/public/js/app/config.jsp
+perl -i -pe 's/in_uid_toolbox: false/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js 
 cp resources/policymodellingtool/public/index.html.local resources/policymodellingtool/public/index.html
--- src/PolicyModellingTool/scripts/make-release.sh
+++ src/PolicyModellingTool/scripts/make-release.sh
@@ -3,8 +3,8 @@
 ### Changes the files to reflect a release configuration
 
 perl -i -pe 's/impact.web.routes-[a-z]+/impact.web.routes-war/' project.clj
-perl -i -pe 's/debug: true/debug: false/' ./resources/policymodellingtool/public/js/app/config.js
-perl -i -pe 's/in_uid_toolbox: false/in_uid_toolbox: true/' ./resources/policymodellingtool/public/js/app/config.js 
+perl -i -pe 's/debug: [a-z]+/debug: false/' ./resources/policymodellingtool/public/js/app/config.js
+perl -i -pe 's/in_uid_toolbox: [a-z]+/in_uid_toolbox: true/' ./resources/policymodellingtool/public/js/app/config.js 
 cp resources/policymodellingtool/public/index.html.in_uid_toolbox resources/policymodellingtool/public/index.html
 
 
