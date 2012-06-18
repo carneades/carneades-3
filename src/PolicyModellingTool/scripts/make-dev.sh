a7a7327cce48cf9f7a5b5b6ae1bfad23643cf353
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-18 15:47:53 +0200

    fix bug in make-dev script

--- src/PolicyModellingTool/scripts/make-dev.sh
+++ src/PolicyModellingTool/scripts/make-dev.sh
@@ -4,5 +4,5 @@
 
 perl -i -pe 's/impact.web.routes-[a-z]+/impact.web.routes-dev/' project.clj
 perl -i -pe 's/debug: [a-z]+/debug: true/' ./resources/policymodellingtool/public/js/app/config.js
-perl -i -pe 's/in_uid_toolbox: false/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js 
+perl -i -pe 's/in_uid_toolbox: [a-z]+/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js 
 cp resources/policymodellingtool/public/index.html.local resources/policymodellingtool/public/index.html
