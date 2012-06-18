eace654d0a0e9e9c1e0ff041105c05e47aaa6fdc
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-18 11:04:38 +0200

    fix error in build scripts

--- src/build-all-and-test.sh
+++ src/build-all-and-test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# build one standalone and one toolbox WAR
-sh ./build.sh --release
-cd PolicyModellingTool
-mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-toolbox.war
+# build one standalone and one toolbox WAR and run tests
+sh -c "./build.sh --release" && \
+    cd PolicyModellingTool && \
+    mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-toolbox.war && \
+    sh -c "./scripts/make-dev.sh" && \
+    cd ../CarneadesEngine && lein test && \
+    cd ../CarneadesWebService && \
+    lein test
 
-./scripts/make-dev.sh
-
-# run tests
-
-cd ../CarneadesEngine && lein test && cd ../CarneadesWebService && lein test
