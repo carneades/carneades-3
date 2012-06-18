5e4120ac19de9b963ba4a263d067ff643426dc7d
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-18 11:15:19 +0200

    fix missing stuff in build script

--- src/build-all-and-test.sh
+++ src/build-all-and-test.sh
@@ -5,6 +5,7 @@ sh -c "./build.sh --release" && \
     cd PolicyModellingTool && \
     mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-toolbox.war && \
     sh -c "./scripts/make-dev.sh" && \
+    lein ring uberwar && \
     cd ../CarneadesEngine && lein test && \
     cd ../CarneadesWebService && \
     lein test
