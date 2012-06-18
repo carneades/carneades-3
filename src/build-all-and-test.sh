2ec59420e4d4c30f355ce7c10804ac24017f675c
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-18 13:57:17 +0200

    typo in build script

--- src/build-all-and-test.sh
+++ src/build-all-and-test.sh
@@ -4,7 +4,7 @@
 sh -c "./build.sh --release" && \
     cd PolicyModellingTool && \
     mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-toolbox.war && \
-    sh -c "./scripts/make-realese-without-toolbox.sh" && \
+    sh -c "./scripts/make-release-without-toolbox.sh" && \
     lein ring uberwar && \
     mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-css.war
     cd ../CarneadesEngine && lein test && \
