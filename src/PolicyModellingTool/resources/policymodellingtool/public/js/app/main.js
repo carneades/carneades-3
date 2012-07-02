315977e940cbd63dc1a8f012afac37e6ddc29f0a
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-07-02 19:17:26 +0200

    removed toolbox test variable

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/main.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/main.js
@@ -63,9 +63,9 @@ PM.dispatch_url = function(sections) {
     }
 };
 
-ImpactToolbox = {
+// ImpactToolbox = {
    
-};
+// };
 
 PM.in_uid_toolbox = function() {
     return window.ImpactToolbox != undefined;
