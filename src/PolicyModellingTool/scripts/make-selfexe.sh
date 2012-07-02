4d735b670bfc289b209a7de24c2afd5a794df07d
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-07-02 14:47:33 +0200

    correction for the UID API

--- src/PolicyModellingTool/scripts/make-selfexe.sh
+++ src/PolicyModellingTool/scripts/make-selfexe.sh
@@ -4,7 +4,6 @@
 
 perl -i -pe 's/impact.web.routes-[^}]+}/impact.web.routes-selfexe}/' project.clj
 perl -i -pe 's/debug: [a-z]+/debug: false/' ./resources/policymodellingtool/public/js/app/config.js
-perl -i -pe 's/in_uid_toolbox: [a-z]+/in_uid_toolbox: false/' ./resources/policymodellingtool/public/js/app/config.js 
 
 
 
