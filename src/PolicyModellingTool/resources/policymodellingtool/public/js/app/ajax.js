6409c100214828e9f78f3719dbdcb7fcfc816e31
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-14 16:25:45 +0200

    some tests with backbone.js

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/ajax.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/ajax.js
@@ -1,13 +1,20 @@
+PM.simple_auth = function(xhr) {
+    var bytes = Crypto.charenc.Binary.stringToBytes(IMPACT.user + ":" + IMPACT.password);
+    var base64 = Crypto.util.bytesToBase64(bytes);
+    xhr.setRequestHeader("Authorization", "Basic " + base64);
+};
+
+PM.send_authentification = function(username, password, xhr) {
+    var bytes = Crypto.charenc.Binary.stringToBytes(username + ":" + password);
+    var base64 = Crypto.util.bytesToBase64(bytes);
+    xhr.setRequestHeader("Authorization", "Basic " + base64);
+};
 
 PM.ajax_post = function(url, jsondata, callback, username, password,
                         callback_error) {
     $.ajax({url: url,
             type: 'POST',
-            'beforeSend' : function(xhr) {
-                var bytes = Crypto.charenc.Binary.stringToBytes(username + ":" + password);
-                var base64 = Crypto.util.bytesToBase64(bytes);
-                xhr.setRequestHeader("Authorization", "Basic " + base64);
-            },
+            'beforeSend': _.bind(PM.send_authentification, PM, username, password),
             dataType : 'json',
             data : JSON.stringify(jsondata),
             contentType: "application/json; charset=utf-8",
@@ -19,6 +26,7 @@ PM.ajax_post = function(url, jsondata, callback, username, password,
                 }
             }
         });
+    $.ajaxSetup({beforeSend: PM.simple_auth});
 };
 
 PM.ajax_get = function(url, callback, callback_error) {
@@ -33,6 +41,7 @@ PM.ajax_get = function(url, callback, callback_error) {
                 }
             }
         });
+    $.ajaxSetup({beforeSend: PM.simple_auth});
 };
 
 PM.ajax_delete  = function(url, callback, username, password,
@@ -53,6 +62,7 @@ PM.ajax_delete  = function(url, callback, username, password,
                 }
             }
            });
+    $.ajaxSetup({beforeSend: PM.simple_auth});
 };
 
 PM.ajax_put = function(url, jsondata, callback, username, password,
@@ -75,4 +85,5 @@ PM.ajax_put = function(url, jsondata, callback, username, password,
                 }
             }
         });
+    $.ajaxSetup({beforeSend: PM.simple_auth});
 };
