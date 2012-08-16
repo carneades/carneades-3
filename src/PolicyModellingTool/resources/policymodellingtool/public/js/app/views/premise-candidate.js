8ea60454191593b8e06439101a17a4256bb16a2f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-16 15:10:15 +0200

    sync between premise candidate view and premise candidate model

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/views/premise-candidate.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/views/premise-candidate.js
@@ -2,14 +2,15 @@ PM.PremiseCandidateView = Backbone.View.extend(
     {className: "premise-candidate",
      
      events: {
-         "change input": "role_changed"
+         "change input": "role_changed",
+         "change input[type=hidden]" : "statement_changed"
      },
      
      initialize: function() {
          this.model.bind('change', this.render, this);
-         _.bindAll(this, 'role_changed');
+         _.bindAll(this, 'role_changed', 'statement_changed', 'render');
      },
-
+     
      render: function() {
          var data = this.model.toJSON();
          
@@ -19,13 +20,40 @@ PM.PremiseCandidateView = Backbone.View.extend(
          role.prop('disabled', !data.editableRole);
          role.val(data.role);
          
-         // TODO display list of statement
+         var statement = this.statement();
+         
+         statement.select2({data: {results: statements.toJSON(),
+                                   text: function(statement) {
+                                       return AGB.statement_text(statement);
+                                   }
+                                  },
+                            placeholder: "Select a statement",
+                            formatSelection: AGB.format_selected_statement,
+                            formatResult: AGB.statement_text,
+                            initSelection: function(element) {
+                                return statements.get(element.val()).toJSON();
+                            }});
 
+         var statement_model = this.model.get('statement');
+         if(statement_model) {
+             statement.val(statement_model.get('id')).trigger('change');    
+         } 
+         
          return this;
+     },
+     
+     statement: function() {
+         return this.$el.find('input[type=hidden]');
      },
      
      role_changed: function() {
          this.model.set('role', $('.role-input').val());
+     },
+     
+     statement_changed: function() {
+         this.model.set('statement', this.model.get('statements').get(this.statement().val()));
      }
     }
 );
+
+
