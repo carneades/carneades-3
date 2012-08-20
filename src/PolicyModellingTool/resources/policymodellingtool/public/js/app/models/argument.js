d54ee5293b1510ee8e00a83fbb370fc22652397e
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-20 16:02:04 +0200

    fix ambiguity and bug with argument and argument-candidate

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/models/argument.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/models/argument.js
@@ -2,10 +2,12 @@ PM.Argument = Backbone.Model.extend(
     {defaults: {
          weight: 0.5
      },
-     
+   
      url: function() {
          return IMPACT.wsurl + '/argument/' + IMPACT.db;
      },
+   
+     // TODO constructor taking an Argument
      
      validate: function(attrs) {
          if(_.isNil(attrs.conclusion)) {
