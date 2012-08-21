b9f4c42aa0e999865e1abbcb6428097ee062d1c0
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-21 15:11:03 +0200

    ArgumentCandidate referencing an Argument model

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/models/argument-candidate.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/models/argument-candidate.js
@@ -1,18 +1,14 @@
 PM.ArgumentCandidate = Backbone.Model.extend(
-    {defaults: {
-         weight: 0.5
-     },
-     
-     initialize: function(attrs) {
+    {initialize: function(attrs) {
          // set the ConclusionCandidate for this ArgumentCandidate
-         var conclusioncandidate = new PM.ConclusionCandidate({statement: attrs.argument.conclusion, 
+         var conclusioncandidate = new PM.ConclusionCandidate({statement: attrs.argument.get('conclusion'), 
                                                                statements: attrs.statements});
          this.set('conclusion', conclusioncandidate);
          
          // set the PremisesCandidates for this ArgumentCandidate
          var premisescandidates = new PM.PremisesCandidates;
          this.set('premises', premisescandidates);
-         _.each(attrs.argument.premises,
+         _.each(attrs.argument.get('premises'),
                function(premise) {
                    premisescandidates.add({premise: premise,
                                            statements: attrs.statements});
@@ -20,10 +16,6 @@ PM.ArgumentCandidate = Backbone.Model.extend(
      },
 
      validate: function(attrs) {
-         if(_.isNil(attrs.argument.conclusion)) {
-             return "Conclusion attribute is missing";
-         }
-
          return undefined;
      }
      
