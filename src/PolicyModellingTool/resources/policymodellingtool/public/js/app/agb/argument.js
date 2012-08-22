467c4b5ccf728b4da73ec92804fb77aa60e0582f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-22 13:33:57 +0200

    fix bug in the editor

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/agb/argument.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/agb/argument.js
@@ -139,7 +139,7 @@ AGB.delete_argument = function(db, argid) {
 
 AGB.edit_argument = function(db, argid) {
     var argument = PM.arguments.get(argid);
-    var argumentcandidate = new PM.ArgumentCandidate({argument: argument,
+    var argumentcandidate = new PM.ArgumentCandidate({argument: argument.clone(),
                                                       statements: PM.statements});
     var argumenteditorview = new PM.ArgumentEditorView({model: argumentcandidate});
 
