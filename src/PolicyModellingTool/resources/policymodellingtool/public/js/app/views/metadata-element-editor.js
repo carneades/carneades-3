f77e67fdfda1dd18ba4849d2671c6ea1ca8772c4
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-30 17:05:04 +0200

    addition of metadata elements in the argument editor

---
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/views/metadata-element-editor.js
@@ -0,0 +1,44 @@
+// A view for an element of a metadata (except description)
+PM.MetadataElementEditorView = Backbone.View.extend(
+    {className: "metadata-element-editor-view",
+     
+     events: {
+         "change .metadata-element-input": "content_changed",
+         "click a.delete": "delete_element"
+     },
+     
+     initialize: function(attrs) {
+         this.type = attrs.type;
+         this.name = attrs.name;
+         this.model.on('change', this.render, this);
+         _.bindAll(this, 'render', 'content_changed');
+     },
+     
+     render: function() {
+         var data = this.model.toJSON();
+         
+         this.$el.html(ich.metadataelementeditor({name: this.name}));
+         var val = this.model.get_element_val(this.type, this.cid);
+         if (val) {
+             this.$('.metadata-element-input').val(val);
+         } 
+         
+         return this;
+     },
+     
+     content_changed: function() {
+         var content = this.$('.metadata-element-input').val();
+         var metadata_candidate = this.model;
+         metadata_candidate.set_element_val(this.type, this.cid, content);
+         
+         return false;
+     },
+     
+     delete_element: function () {
+         // TODO remove the element from the metadata
+         this.remove();
+         return false;
+     }
+     
+    }
+);
