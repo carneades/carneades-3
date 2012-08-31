6c6783df1e0949cd3a72a97e30a0dd0ad0f6e636
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-08-31 12:27:02 +0200

    fix REST service tests

--- src/CarneadesWebService/test/carneades/web/test/service.clj
+++ src/CarneadesWebService/test/carneades/web/test/service.clj
@@ -36,30 +36,30 @@
 (defn unrecordify
   "Converts a record to a hash-map"
   [r]
-  (apply array-map (flatten (map identity r))))
+  (into {} r))
 
 (defn make-some-string [prefix]
   (str prefix "-" (gensym)))
 
 (defn make-some-metadata
   []
-  {:key (make-some-string "key")
-   :contributor (make-some-string "contributor")
-   :creator (make-some-string "creator")
-   :coverage (make-some-string "coverage")
-   :date (make-some-string "date")
+  {:key [(make-some-string "key")]
+   :contributor [(make-some-string "contributor")]
+   :creator [(make-some-string "creator")]
+   :coverage [(make-some-string "coverage")]
+   :date [(make-some-string "date")]
    :description {:en (make-some-string "description-en")
                  :de (make-some-string "description-de")}
-   :format (make-some-string "format")
-   :identifier (make-some-string "identifier")
-   :language (make-some-string "language")
-   :publisher (make-some-string "publisher")
-   :relation (make-some-string "relation")
-   :rights (make-some-string "rights")
-   :source (make-some-string "source")
-   :subject (make-some-string "subject")
-   :title (make-some-string "title")
-   :type (make-some-string "type")})
+   :format [(make-some-string "format")]
+   :identifier [(make-some-string "identifier")]
+   :language [(make-some-string "language")]
+   :publisher [(make-some-string "publisher")]
+   :relation [(make-some-string "relation")]
+   :rights [(make-some-string "rights")]
+   :source [(make-some-string "source")]
+   :subject [(make-some-string "subject")]
+   :title [(make-some-string "title")]
+   :type [(make-some-string "type")]})
 
 (defn make-some-statement-data
   []
@@ -99,13 +99,13 @@
         id (read-json (:body res))
         newtitle (make-some-string "Title")
         newcreator (make-some-string "Creator")
-        update {:title newtitle
-                :creator newcreator}
+        update {:title [newtitle]
+                :creator [newcreator]}
         res2 (put-request (format "/metadata/%s/%s" dbname id) carneades-web-service
                           {"authorization" auth-header}
                           (json-str update))
         res3 (get-request (format "/metadata/%s/%s" dbname id) carneades-web-service)
-        expected (assoc data :title newtitle :creator newcreator)
+        expected (assoc data :title [newtitle] :creator [newcreator])
         returned-data (filter-out-nils (read-json (:body res3)) (keys data))]
     (is (= "true" (:body res2)))
     (is (= expected returned-data))))
