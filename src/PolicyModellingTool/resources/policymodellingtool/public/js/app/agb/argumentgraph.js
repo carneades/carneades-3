83734955d51d243b100a4bd4dedf84e10fc2761f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-09-03 11:41:51 +0200

    fix bug with metadata

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/agb/argumentgraph.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/agb/argumentgraph.js
@@ -21,7 +21,7 @@ AGB.argumentgraph_html = function(db, data)
     AGB.set_mainissues_text(data.main_issues);
     data.references = data.metadata.filter(function (ref) { return ref.key; });
     AGB.set_references_text(data.references);
-    data.title = AGB.markdown_to_html(data.metadata[0].title);
+    data.title = AGB.markdown_to_html(data.metadata[0].title[0]);
     data.outline_text = AGB.outline_text(data.outline, db);
     var argumentgraph_html = ich.argumentgraph(data);
     return argumentgraph_html.filter('#argumentgraph');
