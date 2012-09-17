c37c774c3e6ea8107bc204600ee089194bb12391
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-09-17 16:12:19 +0200

    schemes page with descriptions

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/policies.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/policies.js
@@ -11,7 +11,8 @@ PM.display_policies = function(sectionid) {
                 function(policies) {
                     var ids = [];
                     var current_policy = policies[IMPACT.current_policy];
-                    current_policy.outline_text = PM.policies_outline_text(current_policy.sections);
+                    current_policy.outline_text = PM.theory_outline_text(current_policy.sections,
+                                                                         'policies');
                     current_policy.description_text = current_policy.header.description[IMPACT.lang];
                     current_policy.policies_text = PM.policies_text(current_policy.language, current_policy.sections, 2,
                                                              function(policyid) {
@@ -59,12 +60,12 @@ PM.on_evaluated_policy = function(data) {
     PM.set_arguments_url(IMPACT.db);
 };
 
-PM.policies_outline_text = function(sections) {
+PM.theory_outline_text = function(sections, urlfragment) {
     var text = "<ul>";
     
     _.each(sections, function(section) {
-               text += '<li><a href="#/policies/{0}">{1}</a></li>'.format(section.id, section.header.title);
-               text += PM.policies_outline_text(section.sections);
+               text += '<li><a href="#/{0}/{1}">{2}</a></li>'.format(urlfragment, section.id, section.header.title);
+               text += PM.theory_outline_text(section.sections, urlfragment);
            });
 
     text += "</ul>";
@@ -76,20 +77,30 @@ PM.schemes_text = function(language, schemes) {
     var text = "<ul>";
     
     _.each(schemes, function(scheme) {
-               text += '<li>{0}'.format(PM.markdown_to_html(scheme.header.description[IMPACT.lang]));
-               text += '<p>{0}, {1}'.format(scheme.id, PM.format_sexpr(scheme.conclusion, language));
-               text += '<ul>';
-               _.each(scheme.premises, function(premise) {
-                          if(premise.statement.atom[0] != "valid") {
-                              text += "<li>{0}</li>".format(PM.format_sexpr(premise.statement.atom, language));
-                          }
-                      });
-               text += '</ul>';
-               text += '</p>';
-               text += '</li>';
+               text += PM.scheme_content_text(language, scheme);
            });
     
     text += "</ul>";
+    
+    return text;
+};
+
+PM.scheme_content_text = function(language, scheme) {
+    var text = "";
+    
+    if(scheme.header.description && scheme.header.description[IMPACT.lang]) {
+        text += '<li>{0}'.format(PM.markdown_to_html(scheme.header.description[IMPACT.lang]));
+    }
+    text += '<p>{0}, {1}'.format(scheme.id, PM.format_sexpr(scheme.conclusion, language));
+    text += '<ul>';
+    _.each(scheme.premises, function(premise) {
+               if(premise.statement.atom[0] != "valid") {
+                   text += "<li>{0}</li>".format(PM.format_sexpr(premise.statement.atom, language));
+               }
+           });
+    text += '</ul>';
+    text += '</p>';
+    text += '</li>';
     
     return text;
 };
@@ -99,12 +110,12 @@ PM.policies_text = function(language, sections, level, on_policy) {
     
     _.each(sections, function(section) {
                text += '<div id="{0}">'.format(section.id);
-               text += '<form action=""><h{0}>'.format(level);
+               text += '<form action=""><h{0}>'.format(level + 1);
                if(section.schemes.length > 0) {
                    on_policy(section.id);
                    text += '<input type="submit" value="Select" id="input{0}" />'.format();
                }
-               text += ' {1}</h{0}></form>'.format(level, section.header.title);
+               text += ' {1}</h{0}></form>'.format(level + 1, section.header.title);
                text += '<p>{0}</p>'.format(PM.markdown_to_html(section.header.description[IMPACT.lang]));
                text += PM.markdown_to_html(PM.schemes_text(language, section.schemes));
                text += PM.policies_text(language, section.sections, level + 1, on_policy);
@@ -123,10 +134,16 @@ PM.varname = function(v) {
 };
 
 // Formats an sexpr represented as an array of strings
+// or a string. A single string represents an atom (NOT a sexpr).
 PM.format_sexpr = function(sexpr, language) {
+    if(typeof sexpr == 'string') {
+        // hack
+        return UTILS.escape_html(sexpr.substr(1));
+    }
+    
     var terms = _.map(sexpr.slice(1), PM.varname);
     if(_.isNil(language[sexpr[0]])) {
-        return PM.markdown_to_html(sexpr.join(" "));
+        return UTILS.escape_html(sexpr.join(" "));
     } else {
         var fstring = language[sexpr[0]].forms[IMPACT.lang].positive;
         var args = [fstring].concat(terms);
