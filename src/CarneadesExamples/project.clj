0e550b827cc4d3b35660b11ab7a2201988d42533
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-09-24 11:19:15 +0200

    upgrade to Leinigen 2

--- src/CarneadesExamples/project.clj
+++ src/CarneadesExamples/project.clj
@@ -1,9 +1,6 @@
-;;; Copyright © 2010 Fraunhofer Gesellschaft 
-;;; Licensed under the EUPL V.1.1
-
-(defproject carneades-examples "2.0.0-SNAPSHOT"
-  :description "Carneades is an argument mapping application, with a graphical 
-user interface, and a software library for building applications supporting 
-various argumentation tasks. These are some examples."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [carneades-engine "2.0.0-SNAPSHOT"]])
+(defproject carneades-examples/carneades-examples "2.0.0-SNAPSHOT" 
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "[1.4.0,]"]
+                 [carneades-engine/carneades-engine "2.0.0-SNAPSHOT"]
+                 ]
+  :description "Carneades is an argument mapping application, with a graphical \nuser interface, and a software library for building applications supporting \nvarious argumentation tasks. These are some examples.")
