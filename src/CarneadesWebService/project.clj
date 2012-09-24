d1449cf0875d83e1412b78fbd505c290a36de3bc
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-09-24 11:19:15 +0200

    upgrade to Leinigen 2

--- src/CarneadesWebService/project.clj
+++ src/CarneadesWebService/project.clj
@@ -1,11 +1,11 @@
-(defproject carneades-web-service "1.0.0-SNAPSHOT"
-  :description "This is a RESTful Web Service API for the Carneades 
-                argumentation engine."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+(defproject carneades-web-service/carneades-web-service "1.0.0-SNAPSHOT" 
+  :dependencies [[org.clojure/clojure "[1.4.0,]"]
                  [org.clojure/data.json "0.1.1"]
                  [carneades-engine "2.0.0-SNAPSHOT"]
                  [compojure "1.0.1" :exclusion [clojure]]
                  [org.clojure/java.jdbc "0.1.2"]
                  [ring/ring-servlet "1.0.1"]]
-  :dev-dependencies [[lein-ring "0.5.4"]] 
-  :ring {:handler carneades.web.service/carneades-web-service})
+  :plugins [[lein-ring "0.7.1"]]
+  :ring {:handler carneades.web.service/carneades-web-service}
+  :min-lein-version "2.0.0"
+  :description "This is a RESTful Web Service API for the Carneades argumentation engine.")
