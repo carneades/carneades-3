4e0f565eb8bc968fa6af66e22ffc76dbd24fdf67
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-09-24 11:19:15 +0200

    upgrade to Leinigen 2

--- src/build-all-and-test.sh
+++ src/build-all-and-test.sh
@@ -3,10 +3,10 @@
 # build one standalone and one toolbox WAR and run tests
 bash -c "./build.sh --release" && \
     cd PolicyModellingTool && \
-    mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-toolbox.war && \
+    mv target/policymodellingtool-1.0.0-SNAPSHOT-standalone.war target/policymodellingtool-1.0.0-SNAPSHOT-toolbox.war && \
     sh -c "./scripts/make-release-without-toolbox.sh" && \
     lein ring uberwar && \
-    mv policymodellingtool-1.0.0-SNAPSHOT-standalone.war policymodellingtool-1.0.0-SNAPSHOT-css.war
+    mv target/policymodellingtool-1.0.0-SNAPSHOT-standalone.war target/policymodellingtool-1.0.0-SNAPSHOT-css.war
     cd ../CarneadesEngine && lein test && \
     cd ../CarneadesWebService && \
     lein test
