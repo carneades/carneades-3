f23b8d7fc8e24ff14fc2b750011773d516fe0de5
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-09-24 11:19:15 +0200

    upgrade to Leinigen 2

--- src/PolicyModellingTool/project.clj
+++ src/PolicyModellingTool/project.clj
@@ -1,6 +1,5 @@
-(defproject policymodellingtool "1.0.0-SNAPSHOT"
-  :description "Policy Modelling Tool for the IMPACT Policy project"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+(defproject policymodellingtool/policymodellingtool "1.0.0-SNAPSHOT" 
+  :dependencies [[org.clojure/clojure "[1.4.0,]"]
                  [org.clojure/data.json "0.1.1"]
                  [compojure "1.0.1" :exclusion [clojure]]
                  [hiccup "0.3.6"]
@@ -9,7 +8,9 @@
                  [carneades-engine "2.0.0-SNAPSHOT"]
                  [carneades-web-service "1.0.0-SNAPSHOT"]
                  [org.clojars.pallix/mygengo "1.0.0"]
-                 [lein-ring "0.5.4"]]
-  :dev-dependencies [[lein-ring "0.5.4"]]
-  :ring {:handler impact.web.routes-dev/impact-app}
-  :jvm-opts ["-Xdebug" "-Xrunjdwp:transport=dt_socket,address=jdbconn,server=y,suspend=n"])
+                 ;; [lein-ring "0.7.1"]
+                 ]
+  :plugins [[lein-ring "0.7.1"]]
+  :ring {:handler impact.web.routes-war/impact-app}
+  :min-lein-version "2.0.0"
+  :description "Policy Modelling Tool for the IMPACT Policy project")
