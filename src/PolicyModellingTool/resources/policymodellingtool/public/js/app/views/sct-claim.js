12870aefa4126f2c900ca1b218c92bf6248a660f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-01 16:13:35 +0200

    properly display claims

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/views/sct-claim.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/views/sct-claim.js
@@ -5,13 +5,14 @@ PM.SctClaim = Backbone.View.extend(
      events: {
          "change input" : "input_changed"
      },
-     
+
      initialize: function(attrs) {
+         this.hide_title = attrs.hide_title;
          this.lang = attrs.lang;
          this.model.on('change', this.render, this);
          _.bindAll(this, 'render', 'input_changed');
      },
-     
+
      render: function() {
          var data = this.model.toJSON();
          
@@ -24,19 +25,29 @@ PM.SctClaim = Backbone.View.extend(
               'sct_show_args': $.i18n.prop('sct_show_args'),
               'sct_skip_question': $.i18n.prop('sct_skip_question'),
               'sct_next': $.i18n.prop('sct_next'),
-              'claim_text': data.text[this.lang]
+              'claim_text': data.text[this.lang],
+              'id': this.cid
              };
-         
-         var content = ich['sct-claim'](claim_properties);
 
+         var content = ich['sct-claim'](claim_properties);
+         
          this.$el.html(content);
+         if(this.hide_title) {
+             this.$('h2').attr('hidden', true);
+         }
+         
+         if(data.pro.length == 0 && data.con.length == 0) {
+             this.$(".show-arguments").remove();
+         }
+
+         this.$('input:last').attr('checked', true);
          
          return this;
      },
-     
+
      input_changed: function() {
          return false;
      }
-     
+
     }
 );
