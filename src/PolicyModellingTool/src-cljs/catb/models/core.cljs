04c96218f52836149afc54535607b2349e1891de
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-23 17:14:30 +0200

    refactoring

--- src/PolicyModellingTool/src-cljs/catb/models/core.cljs
+++ src/PolicyModellingTool/src-cljs/catb/models/core.cljs
@@ -1,11 +1,21 @@
-(ns catb.models.arguments
+(ns catb.models.core
   (:use [jayq.util :only [log clj->js]])
   (:require [clojure.set :as set]
             [catb.models.similarity :as similarity]))
 
+(defn get-stmt
+  "Returns the statement model."
+  [statements id]
+  (.get statements id))
+
+(defn get-arg
+  "Returns the argument model."
+  [args id]
+  (.get args id))
+
 (defn- update-statements
   "Adds all the id of the premises to the statements
-indexed by the sources"
+indexed by the sources" ;; TODO: add conclusions
   [m sources premises]
   (let [ids (set (map (fn [p] (.-id (.-statement p))) premises))]
     (reduce (fn [m source]
@@ -51,3 +61,24 @@ accepted is the set of the accepted statements' ids"
                 (update-in index [v] conj source)
                 (assoc index v [source]))))
              {} statements))
+
+(defn arguments-for-statement
+  "Returns all descendant arguments models for a given JSON statement
+as a sequence."
+  [issue args]
+  (let [pro-statements
+        (mapcat (fn [proid]
+                  (map (fn [p]
+                         (.-statement p))
+                       (.get (.get args proid) "premises")))
+                (.-pro issue))
+        con-statements
+        (mapcat (fn [conid]
+                  (map (fn [p]
+                         (.-statement p))
+                       (.get (.get args conid) "premises")))
+                (.-con issue))]
+    (concat (map (partial get-arg args) (.-pro issue))
+            (map (partial get-arg args) (.-con issue))
+            (mapcat (fn [s] (arguments-for-statement s args)) pro-statements)
+            (mapcat (fn [s] (arguments-for-statement s args)) con-statements))))
