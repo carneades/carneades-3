ab7cf22a8b5f9f23e52ed12b02003e2ef839c645
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-23 17:44:40 +0200

    fix metadata models and collections

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/collections/metadata-list.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/collections/metadata-list.js
@@ -2,8 +2,16 @@ PM.MetadataList = Backbone.Collection.extend(
     {model: PM.Metadata,
 
      url: function() {
-         return IMPACT.wsurl + '/metadata/' + IMPACT.db;
-     }        
+         return IMPACT.wsurl + '/metadata/' + (this.db || IMPACT.db);
+     },
+     
+     initialize: function(attrs) {
+         if(attrs) {
+             this.db = attrs.db || IMPACT.db;    
+         } else {
+             attrs = IMPACT.db;
+         } 
+     }
 
     }
     
