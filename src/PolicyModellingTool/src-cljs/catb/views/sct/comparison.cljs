1a84de4d394e911cf167a3dc0498fd74d458c09d
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-23 17:14:30 +0200

    refactoring

--- src/PolicyModellingTool/src-cljs/catb/views/sct/comparison.cljs
+++ src/PolicyModellingTool/src-cljs/catb/views/sct/comparison.cljs
@@ -1,29 +1,28 @@
 (ns catb.views.sct.comparison
   (:use [catb.views.core :only [template json score-agreed?]]
-   [jayq.core :only [$ css inner attr val]]
-   [jayq.util :only [log clj->js]])
-    (:require [catb.backbone.core :as bb]
-              [catb.models.arguments :as args])
-    (:require-macros [catb.backbone.macros :as bb]))
+        [jayq.core :only [$ css inner attr val]]
+        [jayq.util :only [log clj->js]])
+  (:require [catb.backbone.core :as bb]
+            [catb.models.core :as model])
+  (:require-macros [catb.backbone.macros :as bb]))
 
 (bb/defview Comparison
   :className "sct-comparison"
   :render
   ([]
-     (bb/with-attrs [:statement-poll :arguments]
+     (bb/with-attrs [:statement-poll :statements :arguments :issue]
        (let [votes (bb/get-in statement-poll [:votes])
-             _ (log "votes")
-             _ (log votes)
              accepted-statements (set (map first
                                            (filter (fn [[id score]]
-                                                     (log "score =")
-                                                     (log score)
                                                      (score-agreed? score))
                                                    votes)))
-             statements (args/statements-by-sources (json arguments))
+             arguments-in-issue (model/arguments-for-statement issue arguments)
+             _ (log "arguments-in-issue")
+             _ (log (clj->js arguments-in-issue))
+             statements (model/statements-by-sources (map json arguments-in-issue))
              _ (log "accepted-statements")
              _ (log (clj->js accepted-statements))
-             sources (args/sources-by-similarity statements accepted-statements)]
+             sources (model/sources-by-similarity statements accepted-statements)]
          (log "sources by similarity")
          (log (clj->js sources))
          (template this :sct-comparison {})))))
