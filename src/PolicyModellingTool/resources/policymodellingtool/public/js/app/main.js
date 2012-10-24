2efcd807816f034230f469ec75d1da56f1648163
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-24 11:59:31 +0200

    formatting of the metadata for the comparison page

--- src/PolicyModellingTool/resources/policymodellingtool/public/js/app/main.js
+++ src/PolicyModellingTool/resources/policymodellingtool/public/js/app/main.js
@@ -220,7 +220,8 @@ PM.common_post_load = function() {
     PM.sct = new PM.Sct({db: IMPACT.debate_db,
                          lang: IMPACT.lang,
                          arguments: PM.debate_arguments,
-                         statements: PM.debate_statements});
+                         statements: PM.debate_statements,
+                         metadata: PM.debate_metadata});
 };
 
 // http://www.lockencreations.com/2011/07/02/cant-debug-imported-js-files-when-using-jquery-getscript/
--- src/PolicyModellingTool/src-cljs/catb/icanhaz/core.cljs
+++ src/PolicyModellingTool/src-cljs/catb/icanhaz/core.cljs
@@ -7,4 +7,6 @@
   "Returns the ICanHaz HTML template"
   [template-key variables]
   (let [tname (str/replace (clj->js template-key) "-" "_")]
+    (log "icanhaz variables")
+    (log (clj->js variables))
     (.call (aget js/ich tname) js/ich (clj->js variables))))
