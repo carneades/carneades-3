db3eb931bd309ab35a551e70b7122f035dc16439
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-24 11:59:31 +0200

    formatting of the metadata for the comparison page

--- src/PolicyModellingTool/src-cljs/catb/views/sct/comparison.cljs
+++ src/PolicyModellingTool/src-cljs/catb/views/sct/comparison.cljs
@@ -1,28 +1,43 @@
 (ns catb.views.sct.comparison
   (:use [catb.views.core :only [template json score-agreed?]]
+        [catb.models.core :only [get-metadata]]
         [jayq.core :only [$ css inner attr val]]
         [jayq.util :only [log clj->js]])
   (:require [catb.backbone.core :as bb]
             [catb.models.core :as model])
   (:require-macros [catb.backbone.macros :as bb]))
 
+(defn build-metadata
+  "Builds the metadata from the similarity map"
+  [sources metadata]
+  (reduce (fn [m [k v]]
+            (log "v =")
+            (log v)
+            (let [md (map (partial get-metadata metadata) v)
+                  _ (log "md")
+                  _ (log (clj->js md))
+                  md_data (map (fn [m]
+                                 {:metadata_text (js/AGB.format_linear_metadata m)})
+                               md)]
+              (assoc m k md_data)))
+             {} sources))
+
 (bb/defview Comparison
   :className "sct-comparison"
   :render
   ([]
-     (bb/with-attrs [:statement-poll :statements :arguments :issue]
+     (bb/with-attrs [:statement-poll :statements :arguments :issue :metadata]
        (let [votes (bb/get-in statement-poll [:votes])
              accepted-statements (set (map first
                                            (filter (fn [[id score]]
                                                      (score-agreed? score))
                                                    votes)))
              arguments-in-issue (model/arguments-for-statement issue arguments)
-             _ (log "arguments-in-issue")
-             _ (log (clj->js arguments-in-issue))
              statements (model/statements-by-sources (map json arguments-in-issue))
-             _ (log "accepted-statements")
-             _ (log (clj->js accepted-statements))
-             sources (model/sources-by-similarity statements accepted-statements)]
+             sources (model/sources-by-similarity statements accepted-statements)
+             variables (build-metadata sources metadata)]
          (log "sources by similarity")
          (log (clj->js sources))
-         (template this :sct-comparison {})))))
+         (log "variables")
+         (log (clj->js variables))
+         (template this :sct-comparison variables)))))
