b2fbe2bf9b09942523e30c131a549fc114fdf69d
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-10-26 15:02:58 +0200

    cleaned up build scripts

--- src/PolicyModellingTool/scripts/make-release-without-toolbox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### Changes the files to reflect a release configuration which includes the toolbox css
-
-perl -i -pe 's/impact.web.routes-[a-z]+/impact.web.routes-war/' project.clj
-perl -i -pe 's/debug: [a-z]+/debug: false/' ./resources/policymodellingtool/public/js/app/config.js
-
-
