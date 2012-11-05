e9a849fee30794b8bf89565f270f37af2eddd87c
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-11-05 12:06:37 +0100

    removed spocs examples from policies.clj

--- src/CarneadesExamples/src/carneades/examples/policies.clj
+++ src/CarneadesExamples/src/carneades/examples/policies.clj
@@ -4,9 +4,6 @@
  {:filename copyright_policies.clj
   :namespace carneades.examples.copyright-policies
   :name copyright-policies}
- {:filename spocs_policies.clj
-  :namespace carneades.examples.spocs-policies
-  :name spocs-policies}
  {:filename polish_tour_operator_insurance.clj
   :namespace carneades.examples.polish-tour-operator-insurance
   :name tour-operator-insurance }
