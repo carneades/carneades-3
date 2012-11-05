c75181059721a56dd3c84f53d14acc58fe83bd71
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-11-05 11:57:48 +0100

    Simplified build procedures

---
+++ src/PolicyModellingTool/scripts/build-all-and-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# build one standalone and one toolbox WAR and run tests
+set -e
+
+bash ./scripts/build.sh
+
+echo -e "\nBuilding WAR archives\n"
+
+cd ../CarneadesWebService
+lein ring uberwar
+
+cd ../PolicyModellingTool
+lein with-profile war ring uberwar
+
+echo -e "\nRunning tests\n"
+
+lein sub test
