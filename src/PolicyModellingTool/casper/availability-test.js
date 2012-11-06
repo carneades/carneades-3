f30767b83a2b952bdb72d2ad6a7f5d8369ac016a
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-11-06 12:22:14 +0100

    fix test

--- src/PolicyModellingTool/casper/availability-test.js
+++ src/PolicyModellingTool/casper/availability-test.js
@@ -5,10 +5,8 @@ casper.start('http://localhost:8080/policymodellingtool/#/introduction');
 casper.waitForSelector('#pm', 
                        function() {
                            this.test.assertTitleMatch(/Policy Modeling Tool/, 'Title is correct');
-    });
+                       });
 
 casper.run(function() {
                this.test.renderResults(true, 0, this.cli.get('save') || false);
            });
-
-casper.run();
