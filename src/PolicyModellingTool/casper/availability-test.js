d65a80091fa1825c7e21164c5ca1fe0e3a978c90
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-11-06 11:23:47 +0100

    added availability test

---
+++ src/PolicyModellingTool/casper/availability-test.js
@@ -0,0 +1,14 @@
+var casper = require('casper').create();
+
+casper.start('http://localhost:8080/policymodellingtool/#/introduction');
+
+casper.waitForSelector('#pm', 
+                       function() {
+                           this.test.assertTitleMatch(/Policy Modeling Tool/, 'Title is correct');
+    });
+
+casper.run(function() {
+               this.test.renderResults(true, 0, '/tmp/casper-log.xml');
+           });
+
+casper.run();
