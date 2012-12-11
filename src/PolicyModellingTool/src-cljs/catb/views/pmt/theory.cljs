d86a536920192f8ea363986213afa7c377053c07
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-12-11 17:57:59 +0100

    added copyright

--- src/PolicyModellingTool/src-cljs/catb/views/pmt/theory.cljs
+++ src/PolicyModellingTool/src-cljs/catb/views/pmt/theory.cljs
@@ -1,3 +1,6 @@
+;;; Copyright (c) 2012 Fraunhofer Gesellschaft
+;;; Licensed under the EUPL V.1.1
+
 (ns ^{:doc "Some utilities to format a statement"}
   catb.views.pmt.theory
   (:use [jayq.util :only [log clj->js]])
