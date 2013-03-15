efe2f8e91a56bc0e4c9248a8b573661a4dcb66df
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-03-15 16:46:35 +0100

    refactoring of the routes

--- src/CarneadesWebApp/src/carneades/analysis/web/routes_war.clj
+++ src/CarneadesWebApp/src/carneades/analysis/web/routes_war.clj
@@ -1,7 +1,7 @@
 ;;; Copyright (c) 2012 Fraunhofer Gesellschaft
 ;;; Licensed under the EUPL V.1.1
 
-(ns carneades.web.routes-war
+(ns carneades.analysis.web.routes-war
   (:use carneades.policy-analysis.web.routes
         compojure.core
         [hiccup.middleware :only (wrap-base-url)])
