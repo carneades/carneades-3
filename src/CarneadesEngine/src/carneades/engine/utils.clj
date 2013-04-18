543ac9cd7837ff83b6c83fd71070f5b316255912
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-04-18 15:53:55 +0200

    fix carneades.database.test.admin file

--- src/CarneadesEngine/src/carneades/engine/utils.clj
+++ src/CarneadesEngine/src/carneades/engine/utils.clj
@@ -222,6 +222,16 @@ greater than the length of s."
     (map (fn [[start end]]
            (subs s start end)) sparts)))
 
+(defn delete-file-recursively
+  "Delete file f. If it's a directory, recursively delete all its contents.
+Raise an exception if any deletion fails unless silently is true."
+  [f & [silently]]
+  (let [f (file f)]
+    (if (.isDirectory f)
+      (doseq [child (.listFiles f)]
+        (delete-file-recursively child silently)))
+    (delete-file f silently)))
+
 ;;; exceptions
 
 (defn first-cause
