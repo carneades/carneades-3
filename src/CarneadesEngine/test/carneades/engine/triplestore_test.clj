8d7d4a85f4d4dfaef095c4072f136128b1c37a20
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-04-22 17:55:39 +0200

    argument generator from triplestore working with non-grounded goals

--- src/CarneadesEngine/test/carneades/engine/triplestore_test.clj
+++ src/CarneadesEngine/test/carneades/engine/triplestore_test.clj
@@ -1,13 +1,29 @@
 (ns carneades.engine.triplestore-test
   (:use carneades.engine.triplestore
-        clojure.test)
+        clojure.test
+        clojure.pprint)
   (:require [carneades.engine.argument-generator :as generator]))
 
 (deftest test-generate-arguments-from-triplestore-grounded
   (let [url "http://dbpedia.org/sparql"
         goal '(dbpedia/Carneades rdf/type dbpedia-owl/Philosopher)
+        subs '{?a "a" ?b "b"}
         triplestore-generator (generate-arguments-from-triplestore url)
-        responses (generator/generate triplestore-generator goal #{})]
+        responses (generator/generate triplestore-generator goal subs)]
     (is (not (empty? responses)))
     (is (= (count responses) 1))
-    (is (= (-> (first responses) :argument :conclusion) goal))))
+    (is (= (-> (first responses) :argument :conclusion) goal))
+    (is (= (-> (first responses) :substitutions) subs))))
+
+(deftest test-generate-arguments-from-triplestore-notgrounded
+  (let [url "http://dbpedia.org/sparql"
+        goal '(dbpedia/Carneades rdf/type ?x)
+        subs '{?a "a" ?b "b"}
+        triplestore-generator (generate-arguments-from-triplestore url)
+        responses (generator/generate triplestore-generator goal subs)]
+    (pprint responses)
+    (is (not (empty? responses)))
+    (is (>= (count responses) 10))
+    (is (not= (:substitutions (first responses)) subs))))
+
+
