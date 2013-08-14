e393f8470139b3cfffa289fd6f0687f7953022a6
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-08-14 15:59:06 +0200

    functions to import an ontology from a project

--- src/CarneadesEngine/src/carneades/owl/import.clj
+++ src/CarneadesEngine/src/carneades/owl/import.clj
@@ -5,6 +5,7 @@
   (:refer-clojure :exclude [import])
   (:require [carneades.engine.statement :refer [literal-complement]]
             [carneades.engine.theory :as t]
+            [carneades.project.admin :as project]
             [carneades.owl.owl :as o])
   (:import [org.semanticweb.owlapi.model OWLLogicalAxiom AxiomType ClassExpressionType]))
 
@@ -317,9 +318,15 @@
     schemes))
 
 (defn import
+  "Imports an OWL ontology serialized in RDF/XML. Returns a map with keys :axioms and :language."
   ([url]
-     (let [ontology (o/load-ontology url)]
-       (ontology->schemes ontology)))
+     (import url "."))
   ([url importdir]
      (let [ontology (o/load-ontology url importdir)]
-       (ontology->schemes ontology))))
+       {:axioms (ontology->schemes ontology)})))
+
+(defn import-from-project
+  "Imports an ontology from a Carneades project."
+  [project ontology]
+  (import (project/absolute-ontology-path project ontology)
+          (project/absolute-theory-dir-path project)))
