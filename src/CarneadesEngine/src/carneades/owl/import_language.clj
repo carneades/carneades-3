651d687b3210e4f0fa1c228743d1d8310808af2a
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-08-20 14:44:49 +0200

    import of individuals into the language

--- src/CarneadesEngine/src/carneades/owl/import_language.clj
+++ src/CarneadesEngine/src/carneades/owl/import_language.clj
@@ -4,7 +4,8 @@
 (ns ^{:doc "Converter from an annotated ontology to a language."}
   carneades.owl.import-language
   (:require [clojure.string :as s]
-            [carneades.engine.utils :refer [safe-read-string]])
+            [carneades.engine.utils :refer [safe-read-string]]
+            [carneades.engine.theory :as t])
   (:import [org.semanticweb.owlapi.model IRI AxiomType]))
 
 (def ontology-iri "http://www.carneades.github.io/ontologies/carneades-annotations")
@@ -25,10 +26,10 @@
   [value]
   (safe-read-string (.getLiteral value)))
 
-(defn owl-class->symbol
-  "Converts an OWL class to a Clojure symbol."
-  [class]
-  (symbol (s/replace (str class) #"[<>]" "")))
+(defn owl-entity->symbol
+  "Converts an OWL entity to a Clojure symbol."
+  [entity]
+  (symbol (s/replace (str entity) #"[<>]" "")))
 
 (defn equivalent-classes-from-annotated-subject
   "Returns all the equivalent classes for an annotated subject."
@@ -36,21 +37,51 @@
   (let [sub (first (.getEntitiesInSignature ontology (IRI/create (str subject))))]
    (into #{(owl-class->symbol sub)} (map owl-class->symbol (.getEquivalentClasses sub ontology)))))
 
-(defn axiom->language-element
-  "Converts an OWL Annotation Assertion to a language element."
-  [ontology language axiom]
-  (if (= (.getAxiomType axiom) AxiomType/ANNOTATION_ASSERTION)
-    (let [subject (.getSubject axiom)
-          ann (.getAnnotation axiom)
-          prop (.getProperty ann)
-          value (.getValue ann)]
-      (if (carneades-annotation? ann)
-        (update-in language [(symbol (str subject))]
-                   assoc (ann-property->key prop) (ann-value->value value)
-                   :type (equivalent-classes-from-annotated-subject ontology subject))
-        language))
-    language))
+;; (defn axiom->language-element
+;;   "Converts an OWL Annotation Assertion to a language element."
+;;   [ontology language axiom]
+;;   (if (= (.getAxiomType axiom) AxiomType/ANNOTATION_ASSERTION)
+;;     (let [subject (.getSubject axiom)
+;;           ann (.getAnnotation axiom)
+;;           prop (.getProperty ann)
+;;           value (.getValue ann)]
+;;       (if (carneades-annotation? ann)
+;;         (update-in language [(symbol (str subject))]
+;;                    assoc (ann-property->key prop) (ann-value->value value)
+;;                    :type (equivalent-classes-from-annotated-subject ontology subject))
+;;         language))
+;;     language))
+
+(defn add-ann-to-language-element
+  [element ann]
+  (concat element
+         [(ann-property->key (.getProperty ann))
+          (ann-value->value (.getValue ann))]))
+
+(defn anns->language-elements
+  "Converts OWL Annotations to a language element."
+  [annotations]
+  (reduce add-ann-to-language-element () annotations))
+
+(defn add-individual
+  [ontology language individual]
+  (let [anns (.getAnnotations individual ontology)]
+    (if (empty? anns)
+      language
+      (let [idv (apply t/make-individual
+                       :symbol (owl-entity->symbol individual)
+                       (anns->language-element anns))]
+        (assoc language (:symbol idv) idv)))))
+
+(defn add-individuals
+  [ontology language]
+  (reduce (partial add-individual ontology)
+          language
+          (.getIndividualsInSignature ontology)))
 
 (defn ontology->language
   [ontology]
-  (reduce (partial axiom->language-element ontology) {} (.getAxioms ontology)))
+  ;; (reduce (partial axiom->language-element ontology) {} (.getAxioms ontology))
+  (prn (->> {}
+            (add-individuals ontology ,,,)))
+  )
