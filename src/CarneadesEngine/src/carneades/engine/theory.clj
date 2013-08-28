7bd9e76f99871b3635c3d22a82a0c551990e4b85
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-08-28 14:18:38 +0200

    apply namespace when constructing a theory

--- src/CarneadesEngine/src/carneades/engine/theory.clj
+++ src/CarneadesEngine/src/carneades/engine/theory.clj
@@ -11,17 +11,18 @@
         carneades.engine.argument
         carneades.engine.argument-generator
         carneades.engine.dublin-core
-        [carneades.engine.utils :only (mapinterleave)]))
+        [carneades.engine.utils :only (mapinterleave)])
+  (:require [carneades.engine.theory.namespace :as namespace]))
 
 (defrecord Form
     [positive     ; string for the positive sentences
      negative     ; string for the negative sentences
      question])   ; string for questions
 
-(defn make-form 
+(defn make-form
   "key value ... -> form"
-  [& key-values]  
-  (merge (Form. 
+  [& key-values]
+  (merge (Form.
           ""    ; positive
           ""    ; negative
           "")   ; question
@@ -73,7 +74,7 @@ call or a symbol."
   "key value ... -> individual"
   [& key-values]
   {:post [(instance? Individual %)]}
-  (merge (Individual. 
+  (merge (Individual.
           (gensym "i")    ; symbol
           {})             ; text map
          (apply hash-map key-values)))
@@ -90,16 +91,16 @@ call or a symbol."
 
 (defn make-function
   "key value ... -> function"
-  [& key-values]  
-  (merge (Function. 
+  [& key-values]
+  (merge (Function.
           (gensym "f")    ; symbol
           0               ; arity
           {})             ; text map
          (apply hash-map key-values)))
 
 (defn function? [x] (instance? Function x))
-     
-(defrecord Predicate 
+
+(defrecord Predicate
     [symbol   ; symbol
      arity    ; integer
      askable  ; boolean
@@ -114,8 +115,8 @@ call or a symbol."
 
 (defn make-predicate
   "key value ... -> predicate"
-  [& key-values]  
-  (merge (Predicate. 
+  [& key-values]
+  (merge (Predicate.
           (gensym "p")                 ; symbol
           0                            ; arity
           false                        ; askable
@@ -146,8 +147,8 @@ call or a symbol."
 
 (defn make-concept
   "key value ... -> class"
-  [& key-values]  
-  (merge (Concept. 
+  [& key-values]
+  (merge (Concept.
           (gensym "c")    ; symbol
           nil             ; category
           false           ; askable
@@ -187,7 +188,7 @@ call or a symbol."
 (defn make-role
   "key value ... -> role"
   [& key-values]
-  (merge (Role. 
+  (merge (Role.
           (gensym "p")                 ; symbol
           1                            ; min
           1                            ; max
@@ -221,7 +222,7 @@ call or a symbol."
   (zipmap (map get-symbol functors) functors))
 
 ;; variables are allowed as conclusions of schemes to enable them
-;; to represent argumentation schemes, such as arguments from 
+;; to represent argumentation schemes, such as arguments from
 ;; expert witness testimony, whose conclusions can be any (literal) proposition.
 
 (defrecord Scheme
@@ -231,17 +232,17 @@ call or a symbol."
      pro           ; boolean; generates con arguments if false
      strict        ; boolean, defeasible if false
      weight        ; nil or number in the range 0.0 to 1.0
-     premises      ; premise sequence 
+     premises      ; premise sequence
      exceptions    ; premise sequence
      assumptions]) ; premise sequence
 
-                                        ; The scope of scheme ids is local to their section in the theory. 
+                                        ; The scope of scheme ids is local to their section in the theory.
 
 (defn make-scheme
   "key value ... -> scheme"
-  [& key-values]  
-  (let [m  (-> (merge (Scheme. 
-                       (gensym "s")    ; id 
+  [& key-values]
+  (let [m  (-> (merge (Scheme.
+                       (gensym "s")    ; id
                        nil             ; header
                        nil             ; conclusion
                        true            ; pro
@@ -262,13 +263,13 @@ call or a symbol."
 
 (defn scheme-conclusion-literal
   "scheme -> literal
-   Returns the conclusion of the scheme as a positive 
+   Returns the conclusion of the scheme as a positive
    literal, if the scheme is pro, or negative literal,
    if the scheme gument is con."
   [s]
   {:pre [(scheme? s)]}
-  (if (:pro s) 
-    (:conclusion s) 
+  (if (:pro s)
+    (:conclusion s)
     (literal-complement (:conclusion s))))
 
 (defn scheme-variables
@@ -280,9 +281,9 @@ call or a symbol."
                             (:exceptions scheme)
                             (:assumptions scheme)))))
 
-;; When applying schemes, undercutters are generated from the exceptions of schemes, 
+;; When applying schemes, undercutters are generated from the exceptions of schemes,
 ;; where the undercutters are arguments with the form:
-;; (make-argument 
+;; (make-argument
 ;;   :conclusion '(undercut <arg-id>)
 ;;   :premises [<exception>])
 
@@ -299,7 +300,7 @@ call or a symbol."
       :premises (map (fn [p] (assoc p :statement (apply-subs (:statement p))))
                      (:premises scheme)),
       :exceptions (map (fn [p] (assoc p :statement (apply-subs (:statement p))))
-                       (:exceptions scheme)),        
+                       (:exceptions scheme)),
       :assumptions (map (fn [p] (assoc p :statement (apply-subs (:statement p))))
                         (:assumptions scheme)))))
 
@@ -320,9 +321,9 @@ call or a symbol."
                  :scheme (:scheme arg))))
        (:exceptions arg)))
 
-(defn instantiate-scheme 
+(defn instantiate-scheme
   "scheme map -> (seq-of response)
-   Constructs a sequence of grounds arguments, in responses, by 
+   Constructs a sequence of grounds arguments, in responses, by
    substituting variables in the scheme with their values in a map of
    substitutions. Only ground arguments are included in the result. It
    is the responsibility of the caller to provide substitutions for all
@@ -331,8 +332,8 @@ call or a symbol."
    the scheme."
   [scheme subs]
   (let [conclusion (apply-substitutions subs (:conclusion scheme))
-        main-arg (instantiate-argument 
-                  (make-argument 
+        main-arg (instantiate-argument
+                  (make-argument
                    :conclusion (literal-atom conclusion),
                    :pro (literal-pos? conclusion)
                    :strict (:strict scheme),
@@ -346,7 +347,7 @@ call or a symbol."
     (if (not (ground-argument? main-arg))
       ()
       (cons
-       (make-response 
+       (make-response
         subs
         (map (fn [p] (if (:positive p)
                        (:statement p)
@@ -356,12 +357,12 @@ call or a symbol."
        (generate-exceptions main-arg)))))
 
 
-(defn axiom 
+(defn axiom
   "literal -> scheme"
   [literal]
-  (make-scheme 
-   :strict true 
-   :conclusion (literal-atom literal) 
+  (make-scheme
+   :strict true
+   :conclusion (literal-atom literal)
    :pro (literal-pos? literal)))
 
                                         ; The scope of section ids is their theory
@@ -370,12 +371,12 @@ call or a symbol."
     [id          ; symbol
      header      ; nil or a dublin core metadata structure about this model
      schemes     ; sequence of schemes
-     sections])  ; sequence of sections; i.e. subsections    
+     sections])  ; sequence of sections; i.e. subsections
 
 (defn make-section
   "key value ... -> section"
-  [& key-values]  
-  (merge (Section. 
+  [& key-values]
+  (merge (Section.
           (gensym "sect-")    ; id
           nil             ; header
           []              ; schemes
@@ -402,7 +403,7 @@ call or a symbol."
         [m2 premises] (rename-variables-in-premises m1 (:premises scheme)),
         [m3 exceptions] (rename-variables-in-premises m2 (:exceptions scheme)),
         [m4 assumptions] (rename-variables-in-premises m3 (:assumptions scheme))]
-    (assoc scheme 
+    (assoc scheme
       :conclusion conclusion
       :premises premises
       :exceptions exceptions
@@ -418,15 +419,16 @@ call or a symbol."
 
 (defn make-theory
   "key value ... -> theory"
-  [& key-values]  
-  (merge (Theory. 
-          nil             ; header
-          {}              ; namespaces
-          {}              ; language
-          []              ; schemes
-          []              ; sections
-          {})             ; references
-         (apply hash-map key-values)))
+  [& key-values]
+  (-> (Theory.
+       nil                          ; header
+       {}                           ; namespaces
+       {}                           ; language
+       []                           ; schemes
+       []                           ; sections
+       {})
+      (merge ,,, (apply hash-map key-values))
+      (namespace/to-absolute-theory ,,,)))
 
 (defn theory? [x] (instance? Theory x))
 
@@ -438,9 +440,9 @@ call or a symbol."
 ;; ensure we don't load twice the same namespace
 (memoize load-theory)
 
-(defn- scheme-index-key 
-  "term -> symbol 
-   Returns the symbol used to index a scheme by its conclusions 
+(defn- scheme-index-key
+  "term -> symbol
+   Returns the symbol used to index a scheme by its conclusions
    for quicker retrieval. By default schemes are indexed under :other."
   [trm]
   {:pre [(term? trm)]}
@@ -461,7 +463,7 @@ call or a symbol."
                        (assoc map2
                          (scheme-index-key (:conclusion scheme))
                          (conj (get map2 (scheme-index-key (:conclusion scheme)))
-                               scheme)))      
+                               scheme)))
                      map1
                      (:schemes part))]
     (reduce (fn [map3 section]
@@ -476,7 +478,7 @@ call or a symbol."
                                         ; (println "section: " section)
   (let [map2 (reduce (fn [map2 scheme]
                                         ; (println "scheme: " scheme)
-                       (assoc map2 (:id scheme) scheme))      
+                       (assoc map2 (:id scheme) scheme))
                      map1
                      (:schemes part))]
     (reduce (fn [map3 section]
@@ -484,16 +486,16 @@ call or a symbol."
             map2
             (:sections part))))
 
-(defn get-schemes 
+(defn get-schemes
   "map goal substititions boolean -> sequence of schemes
    where the goal is a literal. If 'other' is false,
    does not return :other schemes, which match every goal
-   and are not well controlled when using with backward 
+   and are not well controlled when using with backward
    chaining."
   ([index goal subs]
      (get-schemes index goal subs false))
   ([index goal subs other]
-     {:pre [(map? index) 
+     {:pre [(map? index)
             (literal? goal)
             (map? subs)]}
      (let [key (scheme-index-key (apply-substitutions subs goal))]
@@ -524,7 +526,7 @@ call or a symbol."
                          :weight (:weight scheme),
                          :premises (concat (:premises scheme)
                                            (:assumptions scheme)),
-                         :exceptions (:exceptions scheme), 
+                         :exceptions (:exceptions scheme),
                          :scheme `(~(:id scheme)
                                    ~@(apply-substitutions subs2 (scheme-variables scheme)))))]))))
 
@@ -546,4 +548,3 @@ call or a symbol."
         (mapinterleave
          (fn [s] (apply-scheme s goal subs))
          (map rename-scheme-variables (get-schemes index goal subs)))))))
-
