ffcb930489a64eb7f62df926319bb8f5a2f8356a
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-09-02 17:42:50 +0200

    configuration of log4j

--- src/CarneadesEngine/project.clj
+++ src/CarneadesEngine/project.clj
@@ -4,7 +4,6 @@
 (defproject carneades-engine/carneades-engine "2.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.3"]
                  [org.clojars.pallix/owlapi "3.0.0"]
                  [org.clojars.pallix/hermit "1.2.4"]
                  [org.clojure/math.combinatorics "0.0.2"]
@@ -14,7 +13,9 @@
                  [org.clojure/tools.trace "0.7.1"]
                  [edu.ucdenver.ccp/kr-sesame-core "1.4.5"]
                  [lacij "0.8.1"]
-                 [me.raynes/fs "1.4.0"]]
+                 [me.raynes/fs "1.4.0"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]]
   :aot [carneades.engine.argument-evaluation]
   :description "Carneades is an argument mapping application, with a graphical \nuser interface, and a software library for building applications supporting \nvarious argumentation tasks. This is the software library (the engine)."
   :cljsbuild {:builds []})
