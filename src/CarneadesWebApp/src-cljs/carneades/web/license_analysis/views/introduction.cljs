c1393f9d7dd4f177bfd61e18817fb5cccfea1b35
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-09-09 17:38:07 +0200

    basic page for the introduction to the license analysis

---
+++ src/CarneadesWebApp/src-cljs/carneades/web/license_analysis/views/introduction.cljs
@@ -0,0 +1,45 @@
+(ns carneades.web.license-analysis.views.introduction
+  (:require [jayq.core :refer [$ inner attr append]]
+            [jayq.util :refer [log]]
+            [carneades.analysis.web.template :as tp]
+            [carneades.analysis.web.views.header :as header]
+            [carneades.analysis.web.dispatch :as dispatch]
+            [carneades.web.license-analysis.views.debug.facts :as facts]))
+
+;; http://<markos-server-domain>/markos/#/license-analysis/introduction?entity=<project-uri>
+
+(defn start-facts-gathering
+  [msg]
+  (let [entity (:entity msg)]
+    (js/PM.ajax_post (str js/IMPACT.license_analysis_wsurl "/debug/analyse")
+                     (clj->js msg)
+                     (fn [data]
+                       (js/PM.busy_cursor_off)
+                       (log "start-facts-gathering, received question")
+                       (log data)
+                       (facts/show data))
+                     js/IMPACT.user
+                     js/IMPACT.password
+                     js/PM.on_error)))
+
+(dispatch/react-to #{:license-analysis-start-facts-gathering}
+                   (fn [_ msg] (start-facts-gathering msg)))
+
+(defn on-start-analysis
+  []
+  (let [entity (.parameter js/jQuery.address "entity")]
+    (log "entity=" entity)
+    (dispatch/fire :license-analysis-start-facts-gathering {:entity entity}))
+  false)
+
+(defn attach-listeners
+  []
+  (.click ($ "#start") on-start-analysis))
+
+(defn ^:export show
+  [project]
+  (js/PM.load_project project)
+  (header/show {:text :home
+                :link "#/home"})
+  (inner ($ ".content") (tp/get "license_introduction" {}))
+  (attach-listeners))
