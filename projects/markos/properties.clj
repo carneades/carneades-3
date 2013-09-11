4d11e9f55f98abd14269a211f3861df15691ace0
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-09-11 15:41:05 +0200

    loading of the triplestore and repo-name from the properties file

--- projects/markos/properties.clj
+++ projects/markos/properties.clj
@@ -3,6 +3,8 @@
  :policies "oss_licensing_theory",
  :schemes "default/walton_schemes",
  :tools ["arguments" "tour" "policies"],
+ :triplestore "http://markos.man.poznan.pl/openrdf-sesame"
+ :repo-name "markos_test_26-07-2013"
  :description
  {:en
   "The [MARKOS project](http://www.markosproject.eu/) is aimed to realize the prototype of a service and an interactive application providing an integrated view on the Open Source projects available the on web, focusing on functional, structural and licenses aspects of software code"}}
