d3fb00714a8e018c7ff7a2be218dd0f28ff8ec18
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-09-18 17:40:35 +0200

    temporary commented out not compiling code

--- src/CarneadesWebApp/src/carneades/web/license_analysis/model/debug_analysis.clj
+++ src/CarneadesWebApp/src/carneades/web/license_analysis/model/debug_analysis.clj
@@ -24,65 +24,69 @@
 (defn query
   "Returns the result of query in the triplestore"
   [endpoint repo-name query limit]
-  (let [conn (triplestore/make-conn endpoint
-                                    repo-name
-                                    analysis/markos-namespaces)
-        sexp (unserialize-atom query)]
-    (prn "sexp=" sexp)
-    (try
-      (binding [sparql/*select-limit* limit]
-        {:result (pp/write (sparql/query (:kb conn) sexp)
-                           :stream nil)})
-      (catch Exception e
-        {:result (.getMessage e)}))))
+  ;; TODO: update
+  ;; (let [conn (triplestore/make-conn endpoint
+  ;;                                   repo-name
+  ;;                                   analysis/markos-namespaces)
+  ;;       sexp (unserialize-atom query)]
+  ;;   (prn "sexp=" sexp)
+  ;;   (try
+  ;;     (binding [sparql/*select-limit* limit]
+  ;;       {:result (pp/write (sparql/query (:kb conn) sexp)
+  ;;                          :stream nil)})
+  ;;     (catch Exception e
+  ;;       {:result (.getMessage e)})))
+  )
 
 (defn ask
   "Returns the ask result of query in the triplestore"
   [endpoint repo-name query limit]
-  (let [conn (triplestore/make-conn endpoint
-                                    repo-name
-                                    analysis/markos-namespaces)
-        sexp (unserialize-atom query)]
-    (prn "sexp=" sexp)
-    (try
-      (binding [sparql/*select-limit* limit]
-        {:result (pp/write (sparql/ask (:kb conn) sexp)
-                           :stream nil)})
-      (catch Exception e
-        {:result (.getMessage e)}))))
+  ;; (let [conn (triplestore/make-conn endpoint
+  ;;                                   repo-name
+  ;;                                   analysis/markos-namespaces)
+  ;;       sexp (unserialize-atom query)]
+  ;;   (prn "sexp=" sexp)
+  ;;   (try
+  ;;     (binding [sparql/*select-limit* limit]
+  ;;       {:result (pp/write (sparql/ask (:kb conn) sexp)
+  ;;                          :stream nil)})
+  ;;     (catch Exception e
+  ;;       {:result (.getMessage e)})))
+  )
 
 (defn debug-start-engine
   [params]
   (prn "params=")
   (prn params)
-  (let [{:keys [project theories entity query repo-name endpoint ag-name]} params
-        sexp (unserialize-atom query)
-        loaded-theories (project/load-theory project theories)
-        [argument-from-user-generator questions send-answer]
-        (ask/make-argument-from-user-generator (fn [p] (questions/askable? loaded-theories p)))
-        ag (analysis/get-ag project ag-name)
-        engine (shell/make-engine ag 500 #{}
-                                  (list
-                                   (triplestore/generate-arguments-from-triplestore endpoint
-                                                                                    repo-name
-                                                                                    analysis/markos-namespaces)
-                                   (theory/generate-arguments-from-theory loaded-theories)
-                                   argument-from-user-generator))
-        future-ag (future (shell/argue engine sexp))
-        analysis {:ag nil
-                  :project project
-                  :uuid (symbol (uuid/make-uuid-str))
-                  :lang :en
-                  :query sexp
-                  :policies loaded-theories
-                  :future-ag future-ag
-                  :questions questions
-                  :send-answer send-answer
-                  :dialog (dialog/make-dialog)
-                  :last-id 0}
-        analysis (policy/get-ag-or-next-question analysis)]
-    (swap! analysis/state analysis/index-analysis analysis)
-    (analysis/build-response analysis)))
+  ;; (let [{:keys [project theories entity query repo-name endpoint ag-name]} params
+  ;;       sexp (unserialize-atom query)
+  ;;       loaded-theories (project/load-theory project theories)
+  ;;       [argument-from-user-generator questions send-answer]
+  ;;       (ask/make-argument-from-user-generator (fn [p] (questions/askable? loaded-theories p)))
+  ;;       ag (analysis/get-ag project ag-name)
+  ;;       engine (shell/make-engine ag 500 #{}
+  ;;                                 (list
+  ;;                                  (triplestore/generate-arguments-from-triplestore endpoint
+  ;;                                                                                   repo-name
+  ;;                                                                                   analysis/markos-namespaces)
+  ;;                                  (theory/generate-arguments-from-theory loaded-theories)
+  ;;                                  argument-from-user-generator))
+  ;;       future-ag (future (shell/argue engine sexp))
+  ;;       analysis {:ag nil
+  ;;                 :project project
+  ;;                 :uuid (symbol (uuid/make-uuid-str))
+  ;;                 :lang :en
+  ;;                 :query sexp
+  ;;                 :policies loaded-theories
+  ;;                 :future-ag future-ag
+  ;;                 :questions questions
+  ;;                 :send-answer send-answer
+  ;;                 :dialog (dialog/make-dialog)
+  ;;                 :last-id 0}
+  ;;       analysis (policy/get-ag-or-next-question analysis)]
+  ;;   (swap! analysis/state analysis/index-analysis analysis)
+  ;;   (analysis/build-response analysis))
+  )
 
 (defn analyse
   "Begins an analysis of a given software entity. The theories inside project is used.
