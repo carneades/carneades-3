ea15412a37fa6d1a0f29b1da5783597fbf0e1af4
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-09-18 17:27:41 +0200

    fix invalid dependencies

--- src/CarneadesExamples/project.clj
+++ src/CarneadesExamples/project.clj
@@ -1,10 +1,10 @@
 ;;; Copyright (c) 2012 Fraunhofer Gesellschaft
 ;;; Licensed under the EUPL V.1.1
 
-(defproject carneades-examples/carneades-examples "2.0.0-SNAPSHOT" 
+(defproject carneades-examples/carneades-examples "2.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [carneades-engine/carneades-engine "2.0.0-SNAPSHOT"]
+                 [carneades/carneades-engine "2.0.0"]
                  ]
   :description "Carneades is an argument mapping application, with a graphical \nuser interface, and a software library for building applications supporting \nvarious argumentation tasks. These are some examples."
   :cljsbuild {:builds []})
