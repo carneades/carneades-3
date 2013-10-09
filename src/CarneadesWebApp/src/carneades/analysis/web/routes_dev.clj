ccbd1fd2a36e31cb8c66a63ce7f86d63dd953d47
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-10-09 16:56:53 +0200

    better logging configuration

--- src/CarneadesWebApp/src/carneades/analysis/web/routes_dev.clj
+++ src/CarneadesWebApp/src/carneades/analysis/web/routes_dev.clj
@@ -14,7 +14,8 @@
         [ring.middleware.json :only [wrap-json-params]]
         [ring.middleware.keyword-params :only [wrap-keyword-params]]
         [ring.middleware.stacktrace :only [wrap-stacktrace]]
-        [carneades.web.license-analysis.routes.service :only [license-analysis-routes]])
+        [carneades.web.license-analysis.routes.service :only [license-analysis-routes]]
+        [clj-logging-config.log4j :refer :all])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]))
 
@@ -22,6 +23,18 @@
   (context "/carneades" [] policy-analysis-routes)
   (context "/carneades/license-analysis" [] (wrap-restful-response license-analysis-routes))
   (context "/carneadesws" [] (wrap-restful-response carneades-web-service-routes)))
+
+(set-logger! :pattern "[%c]%n%m%n" :level :debug)
+
+(set-loggers! "org.mortbay.log" {:level :info}
+              "httpclient.wire.header" {:level :info}
+              "org.apache.commons.httpclient.HttpMethodBase" {:level :info}
+              "org.apache.commons.httpclient.methods.EntityEnclosingMethod" {:level :info}
+              "org.openrdf.http.client.HTTPClient" {:level :info}
+              "org.apache.commons.httpclient.HttpConnection" {:level :info}
+              "org.apache.commons.httpclient.MultiThreadedHttpConnectionManager" {:level :info}
+              "org.apache.commons.httpclient.util.IdleConnectionHandler" {:level :info}
+              "org.xml.sax.XMLReader" {:level :info})
 
 (def carneades-webapp
   (-> carneades-webapp-routes
