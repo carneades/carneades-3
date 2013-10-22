02b23a8f6d7c7fa56b6a97ebd743e3cc863a4787
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-10-22 16:04:11 +0200

    ported admin-test to midje

--- src/CarneadesEngine/test/carneades/project/admin_test.clj
+++ src/CarneadesEngine/test/carneades/project/admin_test.clj
@@ -1,43 +1,42 @@
 (ns carneades.project.admin-test
-  (:use [clojure.test :only [deftest is use-fixtures]]
-        [carneades.engine.uuid :only [make-uuid-str]]
-        [carneades.engine.utils :only [file-separator]])
   (:require [carneades.project.admin :as project]
-            [me.raynes.fs :as fs]))
+            [me.raynes.fs :as fs]
+            [carneades.engine.uuid :refer [make-uuid-str]]
+            [carneades.engine.utils :refer [file-separator]]
+            [midje.sweet :refer :all]))
 
-(def project-name (str "testproject-" (make-uuid-str)))
+(def state (atom nil))
+
+(defn initial-state-value
+  []
+  {:project-name (str "testproject-" (make-uuid-str))})
 
 (defn create-tmp-project
   []
-  (project/create-project project-name))
+  (reset! state (initial-state-value))
+  (project/create-project (:project-name @state)))
 
 (defn delete-tmp-project
   []
-  (project/delete-project project-name))
+  (project/delete-project (:project-name @state)))
 
-(defn db-fixture [x]
-  (create-tmp-project)
-  (x)
-  (delete-tmp-project))
-
-(use-fixtures :once db-fixture)
-
-
-(deftest relative-theory-path
-  (is (= "copyright/theories/copyright_policies.clj"
-         (project/relative-theory-path "copyright"
-                                       "copyright/copyright_policies")))
-  (is (= "copyright/theories/copyright_policies.clj"
-         (project/relative-theory-path "copyright"
-                                       "copyright_policies")))
-  (is (= "default/theories/walton_scheme.clj"
-         (project/relative-theory-path "copyright" "default/walton_scheme"))) )
-
-(deftest import-document
-  (let [docfile (fs/temp-file "doc-")]
-    (spit docfile (str "content-" (make-uuid-str)))
-    (project/import-document project-name (.getPath docfile) (.getName docfile))
-    (is (fs/exists? (str project/projects-directory file-separator
-                         project-name file-separator
-                         project/documents-directory file-separator
-                         (.getName docfile))))))
+(with-state-changes [(before :facts (create-tmp-project))
+                     (after :facts (delete-tmp-project))]
+  (fact "The relative-path functions works."
+        (expect (project/relative-theory-path "copyright" "copyright/copyright_policies")
+                => "copyright/theories/copyright_policies.clj")
+        (expect (project/relative-theory-path "copyright"
+                                               "copyright_policies")
+                 => "copyright/theories/copyright_policies.clj")
+        (expect (project/relative-theory-path "copyright" "default/walton_scheme")
+                => "default/theories/walton_scheme.clj"))
+  (fact "Importing a document works"
+        (let [docfile (fs/temp-file "doc-")
+              project-name (:project-name @state)]
+          (spit docfile (str "content-" (make-uuid-str)))
+          (project/import-document project-name (.getPath docfile) (.getName docfile))
+          (expect (fs/exists? (str project/projects-directory file-separator
+                               project-name file-separator
+                               project/documents-directory file-separator
+                               (.getName docfile)))
+                  => true))))
