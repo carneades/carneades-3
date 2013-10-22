4c57e07d65c32353f437f3281b71c1e9cf551013
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-10-22 15:44:08 +0200

    ported namespace_test to midje

--- src/CarneadesEngine/test/carneades/engine/theory/namespace_test.clj
+++ src/CarneadesEngine/test/carneades/engine/theory/namespace_test.clj
@@ -1,37 +1,37 @@
 (ns carneades.engine.theory.namespace-test
-  (:require [clojure.test :refer :all]
-            [carneades.engine.theory.namespace :as tn]))
+  (:require [carneades.engine.theory.namespace :as tn]
+            [midje.sweet :refer :all]))
 
-(deftest test-to-literal-absolute
-  (let [sexp1 '(a b c)
-        sexp2 '(owl:a (owl:b rdfs:c d))]
-    (is (= sexp1 (tn/to-absolute-literal sexp1 {})))
-    (is (= '(http://www.markosproject.eu/ontologies/copyright#a
-             http://www.markosproject.eu/ontologies/copyright#b
-             http://www.markosproject.eu/ontologies/copyright#c)
-           (tn/to-absolute-literal sexp1 {"" "http://www.markosproject.eu/ontologies/copyright#"})))
-    (is (= '(http://www.w3.org/2002/07/owl#a
-             (http://www.w3.org/2002/07/owl#b
-              http://www.w3.org/2000/01/rdf-schema#c
-              http://www.markosproject.eu/ontologies/copyright#d))
-           (tn/to-absolute-literal sexp2 {"" "http://www.markosproject.eu/ontologies/copyright#"
-                                          "owl" "http://www.w3.org/2002/07/owl#",
-                                          "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
-                                          })))
-    (is (= '(http://www.w3.org/2002/07/owl#a
-             (http://www.w3.org/2002/07/owl#b
-              http://www.w3.org/2000/01/rdf-schema#c
-              d))
-           (tn/to-absolute-literal sexp2 {"owl" "http://www.w3.org/2002/07/owl#",
-                                          "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
-                                          })))
-    (is (= '(http://www.w3.org/2002/07/owl#a
-             (http://www.w3.org/2002/07/owl#b
-              http://www.w3.org/2000/01/rdf-schema#c))
-           (tn/to-absolute-literal '(http://www.w3.org/2002/07/owl#a
-                                    (http://www.w3.org/2002/07/owl#b
-                                     http://www.w3.org/2000/01/rdf-schema#c))
-                                   {"owl" "http://www.w3.org/2002/07/owl#",
-                                    "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
-                                    "" "http://something"
-                                    })))))
+(fact "to-literal absolute works."
+      (let [sexp1 '(a b c)
+            sexp2 '(owl:a (owl:b rdfs:c d))]
+        (expect (tn/to-absolute-literal sexp1 {}) => sexp1)
+        (expect (tn/to-absolute-literal sexp1 {"" "http://www.markosproject.eu/ontologies/copyright#"})
+                => '(http://www.markosproject.eu/ontologies/copyright#a
+                     http://www.markosproject.eu/ontologies/copyright#b
+                     http://www.markosproject.eu/ontologies/copyright#c))
+        (expect (tn/to-absolute-literal sexp2 {"" "http://www.markosproject.eu/ontologies/copyright#"
+                                               "owl" "http://www.w3.org/2002/07/owl#",
+                                               "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
+                                               })
+                => '(http://www.w3.org/2002/07/owl#a
+                     (http://www.w3.org/2002/07/owl#b
+                      http://www.w3.org/2000/01/rdf-schema#c
+                      http://www.markosproject.eu/ontologies/copyright#d)))
+        (expect (tn/to-absolute-literal sexp2 {"owl" "http://www.w3.org/2002/07/owl#",
+                                               "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
+                                               })
+                => '(http://www.w3.org/2002/07/owl#a
+                     (http://www.w3.org/2002/07/owl#b
+                      http://www.w3.org/2000/01/rdf-schema#c
+                      d)))
+        (expect (tn/to-absolute-literal '(http://www.w3.org/2002/07/owl#a
+                                          (http://www.w3.org/2002/07/owl#b
+                                           http://www.w3.org/2000/01/rdf-schema#c))
+                                        {"owl" "http://www.w3.org/2002/07/owl#",
+                                         "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
+                                         "" "http://something"
+                                         })
+                => '(http://www.w3.org/2002/07/owl#a
+                     (http://www.w3.org/2002/07/owl#b
+                      http://www.w3.org/2000/01/rdf-schema#c)))))
