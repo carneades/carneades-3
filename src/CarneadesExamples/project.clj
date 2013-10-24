6aff8abd02c0ebe5a524c30b91ba26c484ca2046
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-10-24 15:33:25 +0200

    upgrade from clojure 1.4.0 to clojure 1.5.1

--- src/CarneadesExamples/project.clj
+++ src/CarneadesExamples/project.clj
@@ -3,8 +3,8 @@
 
 (defproject carneades-examples/carneades-examples "2.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [carneades/carneades-engine "2.0.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [carneades/carneades-engine "2.0.1"]
                  ]
   :description "Carneades is an argument mapping application, with a graphical \nuser interface, and a software library for building applications supporting \nvarious argumentation tasks. These are some examples."
   :cljsbuild {:builds []})
