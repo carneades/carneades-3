f8ec68736925b0f2581448b0b3defea0838f059b
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-10-30 11:52:23 +0100

    removed outdated desktop editor

--- src/CarneadesEditor/test/CarneadesGraphEditor/core_test.clj
+++
@@ -1,9 +0,0 @@
-;;; Copyright © 2010 Fraunhofer Gesellschaft 
-;;; Licensed under the EUPL V.1.1
-
-(ns CarneadesGraphEditor.core-test
-  (:use [CarneadesGraphEditor.core] :reload-all)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false))
