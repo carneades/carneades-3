bbab4451b69cef9c74e4df01ff5fab3aa35c6a98
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-02-27 16:19:00 +0100

    relicenses the project from EuPL to MPL 2.0

--- src/CarneadesWeb/server/src/carneades/web/modules/polican/dispatcher.clj
+++ src/CarneadesWeb/server/src/carneades/web/modules/polican/dispatcher.clj
@@ -1,5 +1,7 @@
-;;; Copyright (c) 2012 Fraunhofer Gesellschaft
-;;; Licensed under the EUPL V.1.1
+;; Copyright (c) 2012 Fraunhofer Gesellschaft
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 (ns carneades.web.modules.polican.dispatcher
   (:use [clojure.data.json :only [json-str read-json]]
