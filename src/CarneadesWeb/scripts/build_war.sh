e52e188978966a8de2d3df1160e125589639a8e7
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-03-27 15:24:38 +0100

    non-interactive build script

--- src/CarneadesWeb/scripts/build_war.sh
+++ src/CarneadesWeb/scripts/build_war.sh
@@ -1,42 +1,13 @@
 #!/bin/bash
 
-#### vars
+## vars
 WAR_NAME="carneades"
-#TOMCAT_DEPLOY_PATH="/var/lib/tomcat8/webapps"
-TOMCAT_DEPLOY_PATH="/usr/local/Cellar/tomcat/7.0.52/libexec/webapps"
 
-#################################################################################
-####                                                                            #
-#### HELPERS                                                                    #
-####                                                                            #
-####                                                                            #
-#################################################################################
-set_deploy_path () {
-    cont=true
-    while $cont
-    do
-        printf "Set the path to your tomcat webapps folder (${TOMCAT_DEPLOY_PATH}): "
-        read line
-        case "$line" in
-            '')
-                cont=false
-                ;;
-            *)
-                if [ -d "$line" ];
-                then
-                    TOMCAT_DEPLOY_PATH=$line
-                    cont=false
-                else
-                    echo "Folder \'$line\' does not exist. try again."
-                fi
-                ;;
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
-        esac
-    done
-}
+## Helpers
 
 prepare_local_deps () {
-    SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
     cd $SCRIPTPATH/../../CarneadesEngine
     lein install
@@ -45,12 +16,24 @@ prepare_local_deps () {
     lein install
 }
 
-prepare_local_deps
-cd $SCRIPTPATH/../server
-lein with-profiles tomcat ring uberwar $WAR_NAME.war
+show_usage () {
+    echo "`basename $0` [--deploy PATH]"
+    exit 0
+}
 
-if [[ $* == *--deploy* ]]
+## Main
+
+
+prepare_local_deps
+
+cd $SCRIPTPATH/../server
+lein with-profiles war ring uberwar $WAR_NAME.war
+
+if [[ "$1" == "--deploy" ]]
 then
-    set_deploy_path
-    mv $SCRIPTPATH/../server/target/$WAR_NAME.war $TOMCAT_DEPLOY_PATH
+    if [ -z "$2" ]; then
+        show_usage
+    fi
+
+    mv $SCRIPTPATH/../server/target/$WAR_NAME.war $2
 fi
