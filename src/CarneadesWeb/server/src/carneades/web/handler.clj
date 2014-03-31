7588eecf9efe492c15b996f1f523a208689bb0c6
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-03-31 11:48:50 +0200

    replace picky wrap-file ring handler by our own

--- src/CarneadesWeb/server/src/carneades/web/handler.clj
+++ src/CarneadesWeb/server/src/carneades/web/handler.clj
@@ -4,8 +4,7 @@
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 (ns carneades.web.handler
-  ^{:author "Sebastian Kaiser"
-    :doc "Handler for servlet container. Initiates routes."}
+  ^{:doc "Handler for servlet container. Initiates routes."}
   (:require [taoensso.timbre :as timbre :refer (trace debug info warn error fatal spy)]
             [compojure.core :refer :all]
             [org.httpkit.server :refer [run-server]]
@@ -15,13 +14,12 @@
             [compojure.route :as route :refer [files resources not-found]]
             [sandbar.stateful-session :as session]
             [carneades.web.service :as service]
-            [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.multipart-params :refer [wrap-multipart-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.util.codec :as codec]
+            [ring.util.response :as response]
             [com.postspectacular.rotor :as rotor]))
-
-(def deploy false)
 
 (defroutes app-routes
   (route/resources "/")
@@ -65,33 +63,38 @@
   (service/stop)
   (println "shutting down..."))
 
+(defn wrap-file
+  "Like ring.middleware.file/wrap-file but does not ensure that the
+  directory exists. It prevents deployment error if the directory does
+  not exists and the handler is compiled but no used."
+  [app ^String root-path & [opts]]
+  (let [opts (merge {:root root-path, :index-files? true, :allow-symlinks? false} opts)]
+    (fn [req]
+      (if-not (= :get (:request-method req))
+        (app req)
+        (let [path (.substring ^String (codec/url-decode (:uri req)) 1)]
+          (or (response/file-response path opts)
+              (app req)))))))
+
 (def all-routes [carneades-web-routes app-routes])
 
 (def tomcat-all-routes [tomcat-carneades-web-routes tomcat-app-routes])
 
-(if deploy
-  (do
-   (def tomcat-app (-> (apply routes tomcat-all-routes)
+(def tomcat-app (-> (apply routes tomcat-all-routes)
                     (session/wrap-stateful-session)
                     (wrap-keyword-params)
                     (wrap-params)
                     (wrap-multipart-params)))
-   (def tomcat-war-handler (middleware/war-handler tomcat-app))
 
-   (print "Using deploy config."))
+(def tomcat-war-handler (middleware/war-handler tomcat-app))
 
-  (do
-    (def app (-> (apply routes all-routes)
-                (session/wrap-stateful-session)
-                (wrap-keyword-params)
-                (wrap-params)
-                (wrap-multipart-params)
-                (wrap-file "../client/dist")
-                ))
-    (def war-handler (middleware/war-handler app))
+(def app (-> (apply routes all-routes)
+             (session/wrap-stateful-session)
+             (wrap-keyword-params)
+             (wrap-params)
+             (wrap-multipart-params)
+             (wrap-file "../client/dist")))
 
-    (print "Using local config.")))
-
-
+(def war-handler (middleware/war-handler app))
 
 
