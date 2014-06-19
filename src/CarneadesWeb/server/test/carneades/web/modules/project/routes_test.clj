cc6b75e2af141be102e89fa1f99c6df1218163d5
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-06-19 17:01:19 +0200

    create a temporary folder for the test

--- src/CarneadesWeb/server/test/carneades/web/modules/project/routes_test.clj
+++ src/CarneadesWeb/server/test/carneades/web/modules/project/routes_test.clj
@@ -3,11 +3,32 @@
             [ring.mock.request :refer :all]
             [taoensso.timbre :as timbre :refer [debug info spy]]
             [carneades.web.handler :refer [app]]
-            [cheshire.core :refer [parse-string encode]]))
+            [cheshire.core :refer [parse-string encode]]
+            [carneades.engine.utils :as utils]
+            [carneades.engine.uuid :refer [make-uuid-str]]
+            [carneades.project.admin :as project]
+            [carneades.database.admin :as db]))
 
 ;; TODO: creates a temporary project directory
 
 (def base-url "/carneades/api")
+(def user "root")
+(def password "pw1")
+
+(def state (atom nil))
+
+(defn initial-state-value
+  []
+  {:project-name (str "testproject-" (make-uuid-str))})
+
+(defn create-project
+  []
+  (reset! state (initial-state-value))
+  (db/create-missing-dbs (:project-name @state) user password))
+
+(defn delete-project
+  []
+  (utils/delete-file-recursively (project/get-project-path (:project-name @state))))
 
 (defn parse
   [s]
@@ -17,33 +38,41 @@
   [rules sid]
   (:value (first (filter #(= (:ruleid %) sid) rules))))
 
-(fact "It is possible to post a profile and read it back."
-      (let [profile {:metadata {:title "One profile"}
-                     :rules '[{:ruleid r1-a
-                               :value 1.0}
-                              {:ruleid r2-b
-                               :value 0.0}
-                              {:ruleid r3-c
-                               :value 0.5}]
-                     :default true}
-            response (app (-> (request :post
-                                       (str base-url
-                                            "/projects/markos/legalprofiles/"))
-                              (body (encode profile))
-                              (content-type "application/json")))
-            body (parse (:body response))
-            id (:id body)
-            response2 (app (-> (request :get
+
+(with-state-changes [(before :facts (create-project))
+                     (after :facts (delete-project))]
+  (fact "It is possible to post a profile and read it back."
+        (let [project (:project-name @state)
+              profile {:metadata {:title "One profile"}
+                      :rules '[{:ruleid r1-a
+                                :value 1.0}
+                               {:ruleid r2-b
+                                :value 0.0}
+                               {:ruleid r3-c
+                                :value 0.5}]
+                      :default true}
+             response (app (-> (request :post
                                         (str base-url
-                                             "/projects/markos/legalprofiles/"
-                                             id))
+                                             "/projects/"
+                                             project
+                                             "/legalprofiles/"))
+                               (body (encode profile))
                                (content-type "application/json")))
-            profile' (parse (:body response2))]
-        (expect (select-keys (:metadata profile') (keys (:metadata profile))) =>
-                (:metadata profile))
-        (expect (:default profile') => true)
-        (expect (get-rule-value (:rules profile') "r1-a") => 1.0)
-        (expect (get-rule-value (:rules profile') "r2-b") => 0.0)
-        (expect (get-rule-value (:rules profile') "r3-c") => 0.5)))
+             body (parse (:body response))
+             id (:id body)
+             response2 (app (-> (request :get
+                                         (str base-url
+                                              "/projects/"
+                                              project
+                                              "/legalprofiles/"
+                                              id))
+                                (content-type "application/json")))
+             profile' (parse (:body response2))]
+         (expect (select-keys (:metadata profile') (keys (:metadata profile))) =>
+                 (:metadata profile))
+         (expect (:default profile') => true)
+         (expect (get-rule-value (:rules profile') "r1-a") => 1.0)
+         (expect (get-rule-value (:rules profile') "r2-b") => 0.0)
+         (expect (get-rule-value (:rules profile') "r3-c") => 0.5))))
 
 (fact "It is possible to update the metadata of a profile.")
