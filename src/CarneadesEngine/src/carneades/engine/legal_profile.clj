c7243c94dedea102cc66e07268a56a6ee480c634
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-06-30 15:05:55 +0200

    add a minimal incomplete implementation of the legal profile

--- src/CarneadesEngine/src/carneades/engine/legal_profile.clj
+++ src/CarneadesEngine/src/carneades/engine/legal_profile.clj
@@ -9,9 +9,27 @@ associated to each rule in the subset. The arguments instantiated from
 the rules in the subset have a premise added automatically, with the
 corresponding value v. This premise acts as a kind of switch to
 'activate' or 'deactivate' the rule."}
-  carneades.engine.legal-profile)
+  carneades.engine.legal-profile
+  (:require [carneades.engine.theory.zip :as tz]
+            [taoensso.timbre :as timbre :refer [debug info spy]]
+            [carneades.engine.argument :as a]))
+
+(defn- insert-switch-rule
+  [premises scheme rule]
+  (conj premises (a/pm (list 'valid (:id scheme)))))
+
+(defn- apply-profile-to-scheme
+  [profile scheme]
+  (if-let [rule (first (filter #(= (:ruleid %) (:id scheme)) (:rules profile)))]
+    (update-in scheme [:premises] insert-switch-rule scheme rule)
+    scheme))
+
+(defn- apply-profile-to-section
+  [profile section]
+  (assoc section :schemes
+         (into [] (map (partial apply-profile-to-scheme profile) (:schemes section)))))
 
 (defn apply-legal-profile
   "Apply a legal profile to a theory. Return the transformed theory."
   [theory profile]
-  )
+  (tz/map-theory theory (partial apply-profile-to-section profile)))
