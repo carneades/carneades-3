f19bbb9c91905015dc040f248c78aed3bf7f862f
pierre.allix@fokus.fraunhofer.de <pierre.allix@fokus.fraunhofer.de>
2014-07-09 17:15:55 +0200

    remove old sysinfo directory

--- src/CarneadesWeb/server/src/carneades/web/modules/admin/routes.clj
+++ src/CarneadesWeb/server/src/carneades/web/modules/admin/routes.clj
@@ -5,12 +5,14 @@
 
 (ns carneades.web.modules.admin.routes
   (:use compojure.core)
-  (:require [carneades.web.modules.admin.views.layout :as layout]))
+  ;; (:require [carneades.web.modules.admin.views.layout :as layout])
+  )
 
 (defn admin-project-page []
-  (layout/render "home.html" {:projects "abc"
-                              :base "/carneades"
-                              :context2 "/carneades/projects"}))
+  ;; (layout/render "home.html" {:projects "abc"
+  ;;                             :base "/carneades"
+  ;;                             :context2 "/carneades/projects"})
+  )
 
 (defroutes admin-routes*
   (GET "/projects" [] (admin-project-page)))
