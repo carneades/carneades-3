7d4b4b8f18a4fb09089ed722ed6ef727b2906bfc
pierre.allix@fokus.fraunhofer.de <pierre.allix@fokus.fraunhofer.de>
2014-07-09 14:48:27 +0200

    add implementation of the import for CAF XML format

---
+++ src/CarneadesEngine/test/carneades/xml/caf/import_test.clj
@@ -0,0 +1,41 @@
+(ns carneades.xml.caf.import-test
+  (:require [midje.sweet :refer :all]
+            [clojure.pprint :refer [pprint]]
+            [carneades.engine.statement :as s]
+            [carneades.engine.argument :as a]
+            [carneades.engine.argument-graph :as ag]
+            [carneades.xml.caf.export :as cafo]
+            [carneades.xml.caf.import :as cafi]
+            [carneades.engine.argument-evaluation :refer [evaluate]]
+            [carneades.engine.aspic :refer [aspic-grounded]]
+            [taoensso.timbre :as timbre :refer [debug info spy]]))
+
+(fact "(comp import export) is equivalent to identity"
+      (let [wears-ring (s/make-statement :text {:en "Fred wears a ring."}
+                                         :header {:description {:en "A long description from Fred wearing a ring."}
+                                                  :creator "John"})
+            married (s/make-statement :text {:en "Fred is married."} :atom '(married Fred))
+            a1 (a/make-argument :id 'a1 :conclusion married :premises [(a/pm wears-ring)])
+            g (-> (ag/make-argument-graph)
+                  (ag/enter-arguments [a1])
+                  (ag/accept [wears-ring]))
+            g (evaluate aspic-grounded g)
+            g (assoc g :header {:title "Marriage"
+                                :description {:en "Wedding"
+                                              :de "Hochzeit"}})
+            g (ag/enter-reference g {:title "ref1" :key "k1"})
+            g (ag/enter-reference g {:title "ref2" :key "k2"})
+            get-stmt (fn [id] (into {} (ag/get-statement-node g id)))
+            n1 (get-stmt wears-ring)
+            n1attrs (select-keys n1 [:id :main :standard :value :weight])
+            n2 (get-stmt married)
+            n2attrs (select-keys n2 [:id :main :standard :value :weight])
+            output (cafo/export g)
+            tempfile (java.io.File/createTempFile "argumentgraph" ".xml")
+            _ (spit tempfile output)
+            g' (cafi/import tempfile)]
+        (:header g') => (:header g)
+        (:arguments-nodes g') => (:arguments-nodes g)
+        (:statements-nodes g') => (:statements-nodes g)
+        (:references g') => (:references g)
+        g' => g))
