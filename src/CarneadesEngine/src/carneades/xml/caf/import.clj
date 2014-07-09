3f4716589938be3b9efddf6d77e79642e14edabf
pierre.allix@fokus.fraunhofer.de <pierre.allix@fokus.fraunhofer.de>
2014-07-09 12:21:03 +0200

    import of arguments and premises

--- src/CarneadesEngine/src/carneades/xml/caf/import.clj
+++ src/CarneadesEngine/src/carneades/xml/caf/import.clj
@@ -3,46 +3,126 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(ns ^{:doc "Import from XML to argument graphs using the Carneades Argument Format (CAF)."}
+(ns ^{:doc "Import from XML to argument graphs using the Carneades
+  Argument Format (CAF)."}
   carneades.xml.caf.import
   (:require [clojure.data.xml :as x]
             [clojure.java.io :as io]
-            [taoensso.timbre :as timbre :refer [debug info spy]]
-            [carneades.xml.validation :refer [create-validation-fn]]
             [clojure.data.zip.xml :refer [attr text xml->]]
-            [clojure.zip :as z])
+            [clojure.zip :as z]
+            [carneades.xml.validation :refer [create-validation-fn]]
+            [taoensso.timbre :as timbre :refer [debug info spy]]
+            [carneades.engine.utils :refer [unserialize-atom]]
+            [carneades.engine.statement :as st]
+            [carneades.engine.argument :as a]
+            [carneades.engine.argument-graph :as ag])
   (:refer-clojure :exclude [import]))   
 
-(defn import-description
+(defn- import-description
   [desc]
   (let [n (z/node desc)]
     {(keyword (-> n :attrs :lang))
      (first (:content n))}))
 
-(defn import-metadata
+(defn- import-metadata
   [md]
   (let [descs (map import-description (xml-> md :descriptions :description))
         attrs (:attrs (z/node md))]
-    (debug descs)
     (if (empty? descs)
       attrs
       (merge attrs {:description (apply merge descs)}))))
 
+(defn- import-boolean
+  [s]
+  (debug "import-boolean:" s)
+  (when s
+   (Boolean/parseBoolean s)))
+
+(defn- import-atom
+  [s]
+  (when s
+    (unserialize-atom s)))
+
+(defn- import-float
+  [s]
+  (when s
+   (Float/parseFloat s)))
+
+(defn- import-standard
+  [s]
+  (if s
+    (keyword (.toLowerCase s))
+    :pe))
+
 (defn- import-statement
   [stmt]
-  (debug "import-statement")
   (let [metadata (first (map import-metadata (xml-> stmt :metadata)))
-        ]
-    (debug "metadata" metadata)
-    (debug (map z/node (xml-> stmt :descriptions)))))
+        descs (map import-description (xml-> stmt :descriptions :description))
+        text (apply merge descs)
+        attrs (:attrs (z/node stmt))]
+    (-> attrs
+        (update-in [:id] symbol)
+        (update-in [:atom] unserialize-atom)
+        (assoc :header metadata)
+        (update-in [:positive] import-boolean)
+        (update-in [:weight] import-float)
+        (update-in [:value] import-float)
+        (update-in [:main] import-boolean)
+        (update-in [:standard] import-standard)
+        (assoc :text text)
+        (st/map->statement))))
+
+(defn- import-conclusion
+  [conclusion]
+  (symbol (-> (z/node conclusion) :attrs :statement)))
+
+(defn find-statement
+  [id stmts]
+  (first (filter #(= (:id %) id) stmts)))
+
+(defn import-premise
+  [stmts prem]
+  (let [attrs (:attrs (z/node prem))
+        stmtid (symbol (:statement attrs))
+        stmt (find-statement stmtid stmts)]
+    (-> attrs
+        (update-in [:positive] import-boolean)
+        (update-in [:implicit] import-boolean)
+        (assoc :statement stmt)
+        (a/map->premise))))
+
+(defn- import-argument
+  [stmts arg]
+  (let [metadata (first (map import-metadata (xml-> arg :metadata)))
+        conclusionid (import-conclusion (first (xml-> arg :conclusion)))
+        conclusion (find-statement conclusionid stmts)
+        premises (map (partial import-premise stmts) (xml-> arg :premises :premise))
+        exceptions (map (partial import-premise stmts) (xml-> arg :exceptions :exception))
+        attrs (:attrs (z/node arg))]
+    (-> attrs
+        (update-in [:strict] import-boolean)
+        (update-in [:weight] import-float)
+        (update-in [:value] import-float)
+        (assoc :conclusion conclusion)
+        (update-in [:pro] import-boolean)
+        (assoc :premises premises)
+        (assoc :exceptions exceptions)
+        (assoc :header metadata)
+        (a/map->argument))))
 
 (defn- import-caf
   [xml]
   (let [zipper (z/xml-zip xml)
-        metadata (first (map z/node (xml-> zipper :metadata)))
-        statements (doall (map import-statement (xml-> zipper :statements :statement)))
+        metadata (first (map import-metadata (xml-> zipper :metadata)))
+        statements (map import-statement (xml-> zipper :statements :statement))
+        arguments (map (partial import-argument statements)
+                       (xml-> zipper :arguments :argument))
+        ;; _ (debug "statements= " statements)
         ;; metadata (z/node (xml-> zipper :statements))
+        ;; arguments
+        ;; references
         ]
+    (debug statements)
     )
   )
 
