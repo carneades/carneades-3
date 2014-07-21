256617f7111b0a26817c0e6c5c3e00d72c1c9bf7
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-07-21 17:02:15 +0200

    fix carneades.project.fs-test

--- src/CarneadesEngine/test/carneades/project/fs_test.clj
+++ src/CarneadesEngine/test/carneades/project/fs_test.clj
@@ -3,7 +3,7 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(ns carneades.project.admin-test
+(ns carneades.project.fs-test
   (:require [carneades.project.fs :as project]
             [me.raynes.fs :as fs]
             [carneades.engine.uuid :refer [make-uuid-str]]
@@ -19,7 +19,7 @@
 (defn create-tmp-project
   []
   (reset! state (initial-state-value))
-  (project/create-project (:project-name @state)))
+  (project/create-project-files (:project-name @state)))
 
 (defn delete-tmp-project
   []
