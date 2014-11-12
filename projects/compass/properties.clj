de0b551cab85c67dbbcd19ca623d56174de96c91
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2014-11-12 11:14:54 +0100

    add a 'compass' project for policy compass

---
+++ projects/compass/properties.clj
@@ -0,0 +1,9 @@
+{:title "Policy Compass",
+ :creation-date "November 2014",
+ :schemes "default/walton_schemes",
+ :description
+ {:en
+  "The main goal of [Policy Compass](http://policycompass.eu/) is to develop a research prototype of an easy-to-use, highly visual and intuitive tool for social networks and eParticipation platforms, enabling citizens and public officials to easily create, apply, share, embed, annotate and discuss causal models, charts and graphs of historical data from trusted open data sources. The aim is to develop methods and tools that facilitate more factual, evidence-based, transparent and accountable policy evaluation and analysis.
+
+"}
+ }
